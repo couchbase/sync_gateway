0c04525f0c934e24dc966531e7e776551f866d86
Jens Alfke <jens@couchbase.com>
2013-05-08 15:41:25 -0700

    Restructured repository with dependent packages as Git submodules
    This fixes the package versioning problem: "go get" always fetches the
    latest revision, so we can't get repeatable builds of sync_gateway or
    defend against it breaking from someone else's checkin into another
    repository.
    
    Instead of using "go get", we now use git to fetch specific revisions
    of the packages we need, as submodules. This populates "vendor/src", and
    we can tell Go to use "vendor" as a GOPATH entry so it finds the
    packages.
    
    We also treat the repository root directory itself as a GOPATH directory
    and move the gateway sources into it. That way one can check out this
    repository anywhere, and set GOPATH to ".:./vendor" and build.
    
    NOTE: To build, run "build.sh". A regular "go install" or "go build"
    won't work because the GOPATH needs to be set up first.
    
    Fixes #68.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script builds the sync gateway. You can't just run "go install" on its own, because we
+# need to tell the Go compiler how to find the dependent packages (in vendor) and the gateway
+# source code (in src) by setting $GOPATH.
+
+set -e
+export GOPATH="`pwd`:`pwd`/vendor"
+go install github.com/couchbaselabs/sync_gateway
+echo "Success! Output is bin/sync_gateway"
