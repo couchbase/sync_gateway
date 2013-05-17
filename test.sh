653a3238ce72087a8778aba4b2ac0cde6eecc234
Dustin Sallings <dustin@spy.net>
2013-05-16 22:09:17 -0700

    Ignore "untagged fields" warning from vet.

--- test.sh
+++ test.sh
@@ -7,4 +7,9 @@ cd src/github.com/couchbaselabs/sync_gateway
 # First build everything so the tests don't complain about out-of-date packages
 go test -i ./...
 go test ./... "$@"
-go vet ./...
+
+# vet reports this error which is actually OK here.  There are flags
+# to vet to tell it just what you want, but he invocation is a lot
+# more awkward, so I'm just going to grep away the thing we don't care
+# about so we can see the things we do.
+go vet ./... 2>&1 | grep -v "literal uses untagged fields"
