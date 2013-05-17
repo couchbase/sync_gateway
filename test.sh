fb1d55024a44d185a76f6cf20139380c23aad5ad
Dustin Sallings <dustin@spy.net>
2013-05-16 21:49:17 -0700

    Added vet to the test, simplified invocation.

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ cd src/github.com/couchbaselabs/sync_gateway
 
 # First build everything so the tests don't complain about out-of-date packages
 go test -i
-
-go test ./base ./auth ./channels ./db ./rest $@
+go test ./... "$@"
+go vet ./...
