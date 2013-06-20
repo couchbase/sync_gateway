df9d6684d1f1166421cc54f6680be7bd5e03f985
Chris Anderson <jchris@couchbase.com>
2013-06-20 16:21:13 -0700

    fix address for admin_party.json

--- tests/node-tap/test/assert-test.js
+++ tests/node-tap/test/assert-test.js
@@ -2,9 +2,9 @@ var test = require("tap").test,
   coax = require("coax");
 
 test("reachable", function(t){
-  coax("http://localhost:4984/default", function(err, info) {
+  coax("http://localhost:4984/db", function(err, info) {
     t.assert(!err, "no error")
-    t.equal(info.db_name, "default", "correct db name")
+    t.equal(info.db_name, "db", "correct db name")
     t.end()
   })
 })
