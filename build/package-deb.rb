0c33bcf7f9e09ee4c24a97d6ad78cc3f3a7caacc
Phil Labee <philip@couchbase.com>
2013-10-22 16:29:32 -0700

    CBLT-82:  call dh_make in case it's a new directory
    Change-Id: I9d36748121ec57f4e69a9027a2e825c2510ff9da

--- build/package-deb.rb
+++ build/package-deb.rb
@@ -7,33 +7,40 @@ require 'rake'
 PRODUCT         = "couchbase-sync-gateway"
 PRODUCT_BASE    = "couchbase"
 PRODUCT_KIND    = "sync-gateway"
+DEBEMAIL        = "build@couchbase.com"
 
 PREFIX          = ARGV[0] || "/opt/couchbase"
 PREFIXD         = ARGV[1] || "./opt-couchbase-sync-gateway"
 PRODUCT_VERSION = ARGV[2] || "1.0-1234"
 RELEASE         = PRODUCT_VERSION.split('-')[0]
 
+PKGNAME="#{PRODUCT}_#{PRODUCT_VERSION}"
+
 STARTDIR = Dir.getwd()
 
 product_base_cap = PRODUCT_BASE[0..0].upcase + PRODUCT_BASE[1..-1] # Ex: "Couchbase".
 
-STAGE_DIR = "#{STARTDIR}/build/deb/#{PRODUCT}_#{PRODUCT_VERSION}"
-
+STAGE_DIR = "#{STARTDIR}/build/deb/#{PKGNAME}"
 FileUtils.rm_rf   "#{STAGE_DIR}"
 FileUtils.mkdir_p "#{STAGE_DIR}/opt"
 FileUtils.mkdir_p "#{STAGE_DIR}/etc"
-FileUtils.mkdir_p "#{STAGE_DIR}/debian"
 
-Dir.glob("*.tmpl").each do |x|
-    target = "#{x.gsub('.tmpl', '')}"
-    sh %{sed -e s,@@VERSION@@,#{PRODUCT_VERSION},g #{x}         |
-         sed -e s,@@RELEASE@@,#{RELEASE},g                      |
-         sed -e s,@@PREFIX@@,#{PREFIX},g                        |
-         sed -e s,@@PRODUCT@@,#{PRODUCT},g                      |
-         sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g            |
-         sed -e s,@@PRODUCT_BASE_CAP@@,#{product_base_cap},g    |
-         sed -e s,@@PRODUCT_KIND@@,#{PRODUCT_KIND},g > #{target}}
-    sh %{chmod a+x #{target}}
+sh %{cd #{STAGE_DIR} && dh_make -e #{DEBEMAIL} --native --single --packagename #{PKGNAME}}
+
+[["#{STARTDIR}", "#{STAGE_DIR}/debian"]].each do |src_dst|
+    Dir.chdir(src_dst[0]) do
+        Dir.glob("*.tmpl").each do |x|
+            target = "#{src_dst[1]}/#{x.gsub('.tmpl', '')}"
+            sh %{sed -e s,@@VERSION@@,#{PRODUCT_VERSION},g #{x}         |
+                 sed -e s,@@RELEASE@@,#{RELEASE},g                      |
+                 sed -e s,@@PREFIX@@,#{PREFIX},g                        |
+                 sed -e s,@@PRODUCT@@,#{PRODUCT},g                      |
+                 sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g            |
+                 sed -e s,@@PRODUCT_BASE_CAP@@,#{product_base_cap},g    |
+                 sed -e s,@@PRODUCT_KIND@@,#{PRODUCT_KIND},g > #{target}}
+            sh %{chmod a+x #{target}}
+        end 
+    end
 end
 
 sh %{cp -R #{PREFIXD} #{STAGE_DIR}/opt}
