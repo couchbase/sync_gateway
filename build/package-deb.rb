5391360df11da122a178effcd3fb842425ea8a95
Phil Labee <philip@couchbase.com>
2013-10-22 15:26:47 -0700

    CBLT-82:  add 5th param: PRODUCT_VERSION
    Change-Id: If8b837b029c64a99a62a0d6b0bd73a4dff95daa5

--- build/package-deb.rb
+++ build/package-deb.rb
@@ -4,33 +4,20 @@ require 'rubygems'
 require 'fileutils'
 require 'rake'
 
-PREFIX       = ARGV[0] || "/opt/couchbase"
-PRODUCT      = ARGV[1] || "couchbase-sync-gateway"
-PRODUCT_BASE = ARGV[2] || "couchbase"
-PRODUCT_KIND = ARGV[3] || "sync-gateway"
+PREFIX          = ARGV[0] || "/opt/couchbase"
+PRODUCT         = ARGV[1] || "couchbase-sync-gateway"
+PRODUCT_BASE    = ARGV[2] || "couchbase"
+PRODUCT_KIND    = ARGV[3] || "sync-gateway"
+PRODUCT_VERSION = ARGV[4] || "1.0-1234"
+RELEASE         = PRODUCT_VERSION.split('-')[0]
 
 STARTDIR = Dir.getwd()
 
-if ENV["PRODUCT_VERSION"] != nil
-   PRODUCT_GIT            = ENV["PRODUCT_VERSION"]
-else
-    PRODUCT_GIT            = `#{'git'} describe`.chomp
-end
-
-PRODUCT_VERSION        = PRODUCT_GIT + '-' + `uname -s`.chomp.downcase + "." + `uname -m`.chomp
-PRODUCT_VERSION_PREFIX = PRODUCT_VERSION.split('-')[0]
-
 product_base_cap = PRODUCT_BASE[0..0].upcase + PRODUCT_BASE[1..-1] # Ex: "Couchbase".
 
-ver = PRODUCT_VERSION_PREFIX
-ver = "1.0~" + ver unless ver.match(/^[0-9]/) # dpkg-deb/deb-version wants a number prefix.
+STAGE_DIR = "#{STARTDIR}/build/deb/#{PRODUCT}_#{PRODUCT_VERSION}"
 
-REL = PRODUCT_GIT.split('-')[1] || 1
-
-STAGE_DIR = "#{STARTDIR}/build/deb/#{PRODUCT}_#{ver}-#{REL}"
-
-FileUtils.rm_rf STAGE_DIR
-
+FileUtils.rm_rf   "#{STAGE_DIR}"
 FileUtils.mkdir_p "#{STAGE_DIR}/opt"
 FileUtils.mkdir_p "#{STAGE_DIR}/etc"
 FileUtils.mkdir_p "#{STAGE_DIR}/debian"
@@ -39,8 +26,8 @@ FileUtils.mkdir_p "#{STAGE_DIR}/debian"
   Dir.chdir(src_dst[0]) do
     Dir.glob("*.tmpl").each do |x|
       target = "#{src_dst[1]}/#{x.gsub('.tmpl', '')}"
-      sh %{sed -e s,@@VERSION@@,#{ver},g #{x} |
-           sed -e s,@@RELEASE@@,#{REL},g |
+      sh %{sed -e s,@@VERSION@@,#{PRODUCT_VERSION},g #{x} |
+           sed -e s,@@RELEASE@@,#{RELEASE},g |
            sed -e s,@@PREFIX@@,#{PREFIX},g |
            sed -e s,@@PRODUCT@@,#{PRODUCT},g |
            sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g |
@@ -54,8 +41,7 @@ end
 sh %{cp -R #{PREFIX} #{STAGE_DIR}/opt}
 
 Dir.chdir STAGE_DIR do
-  sh %{dch -b -v #{ver} "Released debian package for version #{ver}"}
+  sh %{dch -b -v #{PRODUCT_VERSION} "Released debian package for version #{PRODUCT_VERSION}"}
   sh %{dpkg-buildpackage -b}
 end
-
 
