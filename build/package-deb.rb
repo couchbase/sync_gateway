cb7950a8b0f44c44803e45ad88b2306bab901550
Phil Labee <philip@couchbase.com>
2013-10-22 15:39:41 -0700

    CBLT-82:  call with 3 params: PREFIX, PREFIXD, PRODUCT_VERSION
    Change-Id: If2f484e056bfd1eda9d8cd291cf3f694676ee7e1

--- build/package-deb.rb
+++ build/package-deb.rb
@@ -4,11 +4,13 @@ require 'rubygems'
 require 'fileutils'
 require 'rake'
 
+PRODUCT         = "couchbase-sync-gateway"
+PRODUCT_BASE    = "couchbase"
+PRODUCT_KIND    = "sync-gateway"
+
 PREFIX          = ARGV[0] || "/opt/couchbase"
-PRODUCT         = ARGV[1] || "couchbase-sync-gateway"
-PRODUCT_BASE    = ARGV[2] || "couchbase"
-PRODUCT_KIND    = ARGV[3] || "sync-gateway"
-PRODUCT_VERSION = ARGV[4] || "1.0-1234"
+PREFIXD         = ARGV[1] || "./opt-couchbase-sync-gateway"
+PRODUCT_VERSION = ARGV[3] || "1.0-1234"
 RELEASE         = PRODUCT_VERSION.split('-')[0]
 
 STARTDIR = Dir.getwd()
@@ -22,20 +24,16 @@ FileUtils.mkdir_p "#{STAGE_DIR}/opt"
 FileUtils.mkdir_p "#{STAGE_DIR}/etc"
 FileUtils.mkdir_p "#{STAGE_DIR}/debian"
 
-[["#{PRODUCT_KIND}", "#{STAGE_DIR}/debian"]].each do |src_dst|
-  Dir.chdir(src_dst[0]) do
-    Dir.glob("*.tmpl").each do |x|
-      target = "#{src_dst[1]}/#{x.gsub('.tmpl', '')}"
-      sh %{sed -e s,@@VERSION@@,#{PRODUCT_VERSION},g #{x} |
-           sed -e s,@@RELEASE@@,#{RELEASE},g |
-           sed -e s,@@PREFIX@@,#{PREFIX},g |
-           sed -e s,@@PRODUCT@@,#{PRODUCT},g |
-           sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g |
-           sed -e s,@@PRODUCT_BASE_CAP@@,#{product_base_cap},g |
-           sed -e s,@@PRODUCT_KIND@@,#{PRODUCT_KIND},g > #{target}}
-      sh %{chmod a+x #{target}}
-    end
-  end
+Dir.glob("*.tmpl").each do |x|
+    target = "#{x.gsub('.tmpl', '')}"
+    sh %{sed -e s,@@VERSION@@,#{PRODUCT_VERSION},g #{x}         |
+         sed -e s,@@RELEASE@@,#{RELEASE},g                      |
+         sed -e s,@@PREFIX@@,#{PREFIX},g                        |
+         sed -e s,@@PRODUCT@@,#{PRODUCT},g                      |
+         sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g            |
+         sed -e s,@@PRODUCT_BASE_CAP@@,#{product_base_cap},g    |
+         sed -e s,@@PRODUCT_KIND@@,#{PRODUCT_KIND},g > #{target}}
+    sh %{chmod a+x #{target}}
 end
 
 sh %{cp -R #{PREFIX} #{STAGE_DIR}/opt}
