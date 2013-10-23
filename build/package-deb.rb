bcac4af9aae6ae35348ace2679d3cd5239d09345
Phil Labee <philip@couchbase.com>
2013-10-23 09:14:00 -0700

    CBLT-82:  fix up copy statement so that payload is included
    Change-Id: I0744fec3fb839331e1ba2096dd34fc43825998f2

--- build/package-deb.rb
+++ build/package-deb.rb
@@ -15,11 +15,9 @@ PRODUCT_VERSION = ARGV[2] || "1.0-1234"
 RELEASE         = PRODUCT_VERSION.split('-')[0]
 
 PKGNAME="#{PRODUCT}_#{PRODUCT_VERSION}"
-
-STARTDIR = Dir.getwd()
-
 product_base_cap = PRODUCT_BASE[0..0].upcase + PRODUCT_BASE[1..-1] # Ex: "Couchbase".
 
+STARTDIR  = Dir.getwd()
 STAGE_DIR = "#{STARTDIR}/build/deb/#{PKGNAME}"
 FileUtils.rm_rf   "#{STAGE_DIR}"
 FileUtils.mkdir_p "#{STAGE_DIR}/opt"
@@ -27,6 +25,9 @@ FileUtils.mkdir_p "#{STAGE_DIR}/etc"
 
 sh %{cd #{STAGE_DIR} && dh_make -e #{DEBEMAIL} --native --single --packagename #{PKGNAME}}
 
+FileUtils.copy_entry #{PREFIXD} #{STAGE_DIR}/opt/#{PRODUCT}
+
+[["#{PRODUCT_KIND}", "#{STAGE_DIR}/debian"]].each do |src_dst|
 [["#{STARTDIR}", "#{STAGE_DIR}/debian"]].each do |src_dst|
     Dir.chdir(src_dst[0]) do
         Dir.glob("*.tmpl").each do |x|
@@ -43,9 +44,8 @@ sh %{cd #{STAGE_DIR} && dh_make -e #{DEBEMAIL} --native --single --packagename #
     end
 end
 
-sh %{cp -R #{PREFIXD} #{STAGE_DIR}/opt}
 
-Dir.chdir STAGE_DIR do
+Dir.chdir #{STAGE_DIR} do
   #sh %{dch -b -v #{PRODUCT_VERSION} "Released debian package for version #{PRODUCT_VERSION}"}
   sh %{dpkg-buildpackage -B -uc}
 end
