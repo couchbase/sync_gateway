670412ad7123f3423f7d26ba246d88a37661093d
Jens Alfke <jens@couchbase.com>
2014-02-04 08:18:15 -0800

    Give clear & obvious warning if trying to build with older Go compiler
    This should help people trying to build the Sync Gateway when they don't
    have Go 1.2; I've gotten two reports of this in the past day alone.
    
    Note that due to my very limited bash fu, this will also fail for people
    using a _newer_ version of Go like a dev build of 1.3. To work around
    this you'll either need to locally hack your go.sh script, or fix it
    to do a smarter job of version checking.

--- go.sh
+++ go.sh
@@ -2,5 +2,12 @@
 
 # This script runs the given Go subcommand with GOPATH set up correctly for sync_gateway.
 
+if [[ `go version` != *go1.2* ]]
+then
+    echo "*** Go 1.2 is required to build Sync Gateway; you have" `go version`
+    echo "Please visit http://golang.org/doc/install or use your package manager to upgrade."
+    exit 1
+fi
+
 export GOPATH="`pwd`:`pwd`/vendor"
 go "$@"
