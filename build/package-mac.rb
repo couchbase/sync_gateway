bb5de4b99e823076cbf683ae147b8bd1c3f823d6
Phil Labee <philip@couchbase.com>
2014-02-21 11:09:21 -0800

    CBLT-226:  prepare files for inclusion in macosx pacakge
    Change-Id: If630b1442f24bf2ea87d6f58e005330fac629a88

--- build/package-mac.rb
+++ build/package-mac.rb
@@ -2,7 +2,6 @@
 # 
 ####    ./${PKGR} ${PREFIX} ${PREFIXP} ${REVISION} ${GITSPEC} ${PLATFORM}    ${ARCH}
 #                                                   master     macosx-x84_64  x86_64
-
 require 'rubygems'
 require 'fileutils'
 require 'rake'
@@ -23,14 +22,47 @@ BLDNUM          = PRODUCT_VERSION.split('-')[1]    # e.g., 1234
 
 PKGNAME="#{PRODUCT}_#{RELEASE}-#{BLDNUM}_#{PLATFORM}"
 
+echo "DEBUG:  0: PREFIX          = ${PREFIX}"
+echo "DEBUG:  1: PREFIXD         = ${PREFIXD}"
+echo "DEBUG:  2: PRODUCT_VERSION = ${PRODUCT_VERSION}"
+echo "DEBUG:  3: REPO_SHA        = ${REPO_SHA}"
+echo "DEBUG:  4: PLATFORM        = ${PLATFORM}"
+echo "DEBUG:  5: ARCH            = ${ARCH}"
+echo " "
+echo "DEBUG:  RELEASE            = ${RELEASE}"
+echo "DEBUG:  BLDNUM             = ${BLDNUM}"
+echo " "
+echo "DEBUG:  PKGNAME = ${PKGNAME}"
+echo " "
+
 START_DIR  = Dir.getwd()
 STAGE_DIR = "#{START_DIR}/build/maczip/#{PKGNAME}"
+FileUtils.rm_rf   "#{STAGE_DIR}"
 FileUtils.mkdir_p "#{STAGE_DIR}"
 
+[["#{START_DIR}", "#{STAGE_DIR}"]].each do |src_dst|
+    Dir.chdir(src_dst[0]) do
+        ["manifest.txt.tmpl", "manifest.xml.tmpl"].each do |x|
+            target = "#{src_dst[1]}/#{x.gsub('.tmpl', '')}"
+            sh %{sed -e s,@@VERSION@@,#{BLDNUM},g                    #{x} |
+                 sed -e s,@@PLATFORM@@,#{PLATFORM},g                      |
+                 sed -e s,@@RELEASE@@,#{RELEASE},g                        |
+                 sed -e s,@@REPO_SHA@@,#{REPO_SHA},g                      |
+                 sed -e s,@@PREFIX@@,#{PREFIX},g                          |
+                 sed -e s,@@PRODUCT@@,#{PRODUCT},g                        |
+                 sed -e s,@@PRODUCT_BASE@@,#{PRODUCT_BASE},g              |
+                 sed -e s,@@PRODUCT_BASE_CAP@@,#{product_base_cap},g      |
+                 sed -e s,@@PRODUCT_KIND@@,#{PRODUCT_KIND},g > #{target}}
+            sh %{chmod a+x #{target}}
+        end
+    end
+end
+FileUtils.mv  "#{STAGE_DIR}/manifest.txt", "#{PREFIXD}/manifest.txt"
+FileUtils.mv  "#{STAGE_DIR}/manifest.xml", "#{PREFIXD}/manifest.xml"
 
 Dir.chdir("#{START_DIR}") do
     sh %{tar --directory #{File.dirname(PREFIXD)} -czf "#{PKGNAME}.tar.gz"   #{File.basename(PREFIXD)}}
     FileUtils.cp                                       "#{PKGNAME}.tar.gz", "#{PREFIXD}/#{PKGNAME}.tar.gz"
 end
 
-
+#FileUtils.cp "#{STAGE_DIR}/rpmbuild/RPMS/#{ARCH}/#{PRODUCT}-#{BLDNUM}-#{RELEASE}.#{ARCH}.rpm", "#{PREFIXD}/#{PRODUCT}_#{PRODUCT_VERSION}_#{ARCH}.rpm"
