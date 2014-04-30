46977e8002f15d2130412963b67d0c75c8981ba5
Jens Alfke <jens@couchbase.com>
2014-04-30 14:06:56 -0700

    Logging defaults to non-color (i.e. without ANSI escape sequences)
    To enable color, use -log=color flag

--- src/github.com/couchbaselabs/sync_gateway/base/logging.go
+++ src/github.com/couchbaselabs/sync_gateway/base/logging.go
@@ -46,7 +46,7 @@ func SetLogLevel(level int) {
 
 // Disables ANSI color in log output.
 func LogNoColor() {
-	reset, dim, fgRed, fgYellow = "", "", "", ""
+	// this is now the default state; see LogColor() below
 }
 
 func LogNoTime() {
@@ -69,6 +69,8 @@ func ParseLogFlags(flags []string) {
 		switch key {
 		case "bw":
 			LogNoColor()
+		case "color":
+			LogColor()
 		case "notime":
 			LogNoTime()
 		default:
@@ -205,27 +207,53 @@ func lastComponent(path string) string {
 // ANSI color control escape sequences.
 // Shamelessly copied from https://github.com/sqp/godock/blob/master/libs/log/colors.go
 var (
-	reset      = "\x1b[0m"
-	bright     = "\x1b[1m"
-	dim        = "\x1b[2m"
-	underscore = "\x1b[4m"
-	blink      = "\x1b[5m"
-	reverse    = "\x1b[7m"
-	hidden     = "\x1b[8m"
-	fgBlack    = "\x1b[30m"
-	fgRed      = "\x1b[31m"
-	fgGreen    = "\x1b[32m"
-	fgYellow   = "\x1b[33m"
-	fgBlue     = "\x1b[34m"
-	fgMagenta  = "\x1b[35m"
-	fgCyan     = "\x1b[36m"
-	fgWhite    = "\x1b[37m"
-	bgBlack    = "\x1b[40m"
-	bgRed      = "\x1b[41m"
-	bgGreen    = "\x1b[42m"
-	bgYellow   = "\x1b[43m"
-	bgBlue     = "\x1b[44m"
-	bgMagenta  = "\x1b[45m"
-	bgCyan     = "\x1b[46m"
-	bgWhite    = "\x1b[47m"
+	reset,
+	bright,
+	dim,
+	underscore,
+	blink,
+	reverse,
+	hidden,
+	fgBlack,
+	fgRed,
+	fgGreen,
+	fgYellow,
+	fgBlue,
+	fgMagenta,
+	fgCyan,
+	fgWhite,
+	bgBlack,
+	bgRed,
+	bgGreen,
+	bgYellow,
+	bgBlue,
+	bgMagenta,
+	bgCyan,
+	bgWhite string
 )
+
+func LogColor() {
+	reset = "\x1b[0m"
+	bright = "\x1b[1m"
+	dim = "\x1b[2m"
+	underscore = "\x1b[4m"
+	blink = "\x1b[5m"
+	reverse = "\x1b[7m"
+	hidden = "\x1b[8m"
+	fgBlack = "\x1b[30m"
+	fgRed = "\x1b[31m"
+	fgGreen = "\x1b[32m"
+	fgYellow = "\x1b[33m"
+	fgBlue = "\x1b[34m"
+	fgMagenta = "\x1b[35m"
+	fgCyan = "\x1b[36m"
+	fgWhite = "\x1b[37m"
+	bgBlack = "\x1b[40m"
+	bgRed = "\x1b[41m"
+	bgGreen = "\x1b[42m"
+	bgYellow = "\x1b[43m"
+	bgBlue = "\x1b[44m"
+	bgMagenta = "\x1b[45m"
+	bgCyan = "\x1b[46m"
+	bgWhite = "\x1b[47m"
+}
