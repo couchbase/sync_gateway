1bda320892028b380840127ab2baca27e8fa117b
Sjoerd de Jong <sjoerd@weett.nl>
2014-05-07 15:21:47 +0200

    Added CORS support

--- src/github.com/couchbase/sync_gateway/rest/config.go
+++ src/github.com/couchbase/sync_gateway/rest/config.go
@@ -56,6 +56,7 @@ type ServerConfig struct {
 	ConfigServer                   *string         // URL of config server (for dynamic db discovery)
 	Persona                        *PersonaConfig  // Configuration for Mozilla Persona validation
 	Facebook                       *FacebookConfig // Configuration for Facebook validation
+	CORS                           *CORSConfig     // Configuration for allowing CORS
 	Log                            []string        // Log keywords to enable
 	LogFilePath                    *string         // Path to log file, if missing write to stderr
 	Pretty                         bool            // Pretty-print JSON responses?
@@ -99,6 +100,12 @@ type PersonaConfig struct {
 
 type FacebookConfig struct {
 	Register bool // If true, server will register new user accounts
+}
+
+type CORSConfig struct {
+	Origin []string  // List of allowed origins, use ["*"] to allow access from everywhere
+	Headers []string // List of allowed headers
+	MaxAge int       // Maximum age of the CORS Options request
 }
 
 type ShadowConfig struct {
@@ -278,6 +285,9 @@ func (self *ServerConfig) MergeWith(other *ServerConfig) error {
 	if self.Facebook == nil {
 		self.Facebook = other.Facebook
 	}
+  if self.CORS == nil {
+  	self.CORS = other.CORS
+  }
 	for _, flag := range other.Log {
 		self.Log = append(self.Log, flag)
 	}
--- src/github.com/couchbase/sync_gateway/rest/routing.go
+++ src/github.com/couchbase/sync_gateway/rest/routing.go
@@ -224,12 +224,12 @@ func wrapRouter(sc *ServerContext, privs handlerPrivs, router *mux.Router) http.
 		fixQuotedSlashes(rq)
 		var match mux.RouteMatch
 
-		// Inject CORS if enabled
-		if sc.config.CORS != nil {
-			response.Header().Add("Access-Control-Allow-Origin", strings.Join(sc.config.CORS.Origin, ", "))
-			response.Header().Add("Access-Control-Allow-Credentials", "true")
-			response.Header().Add("Access-Control-Allow-Headers", strings.Join(sc.config.CORS.Headers, ", "))
-		}
+ 		// Inject CORS if enabled
+ 		if sc.config.CORS != nil {
+ 			response.Header().Add("Access-Control-Allow-Origin", strings.Join(sc.config.CORS.Origin, ", "))
+ 			response.Header().Add("Access-Control-Allow-Credentials", "true")
+ 			response.Header().Add("Access-Control-Allow-Headers", strings.Join(sc.config.CORS.Headers, ", "))
+ 		}
 
 		if router.Match(rq, &match) {
 			router.ServeHTTP(response, rq)
@@ -249,10 +249,10 @@ func wrapRouter(sc *ServerContext, privs handlerPrivs, router *mux.Router) http.
 				h.writeStatus(http.StatusNotFound, "unknown URL")
 			} else {
 				response.Header().Add("Allow", strings.Join(options, ", "))
-				if sc.config.CORS != nil {
-					response.Header().Add("Access-Control-Max-Age", strconv.Itoa(sc.config.CORS.MaxAge))
-					response.Header().Add("Access-Control-Allow-Methods", strings.Join(options, ", "))
-				}
+ 				if sc.config.CORS != nil {
+ 					response.Header().Add("Access-Control-Max-Age", strconv.Itoa(sc.config.CORS.MaxAge))
+ 					response.Header().Add("Access-Control-Allow-Methods", strings.Join(options, ", "))
+ 				}
 				if rq.Method != "OPTIONS" {
 					h.writeStatus(http.StatusMethodNotAllowed, "")
 				} else {
