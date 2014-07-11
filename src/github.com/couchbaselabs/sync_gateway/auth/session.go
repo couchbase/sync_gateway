e0d21276d79a3b35e9e4c2f448f1ddcbcdafccb9
Andrew Reslan <andrew@couchbase.com>
2014-07-11 19:15:56 +0100

    Improved readability of cookie expiration tetsts

--- src/github.com/couchbaselabs/sync_gateway/auth/session.go
+++ src/github.com/couchbaselabs/sync_gateway/auth/session.go
@@ -42,7 +42,9 @@ func (auth *Authenticator) AuthenticateCookie(rq *http.Request, response http.Re
 	}
 	// Don't need to check session.Expiration, because Couchbase will have nuked the document.
         //update the session Expiration if 10% or more of the current expiration time has elapsed
-        if(int((time.Now().Add(session.Ttl).Sub(session.Expiration)).Seconds()) > int(session.Ttl.Seconds())/10) {
+        sessionPercentElapsed := int((time.Now().Add(session.Ttl).Sub(session.Expiration)).Seconds())
+	tenPercentOfTtl := int(session.Ttl.Seconds())/10
+        if(sessionPercentElapsed > tenPercentOfTtl) {
         	session.Expiration = time.Now().Add(session.Ttl)
         	ttlSec := int(session.Ttl.Seconds())
 		if err = auth.bucket.Set(docIDForSession(session.ID), ttlSec, session); err != nil {
