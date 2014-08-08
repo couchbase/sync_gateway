13b9a3274daba33c9f0b6d30087271cc16b9e551
Jens Alfke <jens@couchbase.com>
2014-08-08 15:10:17 -0700

    Copied in current go.sh from master, for Go 1.3 compatibility

--- go.sh
+++ go.sh
@@ -2,11 +2,11 @@
 
 # This script runs the given Go subcommand with GOPATH set up correctly for sync_gateway.
 
-if [[ `go version` != *go1.2* ]]
-then
-    echo "*** Go 1.2 is required to build Sync Gateway; you have" `go version`
-    echo "Please visit http://golang.org/doc/install or use your package manager to upgrade."
-    exit 1
+GO_MAJOR_MINOR_VERSION=`go version | sed -E 's/.*go([0-9]\.[0-9]+).*/\1/'`
+if [[ $(echo "$GO_MAJOR_MINOR_VERSION >= 1.2" | bc) -eq 0 ]]; then
+  echo "*** Go 1.2 or higher is required to build Sync Gateway; you have" `go version`
+  echo "Please visit http://golang.org/doc/install or use your package manager to upgrade."
+  exit 1
 fi
 
 export GOPATH="`pwd`:`pwd`/vendor"
