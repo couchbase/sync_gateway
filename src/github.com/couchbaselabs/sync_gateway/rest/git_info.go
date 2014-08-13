9b862bc5197b661c3dc4ae02cbe051c061cb5f23
Andrew Reslan <andrew@couchbase.com>
2014-08-13 17:05:15 +0100

    remove git_info.go from tracking so that local build updates are not committed

--- src/github.com/couchbaselabs/sync_gateway/rest/git_info.go
+++
@@ -1,6 +0,0 @@
-package rest
-
-// The git commit that was compiled. This will be filled in by the compiler.
-const GitCommit = "@GIT_COMMIT@"
-const GitBranch = "@GIT_BRANCH@"
-const GitDirty = "@GIT_DIRTY@"
