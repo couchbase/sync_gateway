couchbase/sync_gateway

commit da9037da8a0a83890eb2b3b9c9cf826d418c4eb7

Fix infinite loops in changes feed by reordering new channels' items
When a user gets access to a channel at sequence m, the pre-existing
items in that channel now appear in the feed right before sequence m,
with sequence IDs of the form "m:n" where n is the actual sequence # of
the item. This allows the changes feed to pick up where it left off,
avoiding infinite loops.
It does mean that public sequence IDs can now be strings, not just
numbers. But clients should not mind, since the IDs are opaque to them.
Fixes #314 (again!)

src/github.com/couchbaselabs/sync_gateway/db/changes.go
@@ -11,7 +11,6 @@ package db
 
 import (
 	"encoding/json"
-	"math"
 
 	"github.com/couchbaselabs/sync_gateway/base"
 	"github.com/couchbaselabs/sync_gateway/channels"
@@ -19,19 +18,19 @@ import (
 
 // Options for changes-feeds
 type ChangesOptions struct {
-	Since       uint64    // sequence # to start _after_
-	Limit       int       // Max number of changes to return, if nonzero
-	Conflicts   bool      // Show all conflicting revision IDs, not just winning one?
-	IncludeDocs bool      // Include doc body of each change?
-	Wait        bool      // Wait for results, instead of immediately returning empty result?
-	Continuous  bool      // Run continuously until terminated?
-	Terminator  chan bool // Caller can close this channel to terminate the feed
+	Since       SequenceID // sequence # to start _after_
+	Limit       int        // Max number of changes to return, if nonzero
+	Conflicts   bool       // Show all conflicting revision IDs, not just winning one?
+	IncludeDocs bool       // Include doc body of each change?
+	Wait        bool       // Wait for results, instead of immediately returning empty result?
+	Continuous  bool       // Run continuously until terminated?
+	Terminator  chan bool  // Caller can close this channel to terminate the feed
 }
 
 // A changes entry; Database.GetChanges returns an array of these.
 // Marshals into the standard CouchDB _changes format.
 type ChangeEntry struct {
-	Seq      uint64      `json:"seq"`
+	Seq      SequenceID  `json:"seq"`
 	ID       string      `json:"id"`
 	Deleted  bool        `json:"deleted,omitempty"`
 	Removed  base.Set    `json:"removed,omitempty"`
@@ -107,8 +106,15 @@ func (db *Database) changesFeed(channel string, options ChangesOptions) (<-chan 
 				// we won't emit the doc at all because we already stopped emitting entries
 				// from the view before this point.
 			}
+			if logEntry.Sequence >= options.Since.TriggeredBy {
+				options.Since.TriggeredBy = 0
+			}
+			seqID := SequenceID{
+				Seq:         logEntry.Sequence,
+				TriggeredBy: options.Since.TriggeredBy,
+			}
 			change := ChangeEntry{
-				Seq:      logEntry.Sequence,
+				Seq:      seqID,
 				ID:       logEntry.DocID,
 				Deleted:  (logEntry.Flags & channels.Deleted) != 0,
 				Changes:  []ChangeRev{{"rev": logEntry.RevID}},
@@ -173,8 +179,9 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 			names := make([]string, 0, len(channelsSince))
 			for name, seqAddedAt := range channelsSince {
 				chanOpts := options
-				if seqAddedAt > options.Since {
-					chanOpts.Since = 0 // Newly added channel so send all of it to user
+				if seqAddedAt > 1 && options.Since.Before(SequenceID{Seq: seqAddedAt}) {
+					// Newly added channel so send all of it to user:
+					chanOpts.Since = SequenceID{Seq: 0, TriggeredBy: seqAddedAt}
 				}
 				feed, err := db.changesFeed(name, chanOpts)
 				if err != nil {
@@ -186,17 +193,20 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 			}
 
 			// If the user object has changed, create a special pseudo-feed for it:
-			if db.user != nil && db.user.Sequence() > options.Since {
-				entry := ChangeEntry{
-					Seq:     db.user.Sequence(),
-					ID:      "_user/" + db.user.Name(),
-					Changes: []ChangeRev{},
+			if db.user != nil {
+				userSeq := SequenceID{Seq: db.user.Sequence()}
+				if options.Since.Before(userSeq) {
+					entry := ChangeEntry{
+						Seq:     userSeq,
+						ID:      "_user/" + db.user.Name(),
+						Changes: []ChangeRev{},
+					}
+					userFeed := make(chan *ChangeEntry, 1)
+					userFeed <- &entry
+					close(userFeed)
+					feeds = append(feeds, userFeed)
+					names = append(names, entry.ID)
 				}
-				userFeed := make(chan *ChangeEntry, 1)
-				userFeed <- &entry
-				close(userFeed)
-				feeds = append(feeds, userFeed)
-				names = append(names, entry.ID)
 			}
 
 			current := make([]*ChangeEntry, len(feeds))
@@ -217,10 +227,10 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 				}
 
 				// Find the current entry with the minimum sequence:
-				var minSeq uint64 = math.MaxUint64
+				minSeq := MaxSequenceID
 				var minEntry *ChangeEntry
 				for _, cur := range current {
-					if cur != nil && cur.Seq < minSeq {
+					if cur != nil && cur.Seq.Before(minSeq) {
 						minSeq = cur.Seq
 						minEntry = cur
 					}
@@ -233,7 +243,7 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 				for i, cur := range current {
 					if cur != nil && cur.Seq == minSeq {
 						current[i] = nil
-						options.Since = minSeq
+						//options.Since = minSeq //TEMP ???
 						// Also concatenate the matching entries' Removed arrays:
 						if cur != minEntry && cur.Removed != nil {
 							if minEntry.Removed == nil {
@@ -243,6 +253,10 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 							}
 						}
 					}
+				}
+
+				if !options.Since.Before(minSeq) {
+					continue // out of order; skip it
 				}
 
 				// Add the doc body or the conflicting rev IDs, if those options are set:
@@ -319,7 +333,7 @@ func (db *Database) GetChanges(channels base.Set, options ChangesOptions) ([]*Ch
 }
 
 func (db *Database) GetChangeLog(channelName string, afterSeq uint64) []*LogEntry {
-	options := ChangesOptions{Since: afterSeq}
+	options := ChangesOptions{Since: SequenceID{Seq: afterSeq}}
 	_, log := db.changeCache.getChannelCache(channelName).getCachedChanges(options)
 	return log
 }

src/github.com/couchbaselabs/sync_gateway/db/changes_view.go
@@ -31,7 +31,7 @@ func (dbc *DatabaseContext) getChangesInChannelFromView(
 	start := time.Now()
 	// Query the view:
 	optMap := changesViewOptions(channelName, endSeq, options)
-	base.LogTo("Cache", "  Querying 'channels' view for %q (start=#%d, end=#%d, limit=%d)", channelName, options.Since+1, endSeq, options.Limit)
+	base.LogTo("Cache", "  Querying 'channels' view for %q (start=#%d, end=#%d, limit=%d)", channelName, options.Since.Seq+1, endSeq, options.Limit)
 	vres := channelsViewResult{}
 	err := dbc.Bucket.ViewCustom("sync_gateway", "channels", optMap, &vres)
 	if err != nil {
@@ -73,7 +73,7 @@ func changesViewOptions(channelName string, endSeq uint64, options ChangesOption
 	}
 	optMap := Body{
 		"stale":    false,
-		"startkey": []interface{}{channelName, options.Since + 1},
+		"startkey": []interface{}{channelName, options.Since.Seq + 1},
 		"endkey":   endKey,
 	}
 	if options.Limit > 0 {

src/github.com/couchbaselabs/sync_gateway/db/channel_cache.go
@@ -78,7 +78,7 @@ func (c *channelCache) _getCachedChanges(options ChangesOptions) (validFrom uint
 		return // Return nil if nothing is cached
 	}
 	var start int
-	for start = len(log) - 1; start >= 0 && log[start].Sequence > options.Since; start-- {
+	for start = len(log) - 1; start >= 0 && log[start].Sequence > options.Since.Seq; start-- {
 	}
 	start++
 
@@ -114,7 +114,8 @@ func (c *channelCache) GetChanges(options ChangesOptions) ([]*LogEntry, error) {
 		base.LogTo("Cache", "getCachedChanges(%q, %d) --> nothing cached",
 			c.channelName, options.Since)
 	}
-	if cacheValidFrom <= options.Since+1 {
+	startSeq := options.Since.Seq + 1
+	if cacheValidFrom <= startSeq {
 		return resultFromCache, nil
 	}
 
@@ -130,7 +131,7 @@ func (c *channelCache) GetChanges(options ChangesOptions) ([]*LogEntry, error) {
 		base.LogTo("Cache", "2nd getCachedChanges(%q, %d) got %d more, valid from #%d!",
 			c.channelName, options.Since, len(resultFromCache)-numFromCache, cacheValidFrom)
 	}
-	if cacheValidFrom <= options.Since+1 {
+	if cacheValidFrom <= startSeq {
 		return resultFromCache, nil
 	}
 
@@ -144,7 +145,7 @@ func (c *channelCache) GetChanges(options ChangesOptions) ([]*LogEntry, error) {
 
 	// Cache some of the view results, if there's room in the cache:
 	if len(resultFromCache) < ChannelCacheMaxLength {
-		c.prependChanges(resultFromView, options.Since+1, options.Limit == 0)
+		c.prependChanges(resultFromView, startSeq, options.Limit == 0)
 	}
 
 	result := resultFromView

src/github.com/couchbaselabs/sync_gateway/db/database_test.go
@@ -267,7 +267,7 @@ func TestAllDocs(t *testing.T) {
 		if i >= 23 {
 			seq++
 		}
-		assert.Equals(t, change.Seq, uint64(seq))
+		assert.Equals(t, change.Seq, SequenceID{Seq: uint64(seq)})
 		assert.Equals(t, change.Deleted, i == 99)
 		var removed base.Set
 		if i == 99 {
@@ -281,7 +281,7 @@ func TestAllDocs(t *testing.T) {
 	assertNoError(t, err, "Couldn't GetChanges")
 	assert.Equals(t, len(changes), 10)
 	for i, change := range changes {
-		assert.Equals(t, change.Seq, uint64(10*i+1))
+		assert.Equals(t, change.Seq, SequenceID{Seq: uint64(10*i + 1)})
 		assert.Equals(t, change.ID, ids[10*i].DocID)
 		assert.Equals(t, change.Deleted, false)
 		assert.DeepEquals(t, change.Removed, base.Set(nil))
@@ -316,16 +316,16 @@ func TestChangesAfterChannelAdded(t *testing.T) {
 	// Check the _changes feed:
 	db.changeCache.waitForSequence(1)
 	db.user, _ = authenticator.GetUser("naomi")
-	changes, err := db.GetChanges(base.SetOf("*"), ChangesOptions{Since: 1})
+	changes, err := db.GetChanges(base.SetOf("*"), ChangesOptions{Since: SequenceID{Seq: 1}})
 	assertNoError(t, err, "Couldn't GetChanges")
 
 	assert.Equals(t, len(changes), 2)
 	assert.DeepEquals(t, changes[0], &ChangeEntry{
-		Seq:     1,
+		Seq:     SequenceID{Seq: 1, TriggeredBy: 2},
 		ID:      "doc1",
 		Changes: []ChangeRev{{"rev": revid}}})
 	assert.DeepEquals(t, changes[1], &ChangeEntry{
-		Seq:     2,
+		Seq:     SequenceID{Seq: 2},
 		ID:      "_user/naomi",
 		Changes: []ChangeRev{}})
 
@@ -335,12 +335,12 @@ func TestChangesAfterChannelAdded(t *testing.T) {
 	// Check the _changes feed -- this is to make sure the changeCache properly received
 	// sequence 2 (the user doc) and isn't stuck waiting for it.
 	db.changeCache.waitForSequence(3)
-	changes, err = db.GetChanges(base.SetOf("*"), ChangesOptions{Since: 2})
+	changes, err = db.GetChanges(base.SetOf("*"), ChangesOptions{Since: SequenceID{Seq: 2}})
 	assertNoError(t, err, "Couldn't GetChanges (2nd)")
 
 	assert.Equals(t, len(changes), 1)
 	assert.DeepEquals(t, changes[0], &ChangeEntry{
-		Seq:     3,
+		Seq:     SequenceID{Seq: 3},
 		ID:      "doc2",
 		Changes: []ChangeRev{{"rev": revid}}})
 }
@@ -403,7 +403,7 @@ func TestConflicts(t *testing.T) {
 	assertNoError(t, err, "Couldn't GetChanges")
 	assert.Equals(t, len(changes), 1)
 	assert.DeepEquals(t, changes[0], &ChangeEntry{
-		Seq:      3,
+		Seq:      SequenceID{Seq: 3},
 		ID:       "doc",
 		Changes:  []ChangeRev{{"rev": "2-b"}, {"rev": "2-a"}},
 		branched: true})
@@ -428,7 +428,7 @@ func TestConflicts(t *testing.T) {
 	assertNoError(t, err, "Couldn't GetChanges")
 	assert.Equals(t, len(changes), 1)
 	assert.DeepEquals(t, changes[0], &ChangeEntry{
-		Seq:      4,
+		Seq:      SequenceID{Seq: 4},
 		ID:       "doc",
 		Changes:  []ChangeRev{{"rev": "2-a"}, {"rev": rev3}},
 		branched: true})

src/github.com/couchbaselabs/sync_gateway/db/sequence_id.go
@@ -1,0 +1,87 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/couchbaselabs/sync_gateway/base"
+)
+
+// A change sequence as reported externally in a _changes feed.
+// Most of the time the TriggerSeq is 0, but if a revision is being sent retroactively because
+// the user got access to a channel, the TriggerSeq will be equal to the sequence of the change
+// that gave the user access.
+type SequenceID struct {
+	TriggeredBy uint64 // The sequence # that triggered this (0 if none)
+	Seq         uint64 // The actual internal sequence
+}
+
+var MaxSequenceID = SequenceID{Seq: math.MaxUint64}
+
+func (s SequenceID) String() string {
+	if s.TriggeredBy > 0 {
+		return fmt.Sprintf("%d:%d", s.TriggeredBy, s.Seq)
+	} else {
+		return strconv.FormatUint(s.Seq, 10)
+	}
+}
+
+func ParseSequenceID(str string) (s SequenceID, err error) {
+	if str == "" {
+		return SequenceID{}, nil
+	}
+	components := strings.Split(str, ":")
+	if len(components) == 1 {
+		s.Seq, err = strconv.ParseUint(str, 10, 64)
+	} else if len(components) != 2 {
+		err = base.HTTPErrorf(400, "Invalid sequence")
+	} else {
+		s.TriggeredBy, err = strconv.ParseUint(components[0], 10, 64)
+		if err == nil {
+			s.Seq, err = strconv.ParseUint(components[1], 10, 64)
+		}
+	}
+	if err != nil {
+		err = base.HTTPErrorf(400, "Invalid sequence")
+	}
+	return
+}
+
+func (s SequenceID) MarshalJSON() ([]byte, error) {
+	if s.TriggeredBy > 0 {
+		return []byte(fmt.Sprintf("\"%d:%d\"", s.TriggeredBy, s.Seq)), nil
+	} else {
+		return []byte(strconv.FormatUint(s.Seq, 10)), nil
+	}
+}
+
+func (s *SequenceID) UnmarshalJSON(data []byte) error {
+	if len(data) > 0 && data[0] == '"' {
+		var raw string
+		err := json.Unmarshal(data, &raw)
+		if err == nil {
+			*s, err = ParseSequenceID(string(raw))
+		}
+		return err
+	} else {
+		s.TriggeredBy = 0
+		return json.Unmarshal(data, &s.Seq)
+	}
+}
+
+// The most significant value is TriggeredBy, unless it's zero, in which case use Seq.
+// The tricky part is that "n" sorts after "n:m" for any nonzero m
+func (s SequenceID) Before(s2 SequenceID) bool {
+	if s.TriggeredBy == s2.TriggeredBy {
+		return s.Seq < s2.Seq // the simple case: untriggered, or triggered by same sequence
+	} else if s.TriggeredBy == 0 {
+		return s.Seq < s2.TriggeredBy // s2 triggered but not s
+	} else if s2.TriggeredBy == 0 {
+		return s.TriggeredBy <= s2.Seq // s triggered but not s2
+	} else {
+		return s.TriggeredBy < s2.TriggeredBy // both triggered, but by different sequences
+	}
+}

src/github.com/couchbaselabs/sync_gateway/db/sequence_id_test.go
@@ -1,0 +1,77 @@
+package db
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/couchbaselabs/go.assert"
+)
+
+func TestParseSequenceID(t *testing.T) {
+	s, err := ParseSequenceID("1234")
+	assertNoError(t, err, "ParseSequenceID")
+	assert.Equals(t, s, SequenceID{Seq: 1234})
+
+	s, err = ParseSequenceID("5678:1234")
+	assertNoError(t, err, "ParseSequenceID")
+	assert.Equals(t, s, SequenceID{Seq: 1234, TriggeredBy: 5678})
+
+	s, err = ParseSequenceID("")
+	assertNoError(t, err, "ParseSequenceID")
+	assert.Equals(t, s, SequenceID{Seq: 0, TriggeredBy: 0})
+
+	s, err = ParseSequenceID("foo")
+	assert.True(t, err != nil)
+	s, err = ParseSequenceID(":")
+	assert.True(t, err != nil)
+	s, err = ParseSequenceID("123:456:789")
+	assert.True(t, err != nil)
+	s, err = ParseSequenceID(":1")
+	assert.True(t, err != nil)
+	s, err = ParseSequenceID("123:ggg")
+	assert.True(t, err != nil)
+}
+
+func TestMarshalSequenceID(t *testing.T) {
+	s := SequenceID{Seq: 1234}
+	assert.Equals(t, s.String(), "1234")
+	asJson, err := json.Marshal(s)
+	assertNoError(t, err, "Marshal failed")
+	assert.Equals(t, string(asJson), "1234")
+
+	var s2 SequenceID
+	err = json.Unmarshal(asJson, &s2)
+	assertNoError(t, err, "Unmarshal failed")
+	assert.Equals(t, s2, s)
+}
+
+func TestMarshalTriggeredSequenceID(t *testing.T) {
+	s := SequenceID{TriggeredBy: 5678, Seq: 1234}
+	assert.Equals(t, s.String(), "5678:1234")
+	asJson, err := json.Marshal(s)
+	assertNoError(t, err, "Marshal failed")
+	assert.Equals(t, string(asJson), "\"5678:1234\"")
+
+	var s2 SequenceID
+	err = json.Unmarshal(asJson, &s2)
+	assertNoError(t, err, "Unmarshal failed")
+	assert.Equals(t, s2, s)
+}
+
+func TestCompareSequenceIDs(t *testing.T) {
+	orderedSeqs := []SequenceID{
+		SequenceID{Seq: 1234},
+		SequenceID{Seq: 5677},
+		SequenceID{TriggeredBy: 5678, Seq: 1234},
+		SequenceID{TriggeredBy: 5678, Seq: 2222},
+		SequenceID{Seq: 5678}, // 5678 comes after the sequences it triggered
+		SequenceID{TriggeredBy: 6666, Seq: 5678},
+		SequenceID{Seq: 6666},
+	}
+
+	for i := 0; i < len(orderedSeqs); i++ {
+		for j := 0; j < len(orderedSeqs); j++ {
+			assert.Equals(t, orderedSeqs[i].Before(orderedSeqs[j]), i < j)
+		}
+	}
+}

src/github.com/couchbaselabs/sync_gateway/rest/api_test.go
@@ -555,13 +555,13 @@ func TestLogin(t *testing.T) {
 }
 
 func TestReadChangesOptionsFromJSON(t *testing.T) {
-	optStr := `{"feed":"longpoll", "since": 123456, "limit":123, "style": "all_docs",
+	optStr := `{"feed":"longpoll", "since": "123456:78", "limit":123, "style": "all_docs",
 				"include_docs": true, "filter": "Melitta", "channels": "ABC,BBC"}`
 	feed, options, filter, channelsArray, err := readChangesOptionsFromJSON([]byte(optStr))
 	assert.Equals(t, err, nil)
 	assert.Equals(t, feed, "longpoll")
 	assert.DeepEquals(t, options, db.ChangesOptions{
-		Since:       123456,
+		Since:       db.SequenceID{Seq: 78, TriggeredBy: 123456},
 		Limit:       123,
 		Conflicts:   true,
 		IncludeDocs: true})
@@ -661,21 +661,21 @@ func TestChannelAccessChanges(t *testing.T) {
 	a.Save(zegpold)
 
 	// Create some docs that give users access:
-	response := rt.send(request("PUT", "/db/alpha", `{"owner":"alice"}`))
+	response := rt.send(request("PUT", "/db/alpha", `{"owner":"alice"}`)) // seq=1
 	assertStatus(t, response, 201)
 	var body db.Body
 	json.Unmarshal(response.Body.Bytes(), &body)
 	assert.Equals(t, body["ok"], true)
 	alphaRevID := body["rev"].(string)
 
-	assertStatus(t, rt.send(request("PUT", "/db/beta", `{"owner":"boadecia"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/delta", `{"owner":"alice"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/gamma", `{"owner":"zegpold"}`)), 201)
+	assertStatus(t, rt.send(request("PUT", "/db/beta", `{"owner":"boadecia"}`)), 201) // seq=2
+	assertStatus(t, rt.send(request("PUT", "/db/delta", `{"owner":"alice"}`)), 201)   // seq=3
+	assertStatus(t, rt.send(request("PUT", "/db/gamma", `{"owner":"zegpold"}`)), 201) // seq=4
 
-	assertStatus(t, rt.send(request("PUT", "/db/a1", `{"channel":"alpha"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/b1", `{"channel":"beta"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/d1", `{"channel":"delta"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/g1", `{"channel":"gamma"}`)), 201)
+	assertStatus(t, rt.send(request("PUT", "/db/a1", `{"channel":"alpha"}`)), 201) // seq=5
+	assertStatus(t, rt.send(request("PUT", "/db/b1", `{"channel":"beta"}`)), 201)  // seq=6
+	assertStatus(t, rt.send(request("PUT", "/db/d1", `{"channel":"delta"}`)), 201) // seq=7
+	assertStatus(t, rt.send(request("PUT", "/db/g1", `{"channel":"gamma"}`)), 201) // seq=8
 
 	// Check the _changes feed:
 	var changes struct {
@@ -683,10 +683,12 @@ func TestChannelAccessChanges(t *testing.T) {
 	}
 	response = rt.send(requestByUser("GET", "/db/_changes", "", "zegpold"))
 	log.Printf("_changes looks like: %s", response.Body.Bytes())
-	json.Unmarshal(response.Body.Bytes(), &changes)
+	err = json.Unmarshal(response.Body.Bytes(), &changes)
+	assert.Equals(t, err, nil)
 	assert.Equals(t, len(changes.Results), 1)
 	since := changes.Results[0].Seq
-	assert.Equals(t, since, uint64(8))
+	assert.Equals(t, changes.Results[0].ID, "g1")
+	assert.Equals(t, since, db.SequenceID{Seq: 8})
 
 	// Check user access:
 	alice, _ = a.GetUser("alice")
@@ -696,7 +698,7 @@ func TestChannelAccessChanges(t *testing.T) {
 
 	// Update a document to revoke access to alice and grant it to zegpold:
 	str := fmt.Sprintf(`{"owner":"zegpold", "_rev":%q}`, alphaRevID)
-	assertStatus(t, rt.send(request("PUT", "/db/alpha", str)), 201)
+	assertStatus(t, rt.send(request("PUT", "/db/alpha", str)), 201) // seq=9
 
 	// Check user access again:
 	alice, _ = a.GetUser("alice")
@@ -718,12 +720,14 @@ func TestChannelAccessChanges(t *testing.T) {
 	log.Printf("//////// _changes for zegpold looks like: %s", response.Body.Bytes())
 	json.Unmarshal(response.Body.Bytes(), &changes)
 	assert.Equals(t, len(changes.Results), 2)
-	assert.Equals(t, changes.Results[0].ID, "a1")
-	assert.Equals(t, changes.Results[1].ID, "g1")
+	assert.Equals(t, changes.Results[0].ID, "g1")
+	assert.Equals(t, changes.Results[0].Seq, db.SequenceID{Seq: 8})
+	assert.Equals(t, changes.Results[1].ID, "a1")
+	assert.Equals(t, changes.Results[1].Seq, db.SequenceID{Seq: 5, TriggeredBy: 9})
 
 	// Changes feed with since=gamma:8 would ordinarily be empty, but zegpold got access to channel
 	// alpha after sequence 8, so the pre-existing docs in that channel are included:
-	response = rt.send(requestByUser("GET", fmt.Sprintf("/db/_changes?since=%d", since),
+	response = rt.send(requestByUser("GET", fmt.Sprintf("/db/_changes?since=%s", since),
 		"", "zegpold"))
 	log.Printf("_changes looks like: %s", response.Body.Bytes())
 	changes.Results = nil
@@ -782,17 +786,17 @@ func TestRoleAccessChanges(t *testing.T) {
 
 	// Create some docs in the channels:
 	response := rt.send(request("PUT", "/db/fashion",
-		`{"user":"alice","role":["role:hipster","role:bogus"]}`))
+		`{"user":"alice","role":["role:hipster","role:bogus"]}`)) // seq=1
 	assertStatus(t, response, 201)
 	var body db.Body
 	json.Unmarshal(response.Body.Bytes(), &body)
 	assert.Equals(t, body["ok"], true)
 	fashionRevID := body["rev"].(string)
 
-	assertStatus(t, rt.send(request("PUT", "/db/g1", `{"channel":"gamma"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/a1", `{"channel":"alpha"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/b1", `{"channel":"beta"}`)), 201)
-	assertStatus(t, rt.send(request("PUT", "/db/d1", `{"channel":"delta"}`)), 201)
+	assertStatus(t, rt.send(request("PUT", "/db/g1", `{"channel":"gamma"}`)), 201) // seq=2
+	assertStatus(t, rt.send(request("PUT", "/db/a1", `{"channel":"alpha"}`)), 201) // seq=3
+	assertStatus(t, rt.send(request("PUT", "/db/b1", `{"channel":"beta"}`)), 201)  // seq=4
+	assertStatus(t, rt.send(request("PUT", "/db/d1", `{"channel":"delta"}`)), 201) // seq=5
 
 	// Check user access:
 	alice, _ = a.GetUser("alice")
@@ -808,22 +812,22 @@ func TestRoleAccessChanges(t *testing.T) {
 		Last_Seq interface{}
 	}
 	response = rt.send(requestByUser("GET", "/db/_changes", "", "alice"))
-	log.Printf("_changes looks like: %s", response.Body.Bytes())
+	log.Printf("1st _changes looks like: %s", response.Body.Bytes())
 	json.Unmarshal(response.Body.Bytes(), &changes)
 	assert.Equals(t, len(changes.Results), 2)
 	since := changes.Last_Seq
-	assert.Equals(t, since, float64(3))
+	assert.Equals(t, since, "3")
 
 	response = rt.send(requestByUser("GET", "/db/_changes", "", "zegpold"))
-	log.Printf("_changes looks like: %s", response.Body.Bytes())
+	log.Printf("2nd _changes looks like: %s", response.Body.Bytes())
 	json.Unmarshal(response.Body.Bytes(), &changes)
 	assert.Equals(t, len(changes.Results), 1)
 	since = changes.Last_Seq
-	assert.Equals(t, since, float64(4))
+	assert.Equals(t, since, "4")
 
 	// Update "fashion" doc to grant zegpold the role "hipster" and take it away from alice:
 	str := fmt.Sprintf(`{"user":"zegpold", "role":"role:hipster", "_rev":%q}`, fashionRevID)
-	assertStatus(t, rt.send(request("PUT", "/db/fashion", str)), 201)
+	assertStatus(t, rt.send(request("PUT", "/db/fashion", str)), 201) // seq=6
 
 	// Check user access again:
 	alice, _ = a.GetUser("alice")
@@ -834,19 +838,19 @@ func TestRoleAccessChanges(t *testing.T) {
 	// The complete _changes feed for zegpold contains docs g1 and b1:
 	changes.Results = nil
 	response = rt.send(requestByUser("GET", "/db/_changes", "", "zegpold"))
-	log.Printf("_changes looks like: %s", response.Body.Bytes())
+	log.Printf("3rd _changes looks like: %s", response.Body.Bytes())
 	json.Unmarshal(response.Body.Bytes(), &changes)
 	assert.Equals(t, len(changes.Results), 2)
-	assert.Equals(t, changes.Last_Seq, float64(4))
-	assert.Equals(t, changes.Results[0].ID, "g1")
-	assert.Equals(t, changes.Results[1].ID, "b1")
+	assert.Equals(t, changes.Last_Seq, "6:2")
+	assert.Equals(t, changes.Results[0].ID, "b1")
+	assert.Equals(t, changes.Results[1].ID, "g1")
 
 	// Changes feed with since=4 would ordinarily be empty, but zegpold got access to channel
 	// gamma after sequence 4, so the pre-existing docs in that channel are included:
 	base.LogKeys["Changes"] = true
 	base.LogKeys["Cache"] = true
 	response = rt.send(requestByUser("GET", "/db/_changes?since=4", "", "zegpold"))
-	log.Printf("_changes looks like: %s", response.Body.Bytes())
+	log.Printf("4th _changes looks like: %s", response.Body.Bytes())
 	changes.Results = nil
 	json.Unmarshal(response.Body.Bytes(), &changes)
 	assert.Equals(t, len(changes.Results), 1)
@@ -879,7 +883,7 @@ func TestDocDeletionFromChannel(t *testing.T) {
 	json.Unmarshal(response.Body.Bytes(), &changes)
 	assert.Equals(t, len(changes.Results), 1)
 	since := changes.Results[0].Seq
-	assert.Equals(t, since, uint64(1))
+	assert.Equals(t, since, db.SequenceID{Seq: 1})
 
 	assert.Equals(t, changes.Results[0].ID, "alpha")
 	rev1 := changes.Results[0].Changes[0]["rev"]
@@ -889,7 +893,7 @@ func TestDocDeletionFromChannel(t *testing.T) {
 
 	// Get the updates from the _changes feed:
 	time.Sleep(100 * time.Millisecond)
-	response = rt.send(requestByUser("GET", fmt.Sprintf("/db/_changes?since=%d", since),
+	response = rt.send(requestByUser("GET", fmt.Sprintf("/db/_changes?since=%s", since),
 		"", "alice"))
 	log.Printf("_changes looks like: %s", response.Body.Bytes())
 	changes.Results = nil

commit e685d58aa20a2849018bc152614ac646f95ed4ba

Fixed performance regression (and incorrect _changes feeds)
Regression was in commit da9037da. options.Since wasn't being updated
during the MultiChangesFeed inner loop, so in a continuous changes feed,
every time the database changed it would re-emit all the changes since
the original Since parameter, not just the new ones. This created a
huge amount of unnecessary output, consuming a lot of CPU and I/O and
causing the gateway to fall behind in processing data.
Fixes #406

src/github.com/couchbaselabs/sync_gateway/db/changes.go
@@ -253,7 +253,6 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 				for i, cur := range current {
 					if cur != nil && cur.Seq == minSeq {
 						current[i] = nil
-						//options.Since = minSeq //TEMP ???
 						// Also concatenate the matching entries' Removed arrays:
 						if cur != minEntry && cur.Removed != nil {
 							if minEntry.Removed == nil {
@@ -268,6 +267,8 @@ func (db *Database) MultiChangesFeed(chans base.Set, options ChangesOptions) (<-
 				if !options.Since.Before(minSeq) {
 					continue // out of order; skip it
 				}
+				// Remember we got this far, for the next outer loop iteration:
+				options.Since = minSeq
 
 				// Add the doc body or the conflicting rev IDs, if those options are set:
 				if options.IncludeDocs || options.Conflicts {