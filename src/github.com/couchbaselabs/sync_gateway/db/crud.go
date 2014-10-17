4b57dcbd084c7a5e54842530dcea3943a81adac2
Andrew Reslan <andrew@couchbase.com>
2014-10-17 18:10:03 +0100

    Based on feedback from Jens moved the immutable copy into the loadBodyAttachments method and changed the signature to return the copy

--- src/github.com/couchbaselabs/sync_gateway/db/crud.go
+++ src/github.com/couchbaselabs/sync_gateway/db/crud.go
@@ -145,7 +145,6 @@ func (db *Database) GetRev(docid, revid string, listRevisions bool, attachmentsS
 		body["_revisions"] = revisions
 	}
 
-	body = body.ImmutableAttachmentsCopy()
 	// Add attachment bodies:
 	if attachmentsSince != nil && len(BodyAttachments(body)) > 0 {
 		minRevpos := 1
@@ -161,7 +160,7 @@ func (db *Database) GetRev(docid, revid string, listRevisions bool, attachmentsS
 				minRevpos++
 			}
 		}
-		err = db.loadBodyAttachments(body, minRevpos)
+		body, err = db.loadBodyAttachments(body, minRevpos)
 		if err != nil {
 			return nil, err
 		}
