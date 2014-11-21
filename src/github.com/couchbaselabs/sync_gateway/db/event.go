348977dd8a11164ed782d26abfde22c48d88c9b7
Adam Fraser <adamf@couchbase.com>
2014-11-21 09:32:24 -0800

    Add webhook support
    Initial support for webhooks.  Implementation is built to support additional events and event handlers in future.  Currently only one event is being raised - a DocumentCommitEvent, after a doc is successfully updated in updateDoc.
    
    EventManager maintains a channel (eventChannel) for raised events.  The only synchronous activity on the main document update go routine is to add the event to the EventManager channel.
    
    EventManager has a go routine that works the eventChannel.  For each event, it passes the event to each registered eventHandler.  Each eventHandler creates a go routine for their own processing (for webhook, it's making an HTTP POST).
    
    eventHandlers are defined in config.  Currently the only supported eventHandler type is webhook.

---
+++ src/github.com/couchbaselabs/sync_gateway/db/event.go
@@ -0,0 +1,159 @@
+package db
+
+import (
+	"errors"
+	"github.com/couchbaselabs/sync_gateway/base"
+	"github.com/couchbaselabs/walrus"
+	"github.com/robertkrimen/otto"
+	"strconv"
+)
+
+// Event type
+type EventType uint8
+
+const (
+	DocumentChange EventType = iota
+	UserAdd
+)
+
+// An event that can be raised during SG processing.
+type Event interface {
+	Synchronous() bool
+	EventType() EventType
+}
+
+type EventImpl struct {
+	eventType EventType
+}
+
+func (e *EventImpl) EventType() EventType {
+	return e.eventType
+}
+
+// Currently the AsyncEvent type only manages the Synchronous() check.  Future enhancements
+// around async processing would leverage this type.
+type AsyncEvent struct {
+	EventImpl
+}
+
+func (ae *AsyncEvent) Synchronous() bool {
+	return false
+}
+
+// DocumentChangeEvent is raised when a document has been successfully written to the backing
+// data store.  Event has the document body and channel set as properties.
+type DocumentChangeEvent struct {
+	AsyncEvent
+	Doc      Body
+	Channels base.Set
+}
+
+// Javascript function handling for events
+const kTaskCacheSize = 4
+
+type ResponseType uint8
+
+const (
+	StringResponse ResponseType = iota
+	JSObjectResponse
+)
+
+// A compiled JavaScript event function.
+type jsEventTask struct {
+	walrus.JSRunner
+	responseType ResponseType
+}
+
+// Compiles a JavaScript event function to a jsEventTask object.
+func newJsEventTask(funcSource string) (walrus.JSServerTask, error) {
+	eventTask := &jsEventTask{}
+	err := eventTask.Init(funcSource)
+	if err != nil {
+		return nil, err
+	}
+
+	eventTask.After = func(result otto.Value, err error) (interface{}, error) {
+		nativeValue, _ := result.Export()
+		/*
+			switch nativeValue := nativeValue.(type) {
+			case string:
+				stringResult = nativeValue
+				eventTask.responseType = StringResponse
+			case interface{}:
+				resultBytes, marshErr := json.Marshal(nativeValue)
+				if marshErr != nil {
+					err = marshErr
+				} else {
+					stringResult = string(resultBytes)
+					eventTask.responseType = JSObjectResponse
+				}
+			}
+		*/
+		return nativeValue, err
+	}
+
+	return eventTask, nil
+}
+
+//////// JSEventFunction
+
+// A thread-safe wrapper around a jsEventTask, i.e. an event function.
+type JSEventFunction struct {
+	*walrus.JSServer
+}
+
+func NewJSEventFunction(fnSource string) *JSEventFunction {
+
+	base.LogTo("Events", "Creating new JSEventFunction")
+	return &JSEventFunction{
+		JSServer: walrus.NewJSServer(fnSource, kTaskCacheSize,
+			func(fnSource string) (walrus.JSServerTask, error) {
+				return newJsEventTask(fnSource)
+			}),
+	}
+}
+
+// Calls a jsEventFunction returning an interface{}
+func (ef *JSEventFunction) CallFunction(event Event) (interface{}, error) {
+
+	var err error
+	var result interface{}
+
+	// Different events send different parameters
+	switch event := event.(type) {
+
+	case *DocumentChangeEvent:
+		result, err = ef.Call(event.Doc)
+	}
+
+	if err != nil {
+		base.Warn("Error calling function - function processing aborted: %v", err)
+		return "", err
+	}
+
+	return result, err
+}
+
+// Calls a jsEventFunction returning bool.
+func (ef *JSEventFunction) CallValidateFunction(event Event) (bool, error) {
+
+	result, err := ef.CallFunction(event)
+	if err != nil {
+		return false, err
+	}
+
+	switch result := result.(type) {
+	case bool:
+		return result, nil
+	case string:
+		boolResult, err := strconv.ParseBool(result)
+		if err != nil {
+			return false, err
+		}
+		return boolResult, nil
+	default:
+		base.Warn("Event validate function returned non-boolean result %v", result)
+		return false, errors.New("Validate function returned non-boolean value.")
+	}
+
+}
