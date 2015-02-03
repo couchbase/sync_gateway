60b3b7d2a8f4e6e9401915ceec52cdebd8ecc7ff
Adam Fraser <adamf@couchbase.com>
2015-02-03 15:53:28 -0800

    Improvements to  feed latency handling
    The Couchbase Server TAP and DCP feeds don't guarantee sequential delivery of updates.  Sync Gateway previously buffered these updates to rebuild the changes in sequence order (needed for replication). When an entry on the feed was very slow, it led to several problems:
    * After a timeout period, that sequence was skipped.  Even if it later appeared in the feed, clients wouldn't know to retrieve it
    * If the timeout is extended (as it recently was to 60s), the changes feed will go silent until that timeout is reached
    
    This commit implements an alternate approach:
    * After a shorter timeout, late sequences are skipped, but tracked by the change cache in `skippedSeqs`
    * When there are entries in the skippedSeqs, sequence numbers are sent to the client in the form lowSequence::sequence, where lowSequence is the lowest contiguous sequence for the gateway (skippedSeqs[0] - 1).   Subsequent client requests that include a lowSequence in the since value will retrieve from the lowSequence value.
    * A periodic cleanup function abandons values that have been in the skippedSeqs queue (default time for removal from skipped Seqs is 60 minutes).
    
    For continuous changes feeds, late-arriving (previously skipped) sequences are sent in each iteration of the continuous changes loop.  Each channel cache maintains a history of late-arriving sequences for use by continuous changes feeds to support this functionality.
    
    Includes support for customization of sequence handling settings (pending timeout, skipped timeout, max pending) via the database config.

--- src/github.com/couchbaselabs/sync_gateway/db/sequence_id.go
+++ src/github.com/couchbaselabs/sync_gateway/db/sequence_id.go
@@ -16,13 +16,28 @@ import (
 // that gave the user access.
 type SequenceID struct {
 	TriggeredBy uint64 // The sequence # that triggered this (0 if none)
+	LowSeq      uint64 // Lowest contiguous sequence seen on the feed
 	Seq         uint64 // The actual internal sequence
 }
 
 var MaxSequenceID = SequenceID{Seq: math.MaxUint64}
 
+// Sequence IDs can be in one of the following formats:
+//   Seq                    - simple sequence
+//   TriggeredBy:Seq        - when TriggeredBy is non-zero, LowSeq is zero
+//   LowSeq:TriggeredBy:Seq - when LowSeq is non-zero.
+// When LowSeq is non-zero but TriggeredBy is zero, will appear as LowSeq::Seq.
+// When LowSeq is non-zero but is less than s.Seq (occurs when sending previously skipped sequences), ignore LowSeq.
 func (s SequenceID) String() string {
-	if s.TriggeredBy > 0 {
+
+	if s.LowSeq > 0 && s.LowSeq < s.Seq {
+		if s.TriggeredBy > 0 {
+			return fmt.Sprintf("%d:%d:%d", s.LowSeq, s.TriggeredBy, s.Seq)
+		} else {
+			return fmt.Sprintf("%d::%d", s.LowSeq, s.Seq)
+
+		}
+	} else if s.TriggeredBy > 0 {
 		return fmt.Sprintf("%d:%d", s.TriggeredBy, s.Seq)
 	} else {
 		return strconv.FormatUint(s.Seq, 10)
@@ -35,19 +50,44 @@ func ParseSequenceID(str string) (s SequenceID, err error) {
 	}
 	components := strings.Split(str, ":")
 	if len(components) == 1 {
-		s.Seq, err = strconv.ParseUint(str, 10, 64)
-	} else if len(components) != 2 {
-		err = base.HTTPErrorf(400, "Invalid sequence")
-	} else {
-		s.TriggeredBy, err = strconv.ParseUint(components[0], 10, 64)
-		if err == nil {
-			s.Seq, err = strconv.ParseUint(components[1], 10, 64)
+		// Just the internal sequence
+		s.Seq, err = ParseSequenceComponent(components[0], false)
+	} else if len(components) == 2 {
+		// TriggeredBy and InternalSequence
+		if s.TriggeredBy, err = ParseSequenceComponent(components[0], false); err != nil {
+			return
 		}
+		if s.Seq, err = ParseSequenceComponent(components[1], false); err != nil {
+			return
+		}
+	} else if len(components) == 3 {
+		if s.LowSeq, err = ParseSequenceComponent(components[0], false); err != nil {
+			return
+		}
+		if s.TriggeredBy, err = ParseSequenceComponent(components[1], true); err != nil {
+			return
+		}
+		if s.Seq, err = ParseSequenceComponent(components[2], false); err != nil {
+			return
+		}
+	} else {
+		err = base.HTTPErrorf(400, "Invalid sequence")
 	}
+
 	if err != nil {
 		err = base.HTTPErrorf(400, "Invalid sequence")
 	}
 	return
+}
+
+func ParseSequenceComponent(component string, allowEmpty bool) (uint64, error) {
+	value := uint64(0)
+	if allowEmpty && component == "" {
+		return value, nil
+	}
+	value, err := strconv.ParseUint(component, 10, 64)
+	return value, err
+
 }
 
 func (s SequenceID) MarshalJSON() ([]byte, error) {
@@ -72,15 +112,25 @@ func (s *SequenceID) UnmarshalJSON(data []byte) error {
 	}
 }
 
+func (s SequenceID) SafeSequence() uint64 {
+	if s.LowSeq > 0 {
+		return s.LowSeq
+	} else {
+		return s.Seq
+	}
+}
+
 // The most significant value is TriggeredBy, unless it's zero, in which case use Seq.
 // The tricky part is that "n" sorts after "n:m" for any nonzero m
 func (s SequenceID) Before(s2 SequenceID) bool {
+
+	// using SafeSequence for comparison, which takes the lower of LowSeq and Seq
 	if s.TriggeredBy == s2.TriggeredBy {
-		return s.Seq < s2.Seq // the simple case: untriggered, or triggered by same sequence
+		return s.SafeSequence() < s2.SafeSequence() // the simple case: untriggered, or triggered by same sequence
 	} else if s.TriggeredBy == 0 {
-		return s.Seq < s2.TriggeredBy // s2 triggered but not s
+		return s.SafeSequence() < s2.TriggeredBy // s2 triggered but not s
 	} else if s2.TriggeredBy == 0 {
-		return s.TriggeredBy <= s2.Seq // s triggered but not s2
+		return s.TriggeredBy <= s2.SafeSequence() // s triggered but not s2
 	} else {
 		return s.TriggeredBy < s2.TriggeredBy // both triggered, but by different sequences
 	}
