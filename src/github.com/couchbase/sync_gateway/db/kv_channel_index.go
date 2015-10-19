3d004ec8ee41c0e03742fe4026f2305dd5ccd746
Adam Fraser <adamf@couchbase.com>
2015-10-19 15:19:49 -0700

    Additional logging during channel clock update

--- src/github.com/couchbase/sync_gateway/db/kv_channel_index.go
+++ src/github.com/couchbase/sync_gateway/db/kv_channel_index.go
@@ -346,7 +346,9 @@ func (k *kvChannelIndex) loadClock() {
 
 func (k *kvChannelIndex) writeClockCas(updateClock base.SequenceClock) error {
 	// Initial set, for the first cas update attempt
+	base.LogTo("DIndex+", "Updating channel clock [%s] with %s", k.channelName, base.PrintClock(updateClock))
 	k.clock.UpdateWithClock(updateClock)
+	base.LogTo("DIndex+", "In-memory channel clock [%s] updated to %s", k.channelName, base.PrintClock(k.Clock))
 	value, err := k.clock.Marshal()
 	if err != nil {
 		base.Warn("Error marshalling clock [%s] for update:%+v", base.PrintClock(k.clock), err)
@@ -360,6 +362,8 @@ func (k *kvChannelIndex) writeClockCas(updateClock base.SequenceClock) error {
 			return nil, err
 		}
 		k.clock.UpdateWithClock(updateClock)
+		base.LogTo("DIndex+", "CAS retry: Updating channel clock [%s] with %s", k.channelName, base.PrintClock(updateClock))
+		base.LogTo("DIndex+", "CAS retry: in-memory channel clock [%s] updated to %s", k.channelName, base.PrintClock(k.Clock))
 		return k.clock.Marshal()
 	})
 
