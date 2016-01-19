ae606631ef4dcc130fc203d6b9018bd838aabe46
Zachary Gramana <z.gramana@me.com>
2016-01-18 17:55:56 -0800

    Add sg-accel Win service wrapper; .gitignore cleanup
    Renamed sgservice -> sg-service; Fixed start failure
    
    Previously, if the SG or sg-accel exe failed to start,
    the service wrapper would appear as though it was
    running just fine. This adds a panic so that wrapper
    itself will also fail to start.
    
    Adds a Windows service wrapper build script.

---
+++ service/sg-windows/build.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM Build the Sync Gateway service wrapper
+go build -o sg-windows.exe sg-service.go
+
+REM Build the Sync Gateway Accelerator service wrapper
+go build -o sg-accel-service.exe sg-accel-service.go
---
+++ service/sg-windows/sg-service.go
@@ -0,0 +1,126 @@
+// +build windows
+
+package main
+
+import (
+	"log"
+	"os/exec"
+
+	"github.com/kardianos/service"
+	"os"
+)
+
+var logger service.Logger
+
+type program struct {
+	ExePath     string
+	ConfigPath  string
+	SyncGateway *exec.Cmd
+}
+
+func (p *program) Start(s service.Service) error {
+	go p.run()
+	return nil
+}
+
+func (p *program) run() {
+	logger.Infof("Starting Sync Gateway service using command: `%s %s`", p.ExePath, p.ConfigPath)
+
+	if p.ConfigPath != "" {
+		p.SyncGateway = exec.Command(p.ExePath, p.ConfigPath)
+	} else {
+		p.SyncGateway = exec.Command(p.ExePath)
+	}
+	err := p.SyncGateway.Start()
+	if err != nil {
+		logger.Errorf("Failed to start Sync Gateway due to error %v", err)
+		return
+	}
+	err = p.SyncGateway.Wait()
+	if err != nil {
+		logger.Errorf("Sync Gateway exiting with %v", err)
+		panic("Failed to start Sync Gateway service.")
+	}
+}
+
+func (p *program) Stop(s service.Service) error {
+	logger.Infof("Stopping Sync Gateway service...")
+	p.SyncGateway.Process.Kill()
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "SyncGateway",
+		DisplayName: "Couchbase Sync Gateway",
+		Description: "Couchbase Sync Gateway mobile application REST gateway service.",
+	}
+
+	var exePath string
+	var configPath string
+
+	switch len(os.Args) {
+	case 2:
+		exePath = "C:\\Program Files (x86)\\Couchbase\\sync_gateway.exe" // Uses default binary image path
+		svcConfig.Arguments = []string{"start"}                          // Uses the default config
+	case 3:
+		exePath = "C:\\Program Files (x86)\\Couchbase\\sync_gateway.exe" // Uses default binary image path
+		configPath = os.Args[2]                                          // Uses custom config
+		svcConfig.Arguments = []string{"start", configPath}
+	case 4:
+		exePath = os.Args[2]    // Uses custom binary image path
+		configPath = os.Args[3] // Uses custom config
+		svcConfig.Arguments = []string{"start", exePath, configPath}
+	default:
+		panic("Valid parameters combinations are: COMMAND [none, custom config path, or custom exe path and custom config path].")
+	}
+
+	prg := &program{
+		ExePath:    exePath,
+		ConfigPath: configPath,
+	}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch {
+	case os.Args[1] == "install":
+		logger.Info("Installing Sync Gateway")
+		err := s.Install()
+		if err != nil {
+			logger.Errorf("Failed to install Sync Gateway service: %s", err)
+		}
+		return
+	case os.Args[1] == "uninstall":
+		logger.Info("Uninstalling Sync Gateway")
+		err := s.Uninstall()
+		if err != nil {
+			logger.Errorf("Failed to uninstall Sync Gateway service: %s", err)
+		}
+		return
+	case os.Args[1] == "stop":
+		err := s.Stop()
+		if err != nil {
+			logger.Errorf("Failed to stop Sync Gateway service: %s", err)
+		}
+		return
+	case os.Args[1] == "restart":
+		err := s.Restart()
+		if err != nil {
+			logger.Errorf("Failed to restart Sync Gateway service: %s", err)
+		}
+		return
+	}
+
+	err = s.Run()
+
+	if err != nil {
+		logger.Error(err)
+	}
+	logger.Infof("Exiting Sync Gateway service.")
+}
