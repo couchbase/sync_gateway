83a7f6b66f5ed14d3d5b38ba3aa7a110ae81cd87
Andrew Reslan <andrew@couchbase.com>
2016-01-21 14:49:38 +0000

    Add Windows service wrapper package submodule dependencies under service/sg-windows/src to keep them separate from core SG packages

--- service/sg-windows/build.cmd
+++ service/sg-windows/build.cmd
@@ -1,6 +1,13 @@
 @echo off
+
+SETLOCAL
+
+SET GOPATH=%cd%
+
 REM Build the Sync Gateway service wrapper
 go build -o sg-windows.exe sg-service.go
 
 REM Build the Sync Gateway Accelerator service wrapper
 go build -o sg-accel-service.exe sg-accel-service.go
+
+ENDLOCAL
