fbc0c11cb738521af7d7f51d6510ea5effab1d7f
Traun Leyden <tleyden@couchbase.com>
2016-02-18 13:33:02 -0800

    Fixes #1606 - build.sh script is currently broken
    https://github.com/couchbase/sync_gateway/issues/1606

--- bench.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-# This script runs benchmark tests in all the subpackages.
-
-go test ./... -bench='LoggingPerformance' -benchtime 1m -run XXX
-
-go test ./... -bench='RestApiGetDocPerformance' -cpu 1,2,4 -benchtime 1m -run XXX
-
-
--- build.sh
+++ build.sh
@@ -1,23 +1,43 @@
 #!/bin/sh -e
 
-# This script builds the sync gateway.
+# This script builds sync gateway using pinned dependencies.
 
-# Set the git commit info before the build
-BUILD_INFO="./rest/git_info.go"
+## This script is not intended to be run "in place" from a git clone.
+## The next check tries to ensure that's the case
+if [ -f "main.go" ]; then
+    echo "This script is meant to run outside the clone directory.  See README"
+    exit 1
+fi 
 
-#tell git to ignore any local changes to git_info.go, we don't want to commit them to the repo
-git update-index --assume-unchanged ${BUILD_INFO}
+## Make sure the repo tool is installed, otherwise throw an error
+which repo
+if (( $? != 0 )); then
+    echo "You must install the repo tool first.  Try running: "
+    echo "sudo curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/bin/repo"
+    exit 1
+fi
 
-# Escape forward slash's so sed command does not get confused
-# We use thses in feature branches e.g. feature/issue_nnn
-GIT_BRANCH=`git status -b -s | sed q | sed 's/## //' | sed 's/\.\.\..*$//' | sed 's/\\//\\\\\//g' | sed 's/[[:space:]]//g'`
-GIT_COMMIT=`git rev-parse HEAD`
-GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)
+## If we don't already have a .repo directory, run "repo init"
+REPO_DIR=.repo
+if [ ! -d "$REPO_DIR" ]; then
+    echo "No .repo directory found, running 'repo init'"
+    repo init -u "https://github.com/couchbase/sync_gateway.git" -m manifest/default.xml
+fi
 
-sed -i.bak -e 's/GitCommit.*=.*/GitCommit = "'$GIT_COMMIT'"/' $BUILD_INFO
-sed -i.bak -e 's/GitBranch.*=.*/GitBranch = "'$GIT_BRANCH'"/' $BUILD_INFO
-sed -i.bak -e 's/GitDirty.*=.*/GitDirty = "'$GIT_DIRTY'"/' $BUILD_INFO
+## Repo Sync
+repo sync
 
-./go.sh install "$@" -v ./...
+## Update the version
+SG_DIR=`pwd`/godeps/src/github.com/couchbase/sync_gateway
+CURRENT_DIR=`pwd`
+cd $SG_DIR
 
-echo "Success! Output is $GOPATH/bin/sync_gateway and $GOPATH/bin/sg_accel"
+./set-version-stamp.sh
+
+# Go back to previous dir
+cd $CURRENT_DIR
+
+## Go Install
+GOPATH=`pwd`/godeps go install "$@" github.com/couchbase/sync_gateway/...
+
+echo "Success! Output is godeps/bin/sync_gateway and godeps/bin/sg_accel"
