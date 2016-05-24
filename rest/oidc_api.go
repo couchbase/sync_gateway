372508d898b206bc58614329e5b3e2ced09eff22
Adam Fraser <adamf@couchbase.com>
2016-05-24 16:49:45 -0700

    Feature/issue 1688 oidc api (#1795)
    Adds _oidc and _oidc_callback endpoints
    
    Includes updates to manifest to pull in go-oidc dependencies.

---
+++ rest/oidc_api.go
@@ -0,0 +1,112 @@
+//  Copyright (c) 2016 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package rest
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/coreos/go-oidc/oauth2"
+	"github.com/coreos/go-oidc/oidc"
+	"github.com/couchbase/sync_gateway/base"
+)
+
+const (
+	OIDC_AUTH_RESPONSE_TYPE = "response_type"
+	OIDC_AUTH_CLIENT_ID     = "client_id"
+	OIDC_AUTH_SCOPE         = "scope"
+	OIDC_AUTH_REDIRECT_URI  = "redirect_uri"
+	OIDC_AUTH_STATE         = "state"
+
+	OIDC_RESPONSE_TYPE_CODE     = "code"
+	OIDC_RESPONSE_TYPE_IMPLICIT = "id_token%20token"
+)
+
+type OIDCCallbackResponse struct {
+	IDToken      string `json:"id_token"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
+
+func (h *handler) handleOIDC() error {
+
+	client, err := h.getOIDCClient()
+	if err != nil {
+		return err
+	}
+
+	oac, err := client.OAuthClient()
+	if err != nil {
+		return err
+	}
+
+	state := "1234"
+	accessType := ""
+	prompt := ""
+
+	// TODO: Should we support direct pass-through of access_type and prompt from the caller?
+	offline := h.getBoolQuery("offline")
+	if offline {
+		accessType = "offline"
+		prompt = "consent"
+	}
+
+	redirectURL, err := url.Parse(oac.AuthCodeURL(state, accessType, prompt))
+	if err != nil {
+		return err
+	}
+
+	http.Redirect(h.response, h.rq, redirectURL.String(), http.StatusFound)
+
+	return nil
+}
+
+func (h *handler) handleOIDCCallback() error {
+	code := h.getQuery("code")
+	if code == "" {
+		return base.HTTPErrorf(http.StatusBadRequest, "Code must be present on oidc callback")
+	}
+
+	client, err := h.getOIDCClient()
+	if err != nil {
+		return err
+	}
+
+	oac, err := client.OAuthClient()
+	if err != nil {
+		return err
+	}
+
+	tokenResponse, err := oac.RequestToken(oauth2.GrantTypeAuthCode, code)
+	if err != nil {
+		return err
+	}
+
+	callbackResponse := &OIDCCallbackResponse{
+		IDToken:      tokenResponse.IDToken,
+		RefreshToken: tokenResponse.RefreshToken,
+	}
+
+	h.writeJSON(callbackResponse)
+	return nil
+}
+
+func (h *handler) handleOIDCRefresh() error {
+	base.LogTo("Oidc", "handleOidcRefresh() called")
+	return nil
+}
+
+func (h *handler) getOIDCClient() (*oidc.Client, error) {
+	client := h.db.OIDCClient
+	if client == nil {
+		return nil, base.HTTPErrorf(http.StatusBadRequest, fmt.Sprintf("OpenID Connect not configured for database %v", h.db.Name))
+	}
+	return client, nil
+}
