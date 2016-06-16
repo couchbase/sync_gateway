2f81abf4087497944b559c9e47bf7732bf200e61
Adam Fraser <adamf@couchbase.com>
2016-06-15 22:14:47 -0700

    Fix for missing user handling
    Need to check for non-nil and non-empty (guest) user.

--- rest/session_api.go
+++ rest/session_api.go
@@ -52,7 +52,7 @@ func (h *handler) handleSessionPOST() error {
 	user, err := h.getUserFromSessionRequestBody()
 
 	// If we fail to get a user from the body and we've got an authenticated user, create the session based on that user
-	if user == nil && h.user != nil {
+	if user == nil && h.user.Name() != "" {
 		return h.makeSession(h.user)
 	} else {
 		if err != nil {
