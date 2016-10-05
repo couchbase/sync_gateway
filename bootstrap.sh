f485365be12c1a7f188417493b7a7aee2b73aca1
Traun Leyden <tleyden@couchbase.com>
2016-10-04 21:25:58 -0700

    Embed rewrite-manifest.sh into bootstrap.sh
    I feel dirty

--- bootstrap.sh
+++ bootstrap.sh
@@ -92,9 +92,6 @@ parseOptions () {
 # in that case.  I have left that in for now since it enables certain testing.
 rewriteManifest () {
 
-    curl -s "https://raw.githubusercontent.com/couchbase/sync_gateway/master/rewrite-manifest.sh" > rewrite-manifest.sh
-    chmod +x rewrite-manifest.sh
-
     
     case $PRODUCT in
 	sg)
@@ -147,6 +144,102 @@ downloadHelperScripts () {
 
 # --------------------------------------- Main -------------------------------------------
 
+# Super dirty hack to get rewrite-manifest.sh python script on file system
+# since it is awkward to download it from github via curl.
+# The deeper fix is to rewrite this boostrap.sh in python and just
+# call the code directly (rewrite-manifest.sh needs to parse xml)
+cat > rewrite-manifest.sh <<EOF
+#!/usr/bin/env python
+
+# This script updates the manifest created from a 'repo init -u <url>'
+# command with a *different* manifest, presumably before 'repo sync'
+# has been run.
+#
+# The purpose is to build from a manifest on a feature branch, for example
+# as part of validating a github pull request.
+#
+# Here are the actions performed:
+# 
+# 1. Fetches manifest from the given url
+# 2. Updates the given project revision to match the given commit
+# 3. Emits modified manifest to stdout
+#
+# Usage:
+#
+#     rewrite-manifest --manifest-url http://yourwebsite.co/manifest.xml --project-name your-project --set-revision new-sha > .repo/manifest.xml
+#
+# If your existing manifest contained a project entry like:
+#
+#     <project name="your-project" path="somepath" remote="someremote"/> 
+#
+# the above command would modify it to be:
+#
+#     <project name="your-project" path="somepath" revision="new-sha" remote="someremote"/> 
+#
+# and emit it to stdout, which you can redirect to overwrite your .repo/manifest.xml file
+
+import optparse
+import xml.etree.ElementTree as ET
+import urllib2
+import sys
+import base64
+
+def parse_args():
+    """
+    Parse command line args and return a tuple
+    """
+    parser = optparse.OptionParser()
+    parser.add_option('-u', '--manifest-url', help='Manifest URL')
+    parser.add_option('-n', '--username', help='Basic Auth username')
+    parser.add_option('-w', '--password', help='Basic Auth password')        
+    parser.add_option('-p', '--project-name', help="Project name to modify revision")
+    parser.add_option('-s', '--set-revision', help="SHA hash of revision to modify project specified via --project-name")
+    (opts, args) = parser.parse_args()
+    return (parser, opts.manifest_url, opts.project_name, opts.set_revision, opts.username, opts.password)
+
+def validate_args(parser, manifest_url, project_name, set_revision):
+    """
+    Make sure all required args are passed, or else print usage
+    """
+    if manifest_url is None:
+        parser.print_help()
+        exit(-1)
+    if project_name is None:
+        parser.print_help()
+        exit(-1)
+    if set_revision is None:
+        parser.print_help()
+        exit(-1)
+
+if __name__=="__main__":
+    
+   # get arguments
+   (parser, manifest_url, project_name, set_revision, username, password) = parse_args()
+
+   # validate arguments
+   validate_args(parser, manifest_url, project_name, set_revision)
+   
+   # fetch manifest content and parse xml
+   request = urllib2.Request(manifest_url)
+   if username != "":
+       base64string = base64.b64encode('%s:%s' % (username, password))
+       request.add_header("Authorization", "Basic %s" % base64string)
+   tree = ET.ElementTree(file=urllib2.urlopen(request))
+
+   # modify xml according to parameters
+   root = tree.getroot()
+   for element in root:
+       if element.get("name") == project_name:
+           element.set("revision", set_revision)
+           
+   # write modified xml to stdout
+   tree.write(sys.stdout)
+   sys.stdout.write("\n") # trailing newline
+
+EOF
+
+chmod +x rewrite-manifest.sh
+
 # By default, unless overridden by commit getopt arg, use the master branch everywhere
 COMMIT="master"
 
@@ -173,6 +266,7 @@ REPO_DIR=.repo
 if [ ! -d "$REPO_DIR" ]; then
     echo "No .repo directory found, running 'repo init' on $TARGET_REPO"
     repo init -u "$TARGET_REPO" -m manifest/default.xml
+    echo "Done running repo init"
 fi
 
 ## If a command line arg was passed in (commit), then rewrite manifest.xml
