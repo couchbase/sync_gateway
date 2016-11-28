78453db914b1e448604b23185327ffdede5bbcb0
Adam Fraser <adamf@couchbase.com>
2016-11-28 09:41:44 -0800

    Update test.sh to re-enable sgaccel tests

--- test.sh
+++ test.sh
@@ -10,7 +10,6 @@ echo "Running Sync Gateway unit tests"
 go test -v "$@" github.com/couchbase/sync_gateway/...
 
 if [ -d godeps/src/github.com/couchbaselabs/sync-gateway-accel ]; then
-    # when I tried this, it was hanging for a long time for me
-    echo "NOT Running Sync Gateway Accel unit tests -- are these working?"
-    #go test -v "$@" github.com/couchbaselabs/sync-gateway-accel/...
+    echo "Running Sync Gateway Accel unit tests"
+    go test -v "$@" github.com/couchbaselabs/sync-gateway-accel/...
 fi
