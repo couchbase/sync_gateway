807d3132b29d334193ed1ca9641ffb1cc67b2d46
Adam Fraser <adamf@couchbase.com>
2017-01-25 12:30:46 -0800

    Increase the password cache size to support
    Support more concurrent users/node without hitting the cache limit.  Temporary fix, until https://github.com/couchbase/sync_gateway/issues/1294 is implemented

--- auth/password_hash.go
+++ auth/password_hash.go
@@ -22,7 +22,7 @@ var cachedHashes = map[string]struct{}{}
 var cacheLock sync.Mutex
 
 // The maximum number of pairs to keep in the above cache
-const kMaxCacheSize = 10000
+const kMaxCacheSize = 25000
 
 // Optimized wrapper around bcrypt.CompareHashAndPassword that caches successful results in
 // memory to avoid the _very_ high overhead of calling bcrypt.
