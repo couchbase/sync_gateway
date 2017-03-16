a50dcb5f183705b57c3012b445865f85634bb2bc
Andrew Reslan <andrew@couchbase.com>
2017-03-16 13:34:37 +0000

    Removed ParseRevID wrapper func as private func is now public

--- db/revision.go
+++ db/revision.go
@@ -174,8 +174,6 @@ func ParseRevID(revid string) (int, string) {
 	return generation, id
 }
 
-func ParseRevID(revid string) (int, string) { return parseRevID(revid) }
-
 func compareRevIDs(id1, id2 string) int {
 	gen1, sha1 := ParseRevID(id1)
 	gen2, sha2 := ParseRevID(id2)
