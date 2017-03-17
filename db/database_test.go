fc204dedf75195dbc58d558d2955618e9b8bdc85
Andrew Reslan <andrew@couchbase.com>
2017-03-17 12:57:25 +0000

    Changed expected type of revision ID’s to []string from []interface{} to match results.

--- db/database_test.go
+++ db/database_test.go
@@ -212,7 +212,7 @@ func TestGetDeleted(t *testing.T) {
 		"_id":        "doc1",
 		"_rev":       rev2id,
 		"_deleted":   true,
-		"_revisions": map[string]interface{}{"start": 2, "ids": []interface{}{"bc6d97f6e97c0d034a34f8aac2bf8b44", "dfd5e19813767eeddd08270fc5f385cd"}},
+		"_revisions": map[string]interface{}{"start": 2, "ids": []string{"bc6d97f6e97c0d034a34f8aac2bf8b44", "dfd5e19813767eeddd08270fc5f385cd"}},
 	}
 	assert.DeepEquals(t, body, expectedResult)
 
