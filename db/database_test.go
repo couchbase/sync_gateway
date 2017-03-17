eaf2982086386d3e568d0b15399ca5cb9bc4c958
Andrew Reslan <andrew@couchbase.com>
2017-03-17 17:45:37 +0000

    Fixed TestDatabase expected results to use []string instead of []interface{} for rev ids

--- db/database_test.go
+++ db/database_test.go
@@ -148,7 +148,7 @@ func TestDatabase(t *testing.T) {
 	revisions := gotbody["_revisions"].(map[string]interface{})
 	assert.Equals(t, revisions["start"], 2)
 	assert.DeepEquals(t, revisions["ids"],
-		[]interface{}{"488724414d0ed6b398d6d2aeb228d797",
+		[]string{"488724414d0ed6b398d6d2aeb228d797",
 			"cb0c9a22be0e5a1b01084ec019defa81"})
 
 	// Test RevDiff:
