aaf9f8cb995fb57a225cee78329660df7d187b6f
Traun Leyden <tleyden@couchbase.com>
2017-03-21 17:41:04 -0700

    Temp disable outer fix

--- rest/api.go
+++ rest/api.go
@@ -155,11 +155,11 @@ func (h *handler) handleGetDB() error {
 
 	lastSeq := uint64(0)
 	runState := db.RunStateString[atomic.LoadUint32(&h.db.State)]
-	
+
 	// Don't bother trying to lookup LastSequence() if offline
-	if runState != db.RunStateString[db.DBOffline] {
+	// TEMP DISABLE OUTER FIX if runState != db.RunStateString[db.DBOffline] {
 		lastSeq, _ = h.db.LastSequence()
-	}
+	// TEMP DISABLE OUTER FIX }
 
 	response := db.Body{
 		"db_name":              h.db.Name,
