31a8a542626a0c66d0a12cbf26b568f00c941530
Traun Leyden <tleyden@couchbase.com>
2017-03-30 13:29:17 -0700

    Fix snap-manifest.sh + improve docs
    After digging into this, it should work better now

--- snap-manifest.sh
+++ snap-manifest.sh
@@ -4,13 +4,39 @@ import shutil
 import subprocess
 import os
 import xml.etree.ElementTree as ET
+import sys
+import urllib2
 
-# This script is  related to buiilding sync gateway from source via the `repo` tool,
-# which is a tool to deal with multiple git repos based on a manifest XML file which
-# pins dependencies to certain versions.
-#
-# This script has the ability to "snap" all of your dependencies into place,
-# according to the versions specified in your manifest file
+"""
+
+This will reset your local development environment to the sync gateway commit hash
+specified in the argument.
+
+WARNING: make sure all of your commits are pushed up to github before running this!  It may rollback local
+git repositories if you snap to a sync gateway version that points to commits earlier than you have locally,
+and then you will have to look into the git reflog to try to recover them, which is a pain.  To be safe,
+back up your entire sync_gateway directory before running this.
+
+Before running this, you should run:
+
+    $ repo status
+
+and make sure it returns:
+
+    nothing to commit (working directory clean)
+
+Usage:
+
+    $ ./snap-manifest.sh sync-gateway-commit-hash
+
+This will:
+
+1. Download the manifest/default.xml file from the Sync Gateway github repo
+2. Modify the manifest to set the Sync Gateway commit passed in
+3. Write the modified manifest to ./repo/manifest.xml
+4. Run repo sync
+
+"""
 
 def repo_sync():
     """
@@ -19,73 +45,36 @@ def repo_sync():
     """
     subprocess.call(['repo', 'sync', '-d'])  # TODO: does this need a subshell for any reason?
 
-def copy_modified_manifest(product, product_repo_commit, source_manifest_path, dest_path):
-    """
-    This will copy the manifest from source_manifest_path to dest_path, but
-    modify it along the way to update:
 
-    <project name="sync_gateway"/>
-
-    to
-
-    <project name="sync_gateway" revision="82493418e" />
- 
-    To stamp it with the particular revision corresponding to the current repo commit
-    """
-
-    sourcefile = open(source_manifest_path)
-    tree = ET.ElementTree(file=sourcefile)
-
+def update_sg_version(manifest_xml_content, commit):
     # modify xml according to parameters
-    root = tree.getroot()
+    root = manifest_xml_content.getroot()
     for element in root:
-        if element.get("name") == product:
-            element.set("revision", product_repo_commit)
-           
-    # write modified xml to stdout
-    destfile = open(dest_path, 'w')
-    tree.write(destfile)
-    
-    destfile.close()
-    sourcefile.close()
+        if element.get("name") == "sync_gateway":
+            element.set("revision", commit)
+    return manifest_xml_content
 
-def discover_product_repo_commit(source_manifest_path):
+def get_manifest_xml_from_sg_github(commit):
+    manifest_url="https://raw.githubusercontent.com/couchbase/sync_gateway/{}/manifest/default.xml".format(commit)
+    response = urllib2.urlopen(manifest_url)
+    return ET.ElementTree(file=response)
 
-    """
-    Go to that directory and 
-    """
-    cur_dir = os.getcwd()
-    git_revision_hash = "n/a"
-    
-    try:
-        source_manifest_dir = os.path.dirname(source_manifest_path)
-        os.chdir(source_manifest_dir)
-        git_revision_hash = get_git_revision_hash()
-    finally:
-        os.chdir(cur_dir)
-
-    return git_revision_hash.strip()
-
-def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-    
-    
 if __name__=="__main__":
 
-    product = "sync_gateway"
 
-    # Path to source manifest
-    source_manifest_path = "godeps/src/github.com/couchbase/sync_gateway/manifest/default.xml"
 
-    product_repo_commit = discover_product_repo_commit(source_manifest_path)
-    
-    # Modify manifest and copy it to the ./repo/manifest.xml file
-    copy_modified_manifest(
-        product,
-        product_repo_commit,
-        source_manifest_path,
-        ".repo/manifest.xml"
-    )
+    if len(sys.argv) <= 1:
+        raise Exception("Usage: ./snap-manifest.sh sync-gateway-commit-hash")
+
+    commit = sys.argv[1]
+    manifest_xml_content = get_manifest_xml_from_sg_github(commit)
+    versioned_manifest_xml_content = update_sg_version(manifest_xml_content, commit)
+
+    # Write to dest file
+    dest_path = ".repo/manifest.xml"
+    destfile = open(dest_path, 'w')
+    versioned_manifest_xml_content.write(destfile)
+    destfile.close()
 
     # Run repo sync
     repo_sync()
