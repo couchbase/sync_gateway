f4f807d0c7fc5558f8f2c400aa7408eb187b35cc
Traun Leyden <tleyden@couchbase.com>
2017-04-04 13:47:04 -0700

    PR feedback related cb 3

--- db/crud.go
+++ db/crud.go
@@ -701,10 +701,11 @@ func (db *Database) updateDoc(docid string, allowImport bool, expiry uint32, cal
 			changedRoleUsers = doc.RoleAccess.updateAccess(doc, roles)
 
 			if len(changedPrincipals) > 0 || len(changedRoleUsers) > 0 {
-
-				if major, _, _, err := db.Bucket.CouchbaseServerVersion(); err == nil && major >= 3 {
-					base.LogTo("CRUD+", "Optimizing write for Couchbase Server >= 3.0")
-				} else {
+				
+				major, _, _, err := db.Bucket.CouchbaseServerVersion()
+				if err == nil && major < 3 {
+					// If the couchbase server version is less than 3, then do an indexable write.
+					// Prior to couchbase 3, a stale=false query required an indexable write (due to race).
 					// If this update affects user/role access privileges, make sure the write blocks till
 					// the new value is indexable, otherwise when a User/Role updates (using a view) it
 					// might not incorporate the effects of this change.
