3e2b10106ff8170461e1d27b2131960339f28cbb
Traun Leyden <tleyden@couchbase.com>
2017-04-04 12:23:12 -0700

    GetGoCBBucketFromBaseBucket checks for CouchbaseBucketGoCB
    https://github.com/couchbase/sync_gateway/pull/2455#issuecomment-291605276

--- base/util.go
+++ base/util.go
@@ -533,6 +533,10 @@ func GetGoCBBucketFromBaseBucket(baseBucket Bucket) (bucket CouchbaseBucketGoCB,
 		return *baseBucket.CouchbaseBucketGoCB, nil
 	case *CouchbaseBucketGoCBGoCouchbaseHybrid:
 		return *baseBucket.CouchbaseBucketGoCB, nil
+	case *CouchbaseBucketGoCB:
+		return *baseBucket, nil
+	case CouchbaseBucketGoCB:
+		return baseBucket, nil
 	default:
 		return CouchbaseBucketGoCB{}, fmt.Errorf("baseBucket %v was not a CouchbaseBucketGoCB.  Was type: %T", baseBucket, baseBucket)
 	}
