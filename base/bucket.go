02d062eaa46690568de4945eb4c29dcb1ceb108a
Traun Leyden <tleyden@couchbase.com>
2017-05-10 16:16:58 -0700

    PR Feedback regarding views constants
    https://github.com/couchbase/sync_gateway/pull/2543#discussion_r115613285

--- base/bucket.go
+++ base/bucket.go
@@ -42,24 +42,6 @@ const (
 	IndexBucket
 )
 
-const (
-	ViewQueryParamStale         = "stale"
-	ViewQueryParamReduce        = "reduce"
-	ViewQueryParamStartKey      = "startkey"
-	ViewQueryParamEndKey        = "endkey"
-	ViewQueryParamInclusiveEnd  = "inclusive_end"
-	ViewQueryParamLimit         = "limit"
-	ViewQueryParamIncludeDocs   = "include_docs"
-	ViewQueryParamDescending    = "descending"
-	ViewQueryParamGroup         = "group"
-	ViewQueryParamSkip          = "skip"
-	ViewQueryParamGroupLevel    = "group_level"
-	ViewQueryParamStartKeyDocId = "startkey_docid"
-	ViewQueryParamEndKeyDocId   = "endkey_docid"
-	ViewQueryParamKey           = "key"
-	ViewQueryParamKeys          = "keys"
-)
-
 func ChooseCouchbaseDriver(bucketType CouchbaseBucketType, feedType string) CouchbaseDriver {
 
 	feedType = strings.ToLower(feedType)
---
+++ base/views.go
@@ -0,0 +1,20 @@
+package base
+
+const (
+	ViewQueryParamStale         = "stale"
+	ViewQueryParamReduce        = "reduce"
+	ViewQueryParamStartKey      = "startkey"
+	ViewQueryParamEndKey        = "endkey"
+	ViewQueryParamInclusiveEnd  = "inclusive_end"
+	ViewQueryParamLimit         = "limit"
+	ViewQueryParamIncludeDocs   = "include_docs"
+	ViewQueryParamDescending    = "descending"
+	ViewQueryParamGroup         = "group"
+	ViewQueryParamSkip          = "skip"
+	ViewQueryParamGroupLevel    = "group_level"
+	ViewQueryParamStartKeyDocId = "startkey_docid"
+	ViewQueryParamEndKeyDocId   = "endkey_docid"
+	ViewQueryParamKey           = "key"
+	ViewQueryParamKeys          = "keys"
+)
+
