158f0a49a9792f46eb66d372ded2fdbbd9c0ea76
Andrew Reslan <andrew@couchbase.com>
2017-05-26 19:01:57 +0100

    Added unit test to validate that an unused sequence number in a _bulk_docs call (due to rejection by Sync Function), are not reused later.

--- rest/api_test.go
+++ rest/api_test.go
@@ -726,6 +726,38 @@ func TestBulkDocs(t *testing.T) {
 	assertStatus(t, response, 200)
 }
 
+func TestBulkDocsUnusedSequences(t *testing.T) {
+
+	//We want a sync function that will reject some docs
+	rt := restTester{syncFn: `function(doc) {if(doc.type == "failed") {throw("Rejecting failed doc")}}`}
+
+	input := `{"docs": [{"_id": "bulk1", "n": 1}, {"_id": "bulk2", "n": 2, "type": "failed"}, {"_id": "_local/bulk3", "n": 3}]}`
+	response := rt.sendRequest("POST", "/db/_bulk_docs", input)
+	assertStatus(t, response, 201)
+
+	doc1Rev, _ := rt.getDatabase().GetDocSyncData("bulk1")
+	assert.Equals(t, doc1Rev, 1)
+
+	doc3Rev, _ := rt.getDatabase().GetDocSyncData("bulk3")
+	assert.Equals(t, doc3Rev, 3)
+
+
+	//send another _bulk_docs and validate the sequences used
+	input = `{"docs": [{"_id": "bulk21", "n": 21}, {"_id": "bulk22", "n": 22}, {"_id": "_local/bulk23", "n": 23}]}`
+	response = rt.sendRequest("POST", "/db/_bulk_docs", input)
+	assertStatus(t, response, 201)
+
+	doc21Rev, _ := rt.getDatabase().GetDocSyncData("bulk21")
+	assert.Equals(t, doc21Rev, 4)
+
+	doc22Rev, _ := rt.getDatabase().GetDocSyncData("bulk22")
+	assert.Equals(t, doc22Rev, 5)
+
+	doc23Rev, _ := rt.getDatabase().GetDocSyncData("bulk23")
+	assert.Equals(t, doc23Rev, 6)
+
+}
+
 func TestBulkDocsEmptyDocs(t *testing.T) {
 	var rt RestTester
 	input := `{}`
