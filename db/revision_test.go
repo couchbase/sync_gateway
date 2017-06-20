f97ba175ba944edd2ae66395eddd906754ad1d74
Traun Leyden <tleyden@couchbase.com>
2017-06-19 17:28:31 -0700

    Add unit test for ParseRevID

---
+++ db/revision_test.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"testing"
+	"log"
+)
+
+func TestParseRevID(t *testing.T) {
+
+	var generation int
+	var digest string
+
+	generation, _ = ParseRevID("ljlkjl")
+	log.Printf("generation: %v", generation)
+	assertTrue(t, generation == -1, "Expected -1 generation for invalid rev id")
+
+	generation, digest = ParseRevID("1-ljlkjl")
+	log.Printf("generation: %v, digest: %v", generation, digest)
+	assertTrue(t, generation == 1, "Expected 1 generation")
+	assertTrue(t, digest == "ljlkjl", "Unexpected digest")
+
+
+	generation, digest = ParseRevID("2222-lj-lkjl")
+	log.Printf("generation: %v, digest: %v", generation, digest)
+	assertTrue(t, generation == 2222, "Expected generation")
+	assertTrue(t, digest == "lj-lkjl", "Unexpected digest")
+
+	generation, digest = ParseRevID("2222-")
+	log.Printf("generation: %v, digest: %v", generation, digest)
+	assertTrue(t, generation == -1, "Expected invalid generation")
+
+	generation, digest = ParseRevID("333-a")
+	log.Printf("generation: %v, digest: %v", generation, digest)
+	assertTrue(t, generation == 333, "Expected generation")
+	assertTrue(t, digest == "a", "Unexpected digest")
+
+}
