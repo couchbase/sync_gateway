d0fc22384e915f73d30d2aabb96defde5c4b8181
Traun Leyden <tleyden@couchbase.com>
2017-07-13 11:30:11 -0700

    Remove DeleteWithXattrOld()

--- base/bucket_gocb.go
+++ base/bucket_gocb.go
@@ -1053,34 +1053,6 @@ func (bucket CouchbaseBucketGoCB) GetWithXattr(k string, xattrKey string, rv int
 
 }
 
-// Delete a document and it's associated named xattr.  Couchbase server will preserve system xattrs as part of the (CBS) tombstone when a document is deleted.
-// To remove the system xattr as well, an explicit subdoc delete operation is required.
-func (bucket CouchbaseBucketGoCB) DeleteWithXattrOld(k string, xattrKey string) error {
-
-	bucket.singleOps <- struct{}{}
-	defer func() {
-		<-bucket.singleOps
-	}()
-	gocbExpvars.Add("Delete", 1)
-
-	// Normal remove, with soft-delete
-	removeCas, err := bucket.Bucket.Remove(k, 0)
-	if err != nil && err != gocb.ErrKeyNotFound {
-		return err
-	}
-
-	// Remove xattr from recently soft-deleted
-	_, deleteXattrErr := bucket.Bucket.MutateInEx(k, gocb.SubdocDocFlagAccessDeleted, removeCas, 0).
-		RemoveEx(xattrKey, gocb.SubdocFlagXattr).
-		Execute()
-
-	if deleteXattrErr != nil && deleteXattrErr != gocbcore.ErrSubDocSuccessDeleted {
-		return deleteXattrErr
-	}
-
-	return nil
-}
-
 func (bucket CouchbaseBucketGoCB) DeleteWithXattr(k string, xattrKey string) error {
 
 	bucket.singleOps <- struct{}{}
