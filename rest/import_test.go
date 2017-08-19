couchbase/sync_gateway

commit db39d17b22a44420f5620d9e649d782e4a3650a3

Metadata migration and upgrade (#2801)
* Metadata migration and upgrade support

Sync Gateway running with xattrs and import enabled will also migrate documents written by Sync Gateway - moving sync metadata from the document body to xattr.

Also adds upgrade support - nodes running in non-xattr mode will attempt to read xattrs when not finding metadata in the document body.

* Add comment for clarification

* Update to comments based on PR review

base/bucket_gocb.go
@@ -1002,7 +1002,7 @@ func (bucket CouchbaseBucketGoCB) WriteCasWithXattr(k string, xattrKey string, e
 	return cas, err
 }
 
-// CAS-safe update of a document's xattr.  Also deletes the document body if deleteBody is true.
+// CAS-safe update of a document's xattr (only).  Deletes the document body if deleteBody is true.
 func (bucket CouchbaseBucketGoCB) UpdateXattr(k string, xattrKey string, exp int, cas uint64, xv interface{}, deleteBody bool) (casOut uint64, err error) {
 
 	// WriteCasWithXattr always stamps the xattr with the new cas using macro expansion, into a top-level property called 'cas'.
@@ -1425,17 +1425,13 @@ func (bucket CouchbaseBucketGoCB) WriteUpdateWithXattr(k string, xattrKey string
 			if err != nil {
 				if !bucket.IsKeyNotFoundError(err) {
 					// Unexpected error, cancel writeupdate
-					LogTo("CRUD", "Retrieval of existing doc failed during WriteUpdateWithXattr for key=%s, xattrKey=%s: %v", k, xattrKey, err)
+					LogTo("CRUD+", "Retrieval of existing doc failed during WriteUpdateWithXattr for key=%s, xattrKey=%s: %v", k, xattrKey, err)
 					return emptyCas, err
 				}
 				// Key not found - initialize cas and values
-
-				LogTo("CRUD+", "WriteUpdateWithXattr for key=%s. GetWithXattr err: %v.  Treating as insert.", k, cas)
 				cas = 0
 				value = nil
 				xattrValue = nil
-			} else {
-				LogTo("CRUD+", "WriteUpdateWithXattr for key=%s. GetWithXattr successful - cas: %v  len(value):%d", k, cas, len(value))
 			}
 		}
 
@@ -1446,46 +1442,37 @@ func (bucket CouchbaseBucketGoCB) WriteUpdateWithXattr(k string, xattrKey string
 			return emptyCas, err
 		}
 
-		var writeErr error
-		// If this is a tombstone, we want to delete the document and update the xattr
-		if deleteDoc {
-			deleteBody := len(value) > 0
-			deleteCas, deleteErr := bucket.UpdateXattr(k, xattrKey, exp, cas, updatedXattrValue, deleteBody)
-			switch deleteErr {
-			case nil:
-				return deleteCas, deleteErr
-			case gocb.ErrKeyExists:
-				// Retry on CAS failure.
-				continue
-			default:
-				// UpdateXattr already does retry on recoverable errors, so return any error here
-				LogTo("CRUD", "Delete and update of xattr during WriteUpdateWithXattr failed for key %s: %v", k, deleteErr)
-				return emptyCas, deleteErr
-			}
-
-		} else {
-			// Not a delete - update the body and xattr
-			casOut, writeErr = bucket.WriteCasWithXattr(k, xattrKey, exp, cas, updatedValue, updatedXattrValue)
-
-			// ErrKeyExists is CAS failure, which we want to retry.  Other non-recoverable errors should cancel the
-			// WriteUpdate.
-			if writeErr != nil && writeErr != gocb.ErrKeyExists && !isRecoverableGoCBError(writeErr) {
-				LogTo("CRUD", "Update of new value during WriteUpdateWithXattr failed for key %s: %v", k, writeErr)
-				return emptyCas, writeErr
-			}
-
-			// If there was no error, we're done
-			if writeErr == nil {
-				return casOut, nil
-			}
+		// Attempt to write the updated document to the bucket
+		casOut, writeErr := bucket.WriteWithXattr(k, xattrKey, exp, cas, updatedValue, updatedXattrValue, deleteDoc)
+		switch writeErr {
+		case nil:
+			return casOut, nil
+		case gocb.ErrKeyExists:
+			// Retry on cas failure
+		default:
+			// WriteWithXattr already handles retry on recoverable errors, so fail on any errors other than ErrKeyExists
+			return emptyCas, writeErr
 		}
+
 		// Reset value, xattr, cas for cas retry
 		value = nil
 		xattrValue = nil
 		cas = 0
 
 	}
+}
 
+// Single attempt to update a document and xattr.  Setting isDelete=true and value=nil will delete the document body.  Both
+// update types (UpdateXattr, WriteCasWithXattr) include recoverable error retry.
+func (bucket CouchbaseBucketGoCB) WriteWithXattr(k string, xattrKey string, exp int, cas uint64, value []byte, xattrValue []byte, isDelete bool) (casOut uint64, err error) {
+	// If this is a tombstone, we want to delete the document and update the xattr
+	if isDelete {
+		deleteBody := len(value) > 0
+		return bucket.UpdateXattr(k, xattrKey, exp, cas, xattrValue, deleteBody)
+	} else {
+		// Not a delete - update the body and xattr
+		return bucket.WriteCasWithXattr(k, xattrKey, exp, cas, value, xattrValue)
+	}
 }
 
 func (bucket CouchbaseBucketGoCB) Incr(k string, amt, def uint64, exp int) (uint64, error) {

base/error.go
@@ -28,6 +28,7 @@ const (
 	viewTimeoutError      = sgErrorCode(0x03)
 	revTreeAddRevFailure  = sgErrorCode(0x04)
 	importCancelledFilter = sgErrorCode(0x05)
+	documentMigrated      = sgErrorCode(0x06)
 )
 
 type SGError struct {
@@ -35,12 +36,13 @@ type SGError struct {
 }
 
 var (
-	ErrRevTreeAddRevFailure = &SGError{revTreeAddRevFailure}
-	ErrImportCancelled      = &SGError{importCancelled}
-	ErrAlreadyImported      = &SGError{alreadyImported}
-	ErrImportCasFailure     = &SGError{importCasFailure}
-	ErrViewTimeoutError     = &SGError{viewTimeoutError}
+	ErrRevTreeAddRevFailure  = &SGError{revTreeAddRevFailure}
+	ErrImportCancelled       = &SGError{importCancelled}
+	ErrAlreadyImported       = &SGError{alreadyImported}
+	ErrImportCasFailure      = &SGError{importCasFailure}
+	ErrViewTimeoutError      = &SGError{viewTimeoutError}
 	ErrImportCancelledFilter = &SGError{importCancelledFilter}
+	ErrDocumentMigrated      = &SGError{documentMigrated}
 )
 
 func (e SGError) Error() string {
@@ -49,6 +51,8 @@ func (e SGError) Error() string {
 		return "Document already imported"
 	case importCancelled:
 		return "Import cancelled"
+	case documentMigrated:
+		return "Document migrated"
 	case importCancelledFilter:
 		return "Import cancelled based on import filter"
 	case importCasFailure:

db/change_cache.go
@@ -400,8 +400,15 @@ func (c *changeCache) DocChangedSynchronous(event sgbucket.FeedEvent) {
 	}
 
 	if !syncData.HasValidSyncData(c.context.writeSequences()) {
-		base.Warn("changeCache: Doc %q does not have valid sync data.", docID)
-		return
+		// No sync metadata found - check whether we're mid-upgrade and attempting to read a doc w/ metadata stored in xattr
+		migratedDoc, _ := c.context.checkForUpgrade(docID)
+		if migratedDoc != nil && migratedDoc.Cas == event.Cas {
+			base.LogTo("Cache", "Found mobile xattr on document without _sync property - caching, assuming upgrade in progress.")
+			syncData = &migratedDoc.syncData
+		} else {
+			base.Warn("changeCache: Doc %q does not have valid sync data.", docID)
+			return
+		}
 	}
 
 	if syncData.Sequence <= c.initialSequence {

db/crud.go
@@ -48,37 +48,55 @@ func (db *DatabaseContext) GetDoc(docid string) (doc *document, err error) {
 	}
 	dbExpvars.Add("document_gets", 1)
 	if db.UseXattrs() {
-		var rawDoc, rawXattr []byte
-		cas, getErr := db.Bucket.GetWithXattr(key, KSyncXattrName, &rawDoc, &rawXattr)
-		if getErr != nil {
-			return nil, getErr
-		}
-		var unmarshalErr error
-		doc, unmarshalErr = unmarshalDocumentWithXattr(docid, rawDoc, rawXattr, cas)
-		if unmarshalErr != nil {
-			return nil, unmarshalErr
+		var rawBucketDoc *sgbucket.BucketDocument
+		doc, rawBucketDoc, err = db.GetDocWithXattr(key)
+		if err != nil {
+			return nil, err
 		}
 		// If existing doc wasn't an SG Write, import the doc.
 		if !doc.IsSGWrite() {
 			var importErr error
-			doc, importErr = db.OnDemandImportForGet(docid, rawDoc, rawXattr, cas)
+			doc, importErr = db.OnDemandImportForGet(docid, rawBucketDoc.Body, rawBucketDoc.Xattr, rawBucketDoc.Cas)
 			if importErr != nil {
 				return nil, importErr
 			}
 		}
-
+		if !doc.HasValidSyncData(db.writeSequences()) {
+			return nil, base.HTTPErrorf(404, "Not imported")
+		}
 	} else {
 		doc = newDocument(docid)
 		_, err = db.Bucket.Get(key, doc)
 		if err != nil {
 			return nil, err
 		}
+		if !doc.HasValidSyncData(db.writeSequences()) {
+			// Check whether doc has been upgraded to use xattrs
+			upgradeDoc, _ := db.checkForUpgrade(docid)
+			if upgradeDoc == nil {
+				return nil, base.HTTPErrorf(404, "Not imported")
+			}
+			doc = upgradeDoc
+		}
 	}
 
-	if !doc.HasValidSyncData(db.writeSequences()) {
-		return nil, base.HTTPErrorf(404, "Not imported")
-	}
 	return doc, nil
+}
+
+func (db *DatabaseContext) GetDocWithXattr(key string) (doc *document, rawBucketDoc *sgbucket.BucketDocument, err error) {
+	rawBucketDoc = &sgbucket.BucketDocument{}
+	var getErr error
+	rawBucketDoc.Cas, getErr = db.Bucket.GetWithXattr(key, KSyncXattrName, &rawBucketDoc.Body, &rawBucketDoc.Xattr)
+	if getErr != nil {
+		return nil, nil, getErr
+	}
+
+	var unmarshalErr error
+	doc, unmarshalErr = unmarshalDocumentWithXattr(key, rawBucketDoc.Body, rawBucketDoc.Xattr, rawBucketDoc.Cas)
+	if unmarshalErr != nil {
+		return nil, nil, unmarshalErr
+	}
+	return doc, rawBucketDoc, nil
 }
 
 // This gets *just* the Sync Metadata (_sync field) rather than the entire doc, for efficiency reasons.
@@ -510,6 +528,7 @@ func (db *Database) initializeSyncData(doc *document) (err error) {
 }
 
 func (db *Database) OnDemandImportForWrite(docid string, doc *document, body Body) error {
+
 	// Check whether the doc requiring import is an SDK delete
 	isDelete := false
 	if doc.body == nil {
@@ -553,10 +572,12 @@ func (db *Database) Put(docid string, body Body) (newRevID string, err error) {
 		return "", base.HTTPErrorf(http.StatusBadRequest, "Invalid expiry: %v", err)
 	}
 
-	return db.updateDoc(docid, true, expiry, func(doc *document) (Body, AttachmentData, error) {
+	allowImport := db.UseXattrs()
+
+	return db.updateDoc(docid, allowImport, expiry, func(doc *document) (Body, AttachmentData, error) {
 
 		// If the existing doc isn't an SG write, import prior to updating
-		if doc != nil && !doc.IsSGWrite() {
+		if doc != nil && !doc.IsSGWrite() && db.UseXattrs() {
 			err := db.OnDemandImportForWrite(docid, doc, body)
 			if err != nil {
 				return nil, nil, err
@@ -614,10 +635,11 @@ func (db *Database) PutExistingRev(docid string, body Body, docHistory []string)
 		return base.HTTPErrorf(http.StatusBadRequest, "Invalid expiry: %v", err)
 	}
 
-	_, err = db.updateDoc(docid, true, expiry, func(doc *document) (Body, AttachmentData, error) {
+	allowImport := db.UseXattrs()
+	_, err = db.updateDoc(docid, allowImport, expiry, func(doc *document) (Body, AttachmentData, error) {
 
 		// If the existing doc isn't an SG write, import prior to updating
-		if doc != nil && !doc.IsSGWrite() {
+		if doc != nil && !doc.IsSGWrite() && db.UseXattrs() {
 			err := db.OnDemandImportForWrite(docid, doc, body)
 			if err != nil {
 				return nil, nil, err
@@ -689,7 +711,7 @@ func (db *Database) updateAndReturnDoc(
 	}
 
 	// Added annotation to the following variable declarations for reference during future refactoring of documentUpdateFunc into a standalone function
-	var doc *document                                // Passed to documentUpdateFunc as pointer, may be possible to define in documentUpdateFuncnot need to be defined here
+	var doc *document                                // Passed to documentUpdateFunc as pointer, may be possible to define in documentUpdateFunc
 	var body Body                                    // Could be returned by documentUpdateFunc
 	var changedPrincipals, changedRoleUsers []string // Could be returned by documentUpdateFunc
 	var docSequence uint64                           // Must be scoped outside callback, used over multiple iterations
@@ -697,12 +719,11 @@ func (db *Database) updateAndReturnDoc(
 	var oldBodyJSON string                           // Could be returned by documentUpdateFunc.  Stores previous revision body for use by DocumentChangeEvent
 
 	// documentUpdateFunc applies the changes to the document.  Called by either WriteUpdate or WriteUpdateWithXATTR below.
-	documentUpdateFunc := func(doc *document, docExists bool) (updatedDoc *document, writeOpts sgbucket.WriteOptions, shadowerEcho bool, err error) {
+	documentUpdateFunc := func(doc *document, docExists bool, importAllowed bool) (updatedDoc *document, writeOpts sgbucket.WriteOptions, shadowerEcho bool, err error) {
 
 		var newAttachments AttachmentData
-
 		// Be careful: this block can be invoked multiple times if there are races!
-		if !allowImport && docExists && !doc.HasValidSyncData(db.writeSequences()) {
+		if !importAllowed && docExists && !doc.HasValidSyncData(db.writeSequences()) {
 			err = base.HTTPErrorf(409, "Not imported")
 			return
 		}
@@ -921,15 +942,46 @@ func (db *Database) updateAndReturnDoc(
 	var shadowerEcho bool
 
 	// Update the document
-	if db.UseXattrs() {
+	upgradeInProgress := false
+	if !db.UseXattrs() {
+		// Update the document, storing metadata in _sync property
+		err = db.Bucket.WriteUpdate(key, int(expiry), func(currentValue []byte) (raw []byte, writeOpts sgbucket.WriteOptions, err error) {
+			// Be careful: this block can be invoked multiple times if there are races!
+			if doc, err = unmarshalDocument(docid, currentValue); err != nil {
+				return
+			}
+			docOut, writeOpts, shadowerEcho, err = documentUpdateFunc(doc, currentValue != nil, allowImport)
+			if err != nil {
+				return
+			}
+
+			// Return the new raw document value for the bucket to store.
+			raw, err = json.Marshal(docOut)
+			base.LogTo("CRUD+", "Saving doc (seq: #%d, id: %v rev: %v)", doc.Sequence, doc.ID, doc.CurrentRev)
+
+			return raw, writeOpts, err
+		})
+
+		// If we can't find sync metadata in the document body, check for upgrade.  If upgrade, retry write using WriteUpdateWithXattr
+		if err != nil && err.Error() == "409 Not imported" {
+			_, bucketDocument := db.checkForUpgrade(key)
+			if bucketDocument.Xattr != nil {
+				existingDoc = bucketDocument
+				upgradeInProgress = true
+			}
+		}
+	}
+
+	if db.UseXattrs() || upgradeInProgress {
 		var casOut uint64
+		// Update the document, storing metadata in extended attribute
 		casOut, err = db.Bucket.WriteUpdateWithXattr(key, KSyncXattrName, int(expiry), existingDoc, func(currentValue []byte, currentXattr []byte, cas uint64) (raw []byte, rawXattr []byte, deleteDoc bool, err error) {
 			// Be careful: this block can be invoked multiple times if there are races!
 			if doc, err = unmarshalDocumentWithXattr(docid, currentValue, currentXattr, cas); err != nil {
 				return
 			}
 
-			docOut, _, _, err = documentUpdateFunc(doc, currentValue != nil)
+			docOut, _, _, err = documentUpdateFunc(doc, currentValue != nil, true)
 			if err != nil {
 				return
 			}
@@ -948,27 +1000,16 @@ func (db *Database) updateAndReturnDoc(
 			return raw, rawXattr, deleteDoc, err
 		})
 		if err != nil {
-			base.LogTo("CRUD+", "Did not update document %q w/ xattr: %v", key, err)
+			if err == base.ErrDocumentMigrated {
+				base.LogTo("CRUD+", "Migrated document %q to use xattr.", key)
+			} else {
+				base.LogTo("CRUD+", "Did not update document %q w/ xattr: %v", key, err)
+			}
 		} else if docOut != nil {
 			docOut.Cas = casOut
 		}
 	} else {
-		err = db.Bucket.WriteUpdate(key, int(expiry), func(currentValue []byte) (raw []byte, writeOpts sgbucket.WriteOptions, err error) {
-			// Be careful: this block can be invoked multiple times if there are races!
-			if doc, err = unmarshalDocument(docid, currentValue); err != nil {
-				return
-			}
-			docOut, writeOpts, shadowerEcho, err = documentUpdateFunc(doc, currentValue != nil)
-			if err != nil {
-				return
-			}
 
-			// Return the new raw document value for the bucket to store.
-			raw, err = json.Marshal(docOut)
-			base.LogTo("CRUD+", "Saving doc (seq: #%d, id: %v rev: %v)", doc.Sequence, doc.ID, doc.CurrentRev)
-
-			return raw, writeOpts, err
-		})
 	}
 
 	// If the WriteUpdate didn't succeed, check whether there are unused, allocated sequences that need to be accounted for
@@ -1296,6 +1337,18 @@ func (context *DatabaseContext) ComputeVbSequenceRolesForUser(user auth.User) (c
 		roleSet.Add(row.Value)
 	}
 	return roleSet, nil
+}
+
+// Checks whether a document has a mobile xattr.  Used when running in non-xattr mode to support no downtime upgrade.
+func (context *DatabaseContext) checkForUpgrade(key string) (*document, *sgbucket.BucketDocument) {
+	if context.UseXattrs() {
+		return nil, nil
+	}
+	doc, rawDocument, err := context.GetDocWithXattr(key)
+	if err != nil || doc == nil || !doc.HasValidSyncData(context.writeSequences()) {
+		return nil, nil
+	}
+	return doc, rawDocument
 }
 
 //////// REVS_DIFF:

db/database.go
@@ -511,12 +511,14 @@ func (db *Database) DocCount() int {
 func installViews(bucket base.Bucket, useXattrs bool) error {
 
 	// syncData specifies the path to Sync Gateway sync metadata used in the map function -
-	// in the document body when xattrs disabled, in the mobile xattr when xattrs enabled.
-	syncData := "var sync = doc._sync;"
-	if useXattrs {
-		syncData = fmt.Sprintf(`var sync = meta.xattrs.%s; 
-							var mb_24037 = doc.id;`, KSyncXattrName) // Workaround for https://issues.couchbase.com/browse/MB-24037
-	}
+	// in the document body when xattrs available, in the mobile xattr when xattrs enabled.
+	syncData := fmt.Sprintf(`var sync
+							if (meta.xattrs === undefined || meta.xattrs.%s === undefined) {
+		                        sync = doc._sync
+		                  	} else {
+		                       	sync = meta.xattrs.%s
+		                    }
+		                     `, KSyncXattrName, KSyncXattrName)
 
 	// View for finding every Couchbase doc (used when deleting a database)
 	// Key is docid; value is null

db/document.go
@@ -218,10 +218,6 @@ func parseXattrStreamData(xattrName string, data []byte) (body []byte, xattr []b
 
 func (doc *syncData) HasValidSyncData(requireSequence bool) bool {
 	valid := doc != nil && doc.CurrentRev != "" && (doc.Sequence > 0 || !requireSequence)
-	// Additional diagnostics if sync metadata exists but isn't valid
-	if !valid && doc != nil {
-		base.LogTo("CRUD+", "Invalid sync metadata (may be expected):  Current rev: %s, Sequence: %v", doc.CurrentRev, doc.Sequence)
-	}
 	return valid
 }
 

db/import.go
@@ -72,6 +72,40 @@ func (db *Database) importDoc(docid string, body Body, isDelete bool, existingDo
 	var alreadyImportedDoc *document
 	docOut, _, err = db.updateAndReturnDoc(docid, true, 0, existingDoc, func(doc *document) (Body, AttachmentData, error) {
 
+		// Perform cas mismatch check first, as we want to identify cas mismatch before triggering migrate handling.
+		// If there's a cas mismatch, the doc has been updated since the version that triggered the import.  Handling depends on import mode.
+		if doc.Cas != existingDoc.Cas {
+			// If this is a feed import, cancel on cas failure (doc has been updated )
+			if mode == ImportFromFeed {
+				return nil, nil, base.ErrImportCasFailure
+			}
+			// If this is an on-demand import, we want to continue to import the current version of the doc.  Re-initialize existing doc based on the latest doc
+			if mode == ImportOnDemand {
+				body = doc.body
+				existingDoc = &sgbucket.BucketDocument{
+					Cas: doc.Cas,
+				}
+			}
+		}
+
+		// If the existing doc is a legacy SG write (_sync in body), check for migrate instead of import.
+		_, ok := body["_sync"]
+		if ok {
+			migratedDoc, requiresImport, migrateErr := db.migrateMetadata(docid, body, existingDoc)
+			if migrateErr != nil {
+				return nil, nil, migrateErr
+			}
+			// Migration successful, doesn't require import - return ErrDocumentMigrated to cancel import processing
+			if !requiresImport {
+				alreadyImportedDoc = migratedDoc
+				return nil, nil, base.ErrDocumentMigrated
+			}
+			// If document still requires import post-migration attempt, continue with import processing based on the body returned by migrate
+			doc = migratedDoc
+			body = migratedDoc.body
+			base.LogTo("Migrate", "Falling back to import with cas: %v", doc.Cas)
+		}
+
 		// Check if the doc has been deleted
 		if doc.Cas == 0 {
 			base.LogTo("Import+", "Document has been removed from the bucket before it could be imported - cancelling import.")
@@ -86,24 +120,11 @@ func (db *Database) importDoc(docid string, body Body, isDelete bool, existingDo
 		}
 
 		// If the current version of the doc is an SG write, document has been updated by SG subsequent to the update that triggered this import.
-		// Cancel update
+		// Cancel import
 		if doc.IsSGWrite() {
 			base.LogTo("Import+", "During import, existing doc (%s) identified as SG write.  Canceling import.", docid)
 			alreadyImportedDoc = doc
 			return nil, nil, base.ErrAlreadyImported
-		}
-
-		// If there's a cas mismatch, the doc has been updated since the version that triggered the import.  This is an SDK write (since we checked
-		// for SG write above).  How to handle depends on import mode.
-		if doc.Cas != existingDoc.Cas {
-			// If this is a feed import, cancel on cas failure (doc has been updated )
-			if mode == ImportFromFeed {
-				return nil, nil, base.ErrImportCasFailure
-			}
-			// If this is an on-demand import, we want to switch to importing the current version doc
-			if mode == ImportOnDemand {
-				body = doc.body
-			}
 		}
 
 		// If there's a filter function defined, evaluate to determine whether we should import this doc
@@ -139,7 +160,7 @@ func (db *Database) importDoc(docid string, body Body, isDelete bool, existingDo
 	})
 
 	switch err {
-	case base.ErrAlreadyImported:
+	case base.ErrAlreadyImported, base.ErrDocumentMigrated:
 		// If the doc was already imported, we want to return the imported version
 		docOut = alreadyImportedDoc
 	case nil:
@@ -159,6 +180,69 @@ func (db *Database) importDoc(docid string, body Body, isDelete bool, existingDo
 	}
 
 	return docOut, nil
+}
+
+// Migrates document metadata from document body to system xattr.  On CAS failure, retrieves current doc body and retries
+// migration if _sync property exists.  If _sync property is not found, returns doc and sets requiresImport to true
+func (db *Database) migrateMetadata(docid string, body Body, existingDoc *sgbucket.BucketDocument) (docOut *document, requiresImport bool, err error) {
+
+	// TODO: Add unit test for standalone migrateMetadata execution
+	for {
+		// Reload existing doc, if not present
+		if len(existingDoc.Body) == 0 {
+			cas, getErr := db.Bucket.GetWithXattr(docid, KSyncXattrName, &existingDoc.Body, &existingDoc.Xattr)
+			base.LogTo("Migrate", "Reload in migrate got cas: %d", cas)
+			if getErr != nil {
+				return nil, false, getErr
+			}
+
+			// If an xattr exists on the doc, someone has already migrated.  Cancel and return for potential import checking.
+			if len(existingDoc.Xattr) > 0 {
+				updatedDoc, unmarshalErr := unmarshalDocumentWithXattr(docid, existingDoc.Body, existingDoc.Xattr, cas)
+				if unmarshalErr != nil {
+					return nil, false, unmarshalErr
+				}
+				base.LogTo("Migrate", "Returning updated doc because xattr exists (cas=%d): %s", updatedDoc.Cas, existingDoc.Xattr)
+				return updatedDoc, true, nil
+			}
+			existingDoc.Cas = cas
+		}
+
+		// Unmarshal the existing doc in legacy SG format
+		doc, unmarshalErr := unmarshalDocument(docid, existingDoc.Body)
+		if err != nil {
+			return nil, false, unmarshalErr
+		}
+		doc.Cas = existingDoc.Cas
+
+		// If no sync metadata is present, return for import handling
+		if !doc.HasValidSyncData(false) {
+			base.LogTo("Migrate", "During migrate, doc %q doesn't have valid sync data.  Falling back to import handling.  (cas=%d)", docid, doc.Cas)
+			return doc, true, nil
+		}
+
+		// Persist the document in xattr format
+		value, xattrValue, marshalErr := doc.MarshalWithXattr()
+		if marshalErr != nil {
+			return nil, false, marshalErr
+		}
+
+		casOut, writeErr := db.Bucket.WriteCasWithXattr(docid, KSyncXattrName, 0, existingDoc.Cas, value, xattrValue)
+		if writeErr == nil {
+			doc.Cas = casOut
+			base.LogTo("Migrate", "Successfully migrated doc %q", docid)
+			return doc, false, nil
+		}
+
+		// On any error other than cas mismatch, return error
+		if !base.IsCasMismatch(db.Bucket, writeErr) {
+			return nil, false, writeErr
+		}
+
+		base.LogTo("Migrate", "CAS mismatch, retrying")
+		// On cas mismatch, reset existingDoc.Body to trigger reload
+		existingDoc = &sgbucket.BucketDocument{}
+	}
 }
 
 //////// Import Filter Function

commit 523aa11a8744f35b7f399cd272c0705336ef4c40

During tombstone, trigger body deletion if previous body non-empty
Fixes regression to SG tombstone handling introduced in https://github.com/couchbase/sync_gateway/commit/db39d17b22a44420f5620d9e649d782e4a3650a3.  After refactoring, was determining whether to delete the body based on the current document body, not the previous.

Previous unit tests weren't validating empty document body after tombstone - added additional test (and included that test in integration suite).

Fixes #2827

base/bucket_gocb.go
@@ -1436,14 +1436,15 @@ func (bucket CouchbaseBucketGoCB) WriteUpdateWithXattr(k string, xattrKey string
 		}
 
 		// Invoke callback to get updated value
-		updatedValue, updatedXattrValue, deleteDoc, err := callback(value, xattrValue, cas)
+		updatedValue, updatedXattrValue, isDelete, err := callback(value, xattrValue, cas)
 
 		if err != nil {
 			return emptyCas, err
 		}
 
-		// Attempt to write the updated document to the bucket
-		casOut, writeErr := bucket.WriteWithXattr(k, xattrKey, exp, cas, updatedValue, updatedXattrValue, deleteDoc)
+		// Attempt to write the updated document to the bucket.  Mark body for deletion if previous body was non-empty
+		deleteBody := len(value) > 0
+		casOut, writeErr := bucket.WriteWithXattr(k, xattrKey, exp, cas, updatedValue, updatedXattrValue, isDelete, deleteBody)
 		switch writeErr {
 		case nil:
 			return casOut, nil
@@ -1464,10 +1465,9 @@ func (bucket CouchbaseBucketGoCB) WriteUpdateWithXattr(k string, xattrKey string
 
 // Single attempt to update a document and xattr.  Setting isDelete=true and value=nil will delete the document body.  Both
 // update types (UpdateXattr, WriteCasWithXattr) include recoverable error retry.
-func (bucket CouchbaseBucketGoCB) WriteWithXattr(k string, xattrKey string, exp int, cas uint64, value []byte, xattrValue []byte, isDelete bool) (casOut uint64, err error) {
+func (bucket CouchbaseBucketGoCB) WriteWithXattr(k string, xattrKey string, exp int, cas uint64, value []byte, xattrValue []byte, isDelete bool, deleteBody bool) (casOut uint64, err error) {
 	// If this is a tombstone, we want to delete the document and update the xattr
 	if isDelete {
-		deleteBody := len(value) > 0
 		return bucket.UpdateXattr(k, xattrKey, exp, cas, xattrValue, deleteBody)
 	} else {
 		// Not a delete - update the body and xattr

rest/import_test.go
@@ -118,6 +118,48 @@ func TestXattrImportOldDoc(t *testing.T) {
 
 }
 
+// Validate tombstone w/ xattrs
+func TestXattrSGTombstone(t *testing.T) {
+
+	SkipImportTestsIfNotEnabled(t)
+
+	rt := RestTester{SyncFn: `
+		function(doc, oldDoc) { channel(doc.channels) }`}
+	defer rt.Close()
+
+	bucket := rt.Bucket()
+
+	rt.SendAdminRequest("PUT", "/_logging", `{"Import+":true, "CRUD+":true}`)
+
+	// 1. Create doc through SG
+	key := "TestXattrSGTombstone"
+	docBody := make(map[string]interface{})
+	docBody["test"] = key
+	docBody["channels"] = "ABC"
+
+	response := rt.SendAdminRequest("PUT", fmt.Sprintf("/db/%s", key), `{"channels":"ABC"}`)
+	assert.Equals(t, response.Code, 201)
+	log.Printf("insert response: %s", response.Body.Bytes())
+	var body db.Body
+	json.Unmarshal(response.Body.Bytes(), &body)
+	assert.Equals(t, body["ok"], true)
+	revId := body["rev"].(string)
+
+	// 2. Delete the doc through SG
+	response = rt.SendAdminRequest("DELETE", fmt.Sprintf("/db/%s?rev=%s", key, revId), "")
+	assert.Equals(t, response.Code, 200)
+	log.Printf("delete response: %s", response.Body.Bytes())
+	json.Unmarshal(response.Body.Bytes(), &body)
+	assert.Equals(t, body["ok"], true)
+	revId = body["rev"].(string)
+
+	// 3. Attempt to retrieve the doc through the SDK
+	deletedValue := make(map[string]interface{})
+	_, err := bucket.Get(key, deletedValue)
+	assertTrue(t, err != nil, "Expected key not found error trying to retrieve document")
+
+}
+
 // Test cas failure during WriteUpdate, triggering import of SDK write.
 // Disabled, as test depends on artificial latency in PutDoc to reliably hit the CAS failure on the SG write.  Scenario fully covered
 // by functional test.

test_integration.sh
@@ -209,7 +209,8 @@ declare -a arr=(
     "TestChangesIncludeConflicts",
     "TestGetRemoved",
     "TestGetRemovedAndDeleted",
-    "TestGetRemovedAsUser")
+    "TestGetRemovedAsUser",
+    "TestXattrSGTombstone")
 
 
 # --------------------------------- SG Accel tests -----------------------------------------