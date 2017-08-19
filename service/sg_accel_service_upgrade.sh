da05cde30eaaeeb6342436caa69e6d5cf5fcea7e
CogCR <Captr@cogno-sys.com>
2017-08-19 15:36:36 -0700

    Update sg_accel_service_upgrade.sh

--- service/sg_accel_service_upgrade.sh
+++ service/sg_accel_service_upgrade.sh
@@ -4,7 +4,7 @@
 OS=""
 VER=""
 SERVICE_NAME="sg_accel"
-SRCCFGDIR=../examples
+SRCCFGDIR=/opt/couchbase-sg-accel/examples
 SRCCFG=basic_sg_accel_config.json
 RUNAS_TEMPLATE_VAR=sg_accel
 RUNBASE_TEMPLATE_VAR=/home/sg_accel
