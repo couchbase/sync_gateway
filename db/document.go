c3f2e38c419618ee475f0f059b78244d8b2ec40c
Traun Leyden <tleyden@couchbase.com>
2017-08-22 15:48:24 -0700

    Point to later accel

--- db/document.go
+++ db/document.go
@@ -78,7 +78,6 @@ func unmarshalDocument(docid string, data []byte) (*document, error) {
 // (This is somewhat faster, if all you need is the sync data without the doc body.)
 // Needhistory is an optimization to figure out whether doc is insert by looking at the depth of the rev tree
 // (if insert, skip looking through blocks).  Potentially expensive if the history is large.
-// TODO: set needHistory = false when we call this
 func UnmarshalDocumentSyncData(data []byte, needHistory bool) (*syncData, error) {
 	var root documentRoot
 	if needHistory {
