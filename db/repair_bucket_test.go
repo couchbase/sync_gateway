ae783b94259c2ba0238eecc6fa9261817942c8e1
Traun Leyden <tleyden@couchbase.com>
2017-08-30 16:52:45 -0700

    More test docs

--- db/repair_bucket_test.go
+++ db/repair_bucket_test.go
@@ -12,27 +12,34 @@ import (
 
 const (
 	docIdProblematicRevTree = "docIdProblematicRevTree"
+	docIdProblematicRevTree2 = "docIdProblematicRevTree2"
 )
 
 func testBucketWithViewsAndBrokenDoc() (bucket base.Bucket, numDocs int) {
 
 	numDocsAdded := 0
 	bucket = testBucket()
-	log.Printf("installing views")
 	installViews(bucket)
-	log.Printf("installed views")
 
 	// Add a harmless doc
 	testSyncData := syncData{}
 	bucket.Add("foo", 0, map[string]interface{}{"foo": "bar", "_sync": testSyncData})
 	numDocsAdded++
 
-	// Add a doc that should be repaired
+	// Add doc that should be repaired
 	rawDoc, err := unmarshalDocument(docIdProblematicRevTree, []byte(testdocProblematicRevTree))
 	if err != nil {
 		panic(fmt.Sprintf("Error unmarshalling doc: %v", err))
 	}
 	bucket.Add(docIdProblematicRevTree, 0, rawDoc)
+	numDocsAdded++
+
+	// Add 2nd doc that should be repaired
+	rawDoc, err = unmarshalDocument(docIdProblematicRevTree2, []byte(testdocProblematicRevTree))
+	if err != nil {
+		panic(fmt.Sprintf("Error unmarshalling doc: %v", err))
+	}
+	bucket.Add(docIdProblematicRevTree2, 0, rawDoc)
 	numDocsAdded++
 
 	return bucket, numDocsAdded
@@ -82,7 +89,7 @@ func TestRepairBucketRevTreeCycles(t *testing.T) {
 	repairedDocs, err := repairBucket.RepairBucket()
 
 	assertNoError(t, err, fmt.Sprintf("Error repairing bucket: %v", err))
-	assert.True(t, len(repairedDocs) == 1)
+	assert.True(t, len(repairedDocs) == 2)
 
 	// Now get the doc from the bucket
 	var value interface{}
@@ -98,9 +105,9 @@ func TestRepairBucketRevTreeCycles(t *testing.T) {
 	// Since doc was repaired, should contain no cycles
 	assert.False(t, repairedDoc.History.ContainsCycles())
 
-	// There should be a backup doc in the bucket with ID sync:repair:backup:docIdProblematicRevTree
+	// There should be a backup doc in the bucket with ID _sync:repair:backup:docIdProblematicRevTree
 	var backupDocRaw interface{}
-	_, errGetDoc = bucket.Get("sync:repair:backup:docIdProblematicRevTree", &backupDocRaw)
+	_, errGetDoc = bucket.Get("_sync:repair:backup:docIdProblematicRevTree", &backupDocRaw)
 	assertNoError(t, errGetDoc, fmt.Sprintf("Error getting backup doc: %v", errGetDoc))
 
 	marshalledBackup, _ := json.MarshalIndent(backupDocRaw, "", "")
