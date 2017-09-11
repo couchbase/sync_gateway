a9347cf2d1e14f1b4543a40852a03ddaba347897
Traun Leyden <traun.leyden@gmail.com>
2017-09-11 16:39:13 -0700

    Fixes #2892 repair tool efficiency (#2893)
    * Fixes #2892 - Repair tool efficiency improvements
    
    Initial first pass at iterating over the view w/ paging
    
    * Add documentation
    
    * More comments
    
    * Comment regarding min pageSIzeViewResult
    
    * pull ViewQueryPageSize out to a constant.  Enhance tests to add more docs to excercise the view iteration
    
    * Address PR feedback
    
    * Update comment
    
    * Change constant to DefaultViewQueryPageSize
    
    * Run gofmt

--- base/constants.go
+++ base/constants.go
@@ -5,4 +5,7 @@ const (
 	// The username of the special "GUEST" user
 	GuestUsername = "GUEST"
 	ISO8601Format = "2006-01-02T15:04:05.000Z07:00"
+
+	DefaultViewQueryPageSize = 5000 // This must be greater than 1, or the code won't work due to windowing method
+
 )
