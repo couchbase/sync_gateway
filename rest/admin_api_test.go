a72e34a07037b3d81fe0e8811c92d35a532ce288
Traun Leyden <tleyden@couchbase.com>
2017-10-11 15:00:11 -0700

    Add more logging to TestUserAPI

--- rest/admin_api_test.go
+++ rest/admin_api_test.go
@@ -37,6 +37,7 @@ func TestUserAPI(t *testing.T) {
 	assertStatus(t, response, 201)
 
 	// GET the user and make sure the result is OK
+	log.Printf("GET the user and make sure the result is OK")
 	response = rt.SendAdminRequest("GET", "/db/_user/snej", "")
 	assertStatus(t, response, 200)
 	var body db.Body
@@ -48,11 +49,13 @@ func TestUserAPI(t *testing.T) {
 	assert.Equals(t, body["password"], nil)
 
 	// Check the list of all users:
+	log.Printf("Check the list of all users")
 	response = rt.SendAdminRequest("GET", "/db/_user/", "")
 	assertStatus(t, response, 200)
 	assert.Equals(t, string(response.Body.Bytes()), `["snej"]`)
 
 	// Check that the actual User object is correct:
+	log.Printf("Check that the actual User object is correct")
 	user, _ := rt.ServerContext().Database("db").Authenticator().GetUser("snej")
 	assert.Equals(t, user.Name(), "snej")
 	assert.Equals(t, user.Email(), "jens@couchbase.com")
@@ -60,6 +63,7 @@ func TestUserAPI(t *testing.T) {
 	assert.True(t, user.Authenticate("letmein"))
 
 	// Change the password and verify it:
+	log.Printf("Change the password and verify it")
 	response = rt.SendAdminRequest("PUT", "/db/_user/snej", `{"email":"jens@couchbase.com", "password":"123", "admin_channels":["foo", "bar"]}`)
 	assertStatus(t, response, 200)
 
@@ -67,10 +71,12 @@ func TestUserAPI(t *testing.T) {
 	assert.True(t, user.Authenticate("123"))
 
 	// DELETE the user
+	log.Printf("DELETE the user")
 	assertStatus(t, rt.SendAdminRequest("DELETE", "/db/_user/snej", ""), 200)
 	assertStatus(t, rt.SendAdminRequest("GET", "/db/_user/snej", ""), 404)
 
 	// POST a user
+	log.Printf("POST a user")
 	response = rt.SendAdminRequest("POST", "/db/_user", `{"name":"snej", "password":"letmein", "admin_channels":["foo", "bar"]}`)
 	assertStatus(t, response, 301)
 	response = rt.SendAdminRequest("POST", "/db/_user/", `{"name":"snej", "password":"letmein", "admin_channels":["foo", "bar"]}`)
@@ -82,15 +88,18 @@ func TestUserAPI(t *testing.T) {
 	assert.Equals(t, body["name"], "snej")
 
 	// Create a role
+	log.Printf("Create a role")
 	assertStatus(t, rt.SendAdminRequest("GET", "/db/_role/hipster", ""), 404)
 	response = rt.SendAdminRequest("PUT", "/db/_role/hipster", `{"admin_channels":["fedoras", "fixies"]}`)
 	assertStatus(t, response, 201)
 
 	// Give the user that role
+	log.Printf("Give the user that role")
 	response = rt.SendAdminRequest("PUT", "/db/_user/snej", `{"admin_channels":["foo", "bar"],"admin_roles":["hipster"]}`)
 	assertStatus(t, response, 200)
 
 	// GET the user and verify that it shows the channels inherited from the role
+	log.Printf("GET the user and verify that it shows the channels inherited from the role")
 	response = rt.SendAdminRequest("GET", "/db/_user/snej", "")
 	assertStatus(t, response, 200)
 	body = nil
@@ -99,7 +108,9 @@ func TestUserAPI(t *testing.T) {
 	assert.DeepEquals(t, body["all_channels"], []interface{}{"!", "bar", "fedoras", "fixies", "foo"})
 
 	// DELETE the user
+	log.Printf("DELETE the user")
 	assertStatus(t, rt.SendAdminRequest("DELETE", "/db/_user/snej", ""), 200)
+	log.Printf("Test Finished")
 
 }
 func TestUserPasswordValidation(t *testing.T) {
