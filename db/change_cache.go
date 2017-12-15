530076d28d8b52ad4e581919efb0d62240479c64
Adam Fraser <adamf@couchbase.com>
2017-12-15 15:17:39 -0800

    Update change_cache.go

--- db/change_cache.go
+++ db/change_cache.go
@@ -118,7 +118,7 @@ func (c *changeCache) Init(context *DatabaseContext, lastSequence SequenceID, on
 	base.LogTo("Cache", "Initializing changes cache with options %+v", c.options)
 
 	if context.UseGlobalSequence() {
-		base.Logf("_sync:seq for %s: %d", context.Name, c.initialSequence)
+		base.Logf("Initializing changes cache for database %s with sequence: %d", context.Name, c.initialSequence)
 	}
 
 	heap.Init(&c.pendingLogs)
