159820138663a349e43946eb641f173251975dfc
Traun Leyden <tleyden@couchbase.com>
2018-01-26 14:42:17 -0800

    Refactor out a RestDocument used by test

--- base/document.go
+++ base/document.go
@@ -1,5 +1,14 @@
 package base
 
+type RestBody map[string]interface{}
+
+type RestDocument struct {
+	RestBody
+	Attachments AttachmentMap `json:"_attachments,omitempty"`
+}
+
+type AttachmentMap map[string]DocAttachment
+
 type DocAttachment struct {
 	ContentType string `json:"content_type,omitempty"`
 	Digest      string `json:"digest,omitempty"`
@@ -8,3 +17,23 @@ type DocAttachment struct {
 	Stub        bool   `json:"stub,omitempty"`
 	Data        bool   `json:"data,omitempty"`
 }
+
+func NewRestDocument() *RestDocument {
+	emptyBody := make(map[string]interface{})
+	return &RestDocument{
+		RestBody: emptyBody,
+	}
+}
+
+func (d *RestDocument) SetID(docId string) {
+	d.RestBody["_id"] = docId
+}
+
+func (d *RestDocument) SetRevID(revId string) {
+	d.RestBody["_rev"] = revId
+}
+
+func (d *RestDocument) SetAttachments(attachments AttachmentMap) {
+	d.Attachments = attachments
+}
+
