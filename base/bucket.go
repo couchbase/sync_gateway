041adc0ec87d163b5c8fa5c88c0d0708734fcc1f
Adam Fraser <adamf@couchbase.com>
2018-04-13 16:13:43 -0700

    Enforce server version when using GSI (#3449)
    * Enforce server version when using GSI
    
    * Fix unit test cut-and-paste error

--- base/bucket.go
+++ base/bucket.go
@@ -398,6 +398,17 @@ func (bucket *CouchbaseBucket) CouchbaseServerVersion() (major uint64, minor uin
 
 }
 
+func IsMinimumServerVersion(bucket Bucket, minMajor uint64, minMinor uint64) (bool, error) {
+
+	major, minor, _, err := bucket.CouchbaseServerVersion()
+	if err != nil {
+		return false, err
+	}
+
+	return isMinimumVersion(major, minor, minMajor, minMinor), nil
+
+}
+
 func ParseCouchbaseServerVersion(versionString string) (major uint64, minor uint64, micro string, err error) {
 
 	if versionString == "" {
@@ -526,12 +537,12 @@ func GetBucket(spec BucketSpec, callback sgbucket.BucketNotifyFn) (bucket Bucket
 		// If XATTRS are enabled via enable_shared_bucket_access config flag, assert that Couchbase Server is 5.0
 		// or later, otherwise refuse to connect to the bucket since pre 5.0 versions don't support XATTRs
 		if spec.UseXattrs {
-			majorVersion, _, _, errServerVersion := bucket.CouchbaseServerVersion()
+			xattrsSupported, errServerVersion := IsMinimumServerVersion(bucket, 5, 0)
 			if errServerVersion != nil {
 				return nil, errServerVersion
 			}
-			if majorVersion < 5 {
-				Warnf(KeyAll, "If using XATTRS, Couchbase Server version must be >= 5.0.  Major Version: %v", majorVersion)
+			if !xattrsSupported {
+				Warnf(KeyAll, "If using XATTRS, Couchbase Server version must be >= 5.0.")
 				return nil, ErrFatalBucketConnection
 			}
 		}
