969a358ec648161f2f0e361411677f61ef76d42d
Adam Fraser <adamf@couchbase.com>
2018-05-14 17:00:36 -0700

    Unit test fixes after rebase

--- base/bucket_test.go
+++ base/bucket_test.go
@@ -1,7 +1,6 @@
 package base
 
 import (
-	"log"
 	"testing"
 
 	"github.com/couchbaselabs/go.assert"
@@ -18,20 +17,20 @@ func TestBucketSpec(t *testing.T) {
 
 	connStr, err := bucketSpec.GetGoCBConnString()
 	assertNoError(t, err, "Error creating connection string for bucket spec")
-	assert.Equals(t, connStr, "http://localhost:8091?cacertpath=.%2FmyCACertPath&certpath=%2FmyCertPath&keypath=%2Fmy%2Fkey%2Fpath")
+	assert.Equals(t, connStr, "http://localhost:8091?cacertpath=.%2FmyCACertPath&certpath=%2FmyCertPath&http_idle_conn_timeout=90000&http_max_idle_conns=64000&http_max_idle_conns_per_host=256&keypath=%2Fmy%2Fkey%2Fpath")
 
 	// CACertPath not required
 	bucketSpec.CACertPath = ""
 	connStr, err = bucketSpec.GetGoCBConnString()
 	assertNoError(t, err, "Error creating connection string for bucket spec")
-	assert.Equals(t, connStr, "http://localhost:8091?certpath=%2FmyCertPath&keypath=%2Fmy%2Fkey%2Fpath")
+	assert.Equals(t, connStr, "http://localhost:8091?certpath=%2FmyCertPath&http_idle_conn_timeout=90000&http_max_idle_conns=64000&http_max_idle_conns_per_host=256&keypath=%2Fmy%2Fkey%2Fpath")
 
 	// Certpath and keypath must both be defined - if either are missing, they shouldn't be included in connection string
 	bucketSpec.CACertPath = "./myCACertPath"
 	bucketSpec.Certpath = ""
 	connStr, err = bucketSpec.GetGoCBConnString()
 	assertNoError(t, err, "Error creating connection string for bucket spec")
-	assert.Equals(t, connStr, "http://localhost:8091?cacertpath=.%2FmyCACertPath")
+	assert.Equals(t, connStr, "http://localhost:8091?cacertpath=.%2FmyCACertPath&http_idle_conn_timeout=90000&http_max_idle_conns=64000&http_max_idle_conns_per_host=256")
 
 	// Standard no-cert
 	bucketSpec.CACertPath = ""
@@ -39,12 +38,5 @@ func TestBucketSpec(t *testing.T) {
 	bucketSpec.Keypath = ""
 	connStr, err = bucketSpec.GetGoCBConnString()
 	assertNoError(t, err, "Error creating connection string for bucket spec")
-	assert.Equals(t, connStr, "http://localhost:8091")
-
-	// Bad URL should return error
-	bucketSpec.Server = ":noScheme"
-	connStr, err = bucketSpec.GetGoCBConnString()
-	log.Printf("connstr: %s, err: %v", connStr, err)
-	assertTrue(t, err != nil, "Expected error for bad URL")
 
 }
