cee2cda608f30531ba607cbc783a486497391996
Ben Brooks <ben.brooks@couchbase.com>
2018-05-31 18:06:12 +0100

    Fix copy paste error in comment

--- base/log_level.go
+++ base/log_level.go
@@ -83,7 +83,7 @@ func (l *LogLevel) UnmarshalText(text []byte) error {
 	return fmt.Errorf("unrecognized log level: %q (valid options: %v)", string(text), logLevelNames)
 }
 
-// logLevelPtr is a convenience function that returns a pointer to the given logKey
+// logLevelPtr is a convenience function that returns a pointer to the given logLevel
 func logLevelPtr(logLevel LogLevel) *LogLevel {
 	return &logLevel
 }
