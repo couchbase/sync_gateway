8ae8674e19ec8e8fba762ec9f8d4c23b7413bd54
JFlath <jmflather@gmail.com>
2018-09-20 15:06:13 +0100

    Split test for lagging and non-lagging replicas

--- base/bucket_test.go
+++ base/bucket_test.go
@@ -150,6 +150,47 @@ func TestGetStatsVbSeqno(t *testing.T) {
 					"vb_0:high_seqno": "990",
 					"vb_1:uuid":       "91",
 					"vb_1:high_seqno": "991",
+					// replica vbuckets
+					"vb_2:uuid":       "92",
+					"vb_2:high_seqno": "992",
+					"vb_3:uuid":       "93",
+					"vb_3:high_seqno": "993",
+				},
+				"host2:11210": map[string]string{
+					// active vbuckets
+					"vb_2:uuid":       "92",
+					"vb_2:high_seqno": "992",
+					"vb_3:uuid":       "93",
+					"vb_3:high_seqno": "993",
+					// replica vbuckets
+					"vb_0:uuid":       "90",
+					"vb_0:high_seqno": "990",
+					"vb_1:uuid":       "91",
+					"vb_1:high_seqno": "991",
+				},
+			},
+			expectedUUIDs: map[uint16]uint64{
+				0: 90,
+				1: 91,
+				2: 92,
+				3: 93,
+			},
+			expectedHighSeqnos: map[uint16]uint64{
+				0: 990,
+				1: 991,
+				2: 992,
+				3: 993,
+			},
+		},
+		{
+			name: "2 nodes with lagging replica vbuckets",
+			stats: map[string]map[string]string{
+				"host1:11210": map[string]string{
+					// active vbuckets
+					"vb_0:uuid":       "90",
+					"vb_0:high_seqno": "990",
+					"vb_1:uuid":       "91",
+					"vb_1:high_seqno": "991",
 					// slightly behind replicas
 					"vb_2:uuid":       "92",
 					"vb_2:high_seqno": "982",
