41edb1584efd0410d22e8e3a85817df1b02b656a
Ben Brooks <ben.brooks@couchbase.com>
2018-09-20 13:53:12 +0100

    Add test for replica vbuckets

--- base/bucket_test.go
+++ base/bucket_test.go
@@ -1,7 +1,6 @@
 package base
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/couchbaselabs/go.assert"
@@ -48,11 +47,13 @@ func TestGetStatsVbSeqno(t *testing.T) {
 	const maxVbno = 4
 
 	tests := []struct {
+		name               string
 		stats              map[string]map[string]string
 		expectedUUIDs      map[uint16]uint64
 		expectedHighSeqnos map[uint16]uint64
 	}{
 		{
+			name: "1 node",
 			stats: map[string]map[string]string{
 				"host1:11210": map[string]string{
 					"vb_0:uuid":       "90",
@@ -79,6 +80,7 @@ func TestGetStatsVbSeqno(t *testing.T) {
 			},
 		},
 		{
+			name: "2 nodes",
 			stats: map[string]map[string]string{
 				"host1:11210": map[string]string{
 					"vb_0:uuid":       "90",
@@ -107,6 +109,7 @@ func TestGetStatsVbSeqno(t *testing.T) {
 			},
 		},
 		{
+			name: "4 nodes",
 			stats: map[string]map[string]string{
 				"host1:11210": map[string]string{
 					"vb_0:uuid":       "90",
@@ -138,10 +141,51 @@ func TestGetStatsVbSeqno(t *testing.T) {
 				3: 993,
 			},
 		},
+		{
+			name: "2 nodes with replica vbuckets",
+			stats: map[string]map[string]string{
+				"host1:11210": map[string]string{
+					// active vbuckets
+					"vb_0:uuid":       "90",
+					"vb_0:high_seqno": "990",
+					"vb_1:uuid":       "91",
+					"vb_1:high_seqno": "991",
+					// slightly behind replicas
+					"vb_2:uuid":       "92",
+					"vb_2:high_seqno": "982",
+					"vb_3:uuid":       "93",
+					"vb_3:high_seqno": "983",
+				},
+				"host2:11210": map[string]string{
+					// active vbuckets
+					"vb_2:uuid":       "92",
+					"vb_2:high_seqno": "992",
+					"vb_3:uuid":       "93",
+					"vb_3:high_seqno": "993",
+					// slightly behind replicas
+					"vb_0:uuid":       "90",
+					"vb_0:high_seqno": "980",
+					"vb_1:uuid":       "91",
+					"vb_1:high_seqno": "981",
+				},
+			},
+			expectedUUIDs: map[uint16]uint64{
+				0: 90,
+				1: 91,
+				2: 92,
+				3: 93,
+			},
+			expectedHighSeqnos: map[uint16]uint64{
+				0: 990,
+				1: 991,
+				2: 992,
+				3: 993,
+			},
+		},
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("%d nodes", len(test.stats)), func(ts *testing.T) {
+		t.Run(test.name, func(ts *testing.T) {
 			actualUUIDs, actualHighSeqnos, err := GetStatsVbSeqno(test.stats, maxVbno, false)
 			assert.Equals(ts, err, nil)
 			assert.DeepEquals(ts, actualUUIDs, test.expectedUUIDs)
