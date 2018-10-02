3031c4e1f830bb3096bf9ddcbc637101b35148e7
Adam Fraser <adamf@couchbase.com>
2018-10-02 09:33:55 -0700

    Optional old revision body backups during import (#3775)
    Allow import to persist a temporary backup of the previous revision body, when it's available via the rev cache.
    
    Given the additional import/DCP work involved (an additional rev cache lookup and kv op per imported document), this is an opt-in feature, enabled using import_backup_old_rev config property.

--- rest/config.go
+++ rest/config.go
@@ -172,6 +172,7 @@ type DbConfig struct {
 	RevsLimit                 *uint32                        `json:"revs_limit,omitempty"`                   // Max depth a document's revision tree can grow to
 	AutoImport                interface{}                    `json:"import_docs,omitempty"`                  // Whether to automatically import Couchbase Server docs into SG.  Xattrs must be enabled.  true or "continuous" both enable this.
 	ImportFilter              *string                        `json:"import_filter,omitempty"`                // Filter function (import)
+	ImportBackupOldRev        bool                           `json:"import_backup_old_rev"`                  // Whether import should attempt to create a temporary backup of the previous revision body, when available.
 	Shadow                    *ShadowConfig                  `json:"shadow,omitempty"`                       // This is where the ShadowConfig used to be.  If found, it should throw an error
 	EventHandlers             interface{}                    `json:"event_handlers,omitempty"`               // Event handlers (webhook)
 	FeedType                  string                         `json:"feed_type,omitempty"`                    // Feed type - "DCP" or "TAP"; defaults based on Couchbase server version
