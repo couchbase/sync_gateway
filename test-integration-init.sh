de139b50af8bad22948dfa1c162f1888634c487b
Traun Leyden <tleyden@couchbase.com>
2018-10-31 14:45:35 -0700

    Increase bucket size

--- test-integration-init.sh
+++ test-integration-init.sh
@@ -14,10 +14,10 @@ fi
 CB_ADMIN_USERNAME="Administrator"
 CB_ADMIN_PASSWORD="password"
 
-SG_CLUSTER_RAMSIZE=2000
+SG_CLUSTER_RAMSIZE_MB=4000
 
 SG_TEST_BUCKETS=("test_data_bucket" "test_shadowbucket" "test_indexbucket")
-SG_TEST_BUCKET_RAMSIZE=500 # MB
+SG_TEST_BUCKET_RAMSIZE_MB=1000
 
 SG_TEST_BUCKET_PASSWORD="password"
 SG_TEST_BUCKET_RBAC_ROLES=() # No bucket-specific roles when we can rely on global admin
@@ -51,7 +51,7 @@ function init_cluster {
     set +e
     # FTS is currently needed, but once MB-31682 is implemented, that can be removed.
     "$cb_cli_tool" cluster-init -c $CB_SERVER_URL --cluster-name default --cluster-username $CB_ADMIN_USERNAME \
-        --cluster-password $CB_ADMIN_PASSWORD --cluster-ramsize $SG_CLUSTER_RAMSIZE --cluster-index-ramsize 512 \
+        --cluster-password $CB_ADMIN_PASSWORD --cluster-ramsize $SG_CLUSTER_RAMSIZE_MB --cluster-index-ramsize 512 \
         --cluster-fts-ramsize 256 --services data,index,query,mobile_service,fts
     set -e
 }
@@ -73,7 +73,7 @@ function cb_create_buckets {
     for bucket in "${SG_TEST_BUCKETS[@]}"; do
         "$cb_cli_tool" bucket-create -c $CB_SERVER_URL --username $CB_ADMIN_USERNAME \
             --password $CB_ADMIN_PASSWORD --bucket=$bucket --bucket-type=couchbase \
-            --bucket-ramsize=$SG_TEST_BUCKET_RAMSIZE --enable-flush=1 \
+            --bucket-ramsize=$SG_TEST_BUCKET_RAMSIZE_MB --enable-flush=1 \
             --bucket-replica=0 --enable-index-replica=0 --wait
     done
 }
