0da25c2ee424e23af173de5c5819a63d3c200b70
Ben Brooks <ben.brooks@couchbase.com>
2019-02-12 18:54:27 +0000

    Set fleecedelta string diffing params on init

--- base/deltas_ee.go
+++ base/deltas_ee.go
@@ -2,9 +2,19 @@
 
 package base
 
-import fleecedelta "github.com/couchbaselabs/go-fleecedelta"
+import (
+	"time"
+
+	fleecedelta "github.com/couchbaselabs/go-fleecedelta"
+)
 
 // The reason for these wrappers is to keep go-fleecedelta out of the compiled CE builds by providing no-op versions in deltas_ce.go
+
+func init() {
+	fleecedelta.StringDiffTimeout = time.Millisecond * 10 // Aggressive string diff timeout
+	fleecedelta.StringDiffMaxLength = 1024 * 1024         // Impose a 1MB max length for string diffs
+	fleecedelta.StringDiffMinLength = 60                  //
+}
 
 // Diff will return the fleece delta between old and new.
 func Diff(old, new map[string]interface{}) (delta []byte, err error) {
