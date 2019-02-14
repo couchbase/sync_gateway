5c1a46c4f401be88fc2e1d2b0185a25d8101c0d2
Ben Brooks <ben@bbrks.me>
2019-02-14 17:36:44 +0000

    Delta Sync: Add more string diff tuning parameters (#3964)
    * Set fleecedelta string diffing params on init
    
    * Fix manifest conflict
    
    * Add explicit StringDiffEnabled=true
    
    * Bump go-fleecedelta commit

--- base/deltas_ee.go
+++ base/deltas_ee.go
@@ -2,9 +2,20 @@
 
 package base
 
-import fleecedelta "github.com/couchbaselabs/go-fleecedelta"
+import (
+	"time"
+
+	"github.com/couchbaselabs/go-fleecedelta"
+)
 
 // The reason for these wrappers is to keep go-fleecedelta out of the compiled CE builds by providing no-op versions in deltas_ce.go
+
+func init() {
+	fleecedelta.StringDiffEnabled = true             // Global toggle for string diffs
+	fleecedelta.StringDiffMinLength = 60             // 60 B min length to match CBL
+	fleecedelta.StringDiffMaxLength = 1024 * 1024    // 1 MB max length for string diffs
+	fleecedelta.StringDiffTimeout = time.Millisecond // Aggressive string diff timeout
+}
 
 // Diff will return the fleece delta between old and new.
 func Diff(old, new map[string]interface{}) (delta []byte, err error) {
