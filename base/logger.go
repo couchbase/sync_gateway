cd618d41a438d34d02f4cd7b7a0486edeed7740a
Adam Fraser <adamf@couchbase.com>
2019-02-20 09:21:55 -0800

    Revert "CBG-220: Limit total size of rotated logs (#3972)"
    This reverts commit 61f4d93dd2528eb0a2139edb91592e2c35d5b5b5.

--- base/logger.go
+++ base/logger.go
@@ -1,15 +1,9 @@
 package base
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"path/filepath"
 	"strings"
 	"time"
-
-	"github.com/pkg/errors"
 )
 
 // FlushLogBuffers will cause all log collation buffers to be flushed to the output.
@@ -39,43 +33,4 @@ func logCollationWorker(collateBuffer chan string, logger *log.Logger, bufferSiz
 			}
 		}
 	}
-}
-
-// runLogDeletion will delete rotated logs for the supplied logLevel. It will only perform these deletions when the
-// cumulative size of the logs are above the supplied sizeLimitMB.
-// logDirectory is the supplied directory where the logs are stored.
-func runLogDeletion(logDirectory string, logLevel string, sizeLimitMBLowWatermark int, sizeLimitMBHighWatermark int) (err error) {
-
-	sizeLimitMBLowWatermark = sizeLimitMBLowWatermark * 1024 * 1024   //Convert MB input to bytes
-	sizeLimitMBHighWatermark = sizeLimitMBHighWatermark * 1024 * 1024 //Convert MB input to bytes
-
-	files, err := ioutil.ReadDir(logDirectory)
-
-	if err != nil {
-		return errors.New(fmt.Sprintf("Error reading log directory: %v", err))
-	}
-
-	// Traverse backwards through sorted log filenames. When low watermark is reached we record the point at which this
-	// was passed. Once we also pass the high watermark we loop through from the low watermark and remove.
-	totalSize := 0
-	indexDeletePoint := -1
-	for i := len(files) - 1; i >= 0; i-- {
-		file := files[i]
-		if strings.Contains(file.Name(), logLevel) && strings.HasSuffix(file.Name(), ".gz") {
-			totalSize += int(file.Size())
-			if totalSize > sizeLimitMBLowWatermark {
-				indexDeletePoint = i
-			}
-			if totalSize > sizeLimitMBHighWatermark {
-				for j := indexDeletePoint; j >= 0; j-- {
-					err = os.Remove(filepath.Join(logDirectory, file.Name()))
-					if err != nil {
-						return errors.New(fmt.Sprintf("Error deleting stale log file: %v", err))
-					}
-				}
-				break
-			}
-		}
-	}
-	return nil
 }
