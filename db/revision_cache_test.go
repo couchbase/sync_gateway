e92b6380b460a5569e8c1fbb915f60926cc7cac6
JRasagneres <jacquesrascagneres@gmail.com>
2019-05-07 18:40:57 +0100

    Import order whitespace

--- db/revision_cache_test.go
+++ db/revision_cache_test.go
@@ -8,10 +8,9 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/couchbase/sync_gateway/base"
 	goassert "github.com/couchbaselabs/go.assert"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/couchbase/sync_gateway/base"
 )
 
 // testBackingStore always returns an empty doc at rev:"1-abc" in channel "*" except for docs not in 'notFoundDocIDs'
