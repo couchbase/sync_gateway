54e4c7cc89dc7100a14c991fa0690d5c14e45655
JRasagneres <jacquesrascagneres@gmail.com>
2019-06-04 11:23:58 +0100

    Copy default transport

--- db/event_handler.go
+++ db/event_handler.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"time"
 
@@ -57,9 +58,21 @@ func NewWebhook(url string, filterFnString string, timeout *uint64) (*Webhook, e
 	}
 
 	// Initialize transport and client
-	t := http.DefaultTransport.(*http.Transport)
-	t.DisableKeepAlives = false
-	wh.client = &http.Client{Transport: t, Timeout: wh.timeout}
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		DisableKeepAlives:     false,
+	}
+
+	wh.client = &http.Client{Transport: transport, Timeout: wh.timeout}
 
 	return wh, err
 }
