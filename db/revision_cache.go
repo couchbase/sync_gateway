951b54bcce049d12dafb3028284ecfd10c6eb550
Ben Brooks <ben@bbrks.me>
2019-07-11 22:26:41 +0100

    CBG-450 - Backport CBG-449 Fix revisions sent as deltas not handling the _removed case

--- db/revision_cache.go
+++ db/revision_cache.go
@@ -115,6 +115,7 @@ type RevisionDelta struct {
 	ToRevID           string   // Target revID for the delta
 	DeltaBytes        []byte   // The actual delta
 	AttachmentDigests []string // Digests for all attachments present on ToRevID
+	ToChannels        base.Set // Full list of channels for the to revision
 	RevisionHistory   []string // Revision history from parent of ToRevID to source revID, in descending order
 }
 
@@ -124,6 +125,7 @@ func NewRevCacheDelta(deltaBytes []byte, fromRevID string, toRevision DocumentRe
 		ToRevID:           toRevision.RevID,
 		DeltaBytes:        deltaBytes,
 		AttachmentDigests: AttachmentDigests(toRevision.Attachments), // Flatten the AttachmentsMeta into a list of digests
+		ToChannels:        toRevision.Channels,
 		RevisionHistory:   toRevision.History.parseAncestorRevisions(fromRevID),
 	}
 
