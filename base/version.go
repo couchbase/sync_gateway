6dd298e4a2b71178de71a871c19cfcef87c72383
Ben Brooks <ben@bbrks.me>
2019-07-26 17:42:21 +0100

    CBG-454 Create 2.6.0 manifests, bump master to 2.7 (#4204)
    * Create 2.6.0 manifests, bump master to 2.7
    
    * Apply suggestions from code review

--- base/version.go
+++ base/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 const ServerName = "@PRODUCT_NAME@"                  // DO NOT CHANGE; clients check this
-const VersionNumber = "2.6"                          // API/feature level
+const VersionNumber = "2.7"                          // API/feature level
 const VersionBuildNumberString = "@PRODUCT_VERSION@" // Real string substituted by Jenkins build
 const VersionCommitSHA = "@COMMIT_SHA@"              // Real string substituted by Jenkins build
 
