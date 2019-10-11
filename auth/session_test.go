c50566d9c83c4cede6a2e9e5957410b4eb672eb5
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-10-11 13:40:25 -0700

    CBG-497: Code coverage auth package

--- auth/session_test.go
+++ auth/session_test.go
@@ -162,6 +162,6 @@ func TestDeleteSessionForCookie(t *testing.T) {
 
 	request.AddCookie(cookie)
 	newCookie = auth.DeleteSessionForCookie(request)
-	log.Printf("Cookie: %v", newCookie)
+	log.Printf("newCookie: %v", newCookie)
 	assert.Nil(t, newCookie)
 }
