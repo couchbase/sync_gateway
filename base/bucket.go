543e7042dca03e883d036a7a114e4c0678fe1401
Adam Fraser <adam.fraser@couchbase.com>
2019-11-15 16:17:05 -0800

    Disable operation tracing

--- base/bucket.go
+++ base/bucket.go
@@ -149,6 +149,7 @@ func (spec BucketSpec) GetGoCBConnString() (string, error) {
 	asValues.Set("http_max_idle_conns", DefaultHttpMaxIdleConns)
 	asValues.Set("http_idle_conn_timeout", DefaultHttpIdleConnTimeoutMilliseconds)
 	asValues.Set("n1ql_timeout", fmt.Sprintf("%d", spec.GetViewQueryTimeoutMs()))
+	asValues.Set("operation_tracing", "false")
 
 	if spec.Certpath != "" && spec.Keypath != "" {
 		asValues.Set("certpath", spec.Certpath)
