b60757f1166f7e44232dc4d8bfcfedadc6ce44a5
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-11-20 13:02:27 -0800

    CBG-590: Code coverage for rest package (server_context.go) - Make changes to incorporate the review comments

--- rest/server_context_test.go
+++ rest/server_context_test.go
@@ -13,9 +13,8 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
+	"strconv"
 	"sync/atomic"
 	"testing"
 
@@ -166,82 +165,36 @@ func (client *MockClient) RespondToGET(url string, response *http.Response) {
 	tripper.getURLs[url] = response
 }
 
-func TestRemoveDatabase(t *testing.T) {
+func TestAllDatabaseNames(t *testing.T) {
 	server := "walrus:"
 	bucketName := "imbucket"
-	databaseName := "imdb"
 
 	serverConfig := &ServerConfig{CORS: &CORSConfig{}, AdminInterface: &DefaultAdminInterface}
 	serverContext := NewServerContext(serverConfig)
 	bucketConfig := BucketConfig{Server: &server, Bucket: &bucketName}
-	dbConfig := &DbConfig{BucketConfig: bucketConfig, Name: databaseName, AllowEmptyPassword: true}
 
+	dbConfig := &DbConfig{BucketConfig: bucketConfig, Name: "imdb1", AllowEmptyPassword: true}
 	dbContext, err := serverContext.AddDatabaseFromConfig(dbConfig)
 	assert.NoError(t, err, "No error while adding database to server context")
 	assert.Equal(t, server, dbContext.BucketSpec.Server)
 	assert.Equal(t, bucketName, dbContext.BucketSpec.BucketName)
+	assert.Len(t, serverContext.AllDatabaseNames(), 1)
+	assert.Contains(t, serverContext.AllDatabaseNames(), "imdb1")
 
-	status := serverContext.RemoveDatabase(databaseName)
-	assert.True(t, status, "Database should be removed from server context")
-	dbContext, err = serverContext.GetDatabase(databaseName)
-	assert.Nil(t, dbContext, "Database context should not be available!")
-}
-
-func TestAllDatabaseNames(t *testing.T) {
-	server := "walrus:"
-	bucketName := "imbucket"
-	var databaseNames []string
-
-	serverConfig := &ServerConfig{CORS: &CORSConfig{}, AdminInterface: &DefaultAdminInterface}
-	serverContext := NewServerContext(serverConfig)
-	bucketConfig := BucketConfig{Server: &server, Bucket: &bucketName}
-
-	for i := 0; i < 5; i++ {
-		databaseName := fmt.Sprintf("imdb%d", i)
-		databaseNames = append(databaseNames, databaseName)
-		dbConfig := &DbConfig{BucketConfig: bucketConfig, Name: databaseName, AllowEmptyPassword: true}
-		dbContext, err := serverContext.AddDatabaseFromConfig(dbConfig)
-
-		assert.NoError(t, err, "No error while adding database to server context")
-		assert.Equal(t, server, dbContext.BucketSpec.Server)
-		assert.Equal(t, bucketName, dbContext.BucketSpec.BucketName)
-		assert.ElementsMatch(t, databaseNames, serverContext.AllDatabaseNames())
-	}
-
-	databaseNames = removeElementWithValue(databaseNames, "imdb2")
-	databaseNames = removeElementWithValue(databaseNames, "imdb3")
+	dbConfig = &DbConfig{BucketConfig: bucketConfig, Name: "imdb2", AllowEmptyPassword: true}
+	dbContext, err = serverContext.AddDatabaseFromConfig(dbConfig)
+	assert.NoError(t, err, "No error while adding database to server context")
+	assert.Equal(t, server, dbContext.BucketSpec.Server)
+	assert.Equal(t, bucketName, dbContext.BucketSpec.BucketName)
+	assert.Len(t, serverContext.AllDatabaseNames(), 2)
+	assert.Contains(t, serverContext.AllDatabaseNames(), "imdb1")
+	assert.Contains(t, serverContext.AllDatabaseNames(), "imdb2")
 
 	status := serverContext.RemoveDatabase("imdb2")
 	assert.True(t, status, "Database should be removed from server context")
-	status = serverContext.RemoveDatabase("imdb3")
-	assert.True(t, status, "Database should be removed from server context")
-
-	assert.ElementsMatch(t, databaseNames, serverContext.AllDatabaseNames())
-}
-
-func removeElementWithIndex(s []string, index int) []string {
-	return append(s[:index], s[index+1:]...)
-}
-
-func removeElementWithValue(s []string, e string) []string {
-	return removeElementWithIndex(s, indexOf(s, e))
-}
-
-func indexOf(elements []string, e string) int {
-	for k, v := range elements {
-		if e == v {
-			return k
-		}
-	}
-	return -1 // Not found.
-}
-
-func captureOutput(f func()) string {
-	var buf bytes.Buffer
-	log.SetOutput(&buf)
-	defer func() { log.SetOutput(os.Stderr) }()
-	f()
-	return buf.String()
+	assert.Len(t, serverContext.AllDatabaseNames(), 1)
+	assert.Contains(t, serverContext.AllDatabaseNames(), "imdb1")
+	assert.NotContains(t, serverContext.AllDatabaseNames(), "imdb2")
 }
 
 func TestStartReplicators(t *testing.T) {
@@ -250,75 +203,79 @@ func TestStartReplicators(t *testing.T) {
 
 	// Should be skipped; create_target option is not currently supported.
 	replications = append(replications, &ReplicationConfig{
-		Source:       "http://127.0.0.1:4985/imdb_us",
-		Target:       "http://127.0.0.1:4985/imdb_uk",
-		Async:        true,
-		CreateTarget: true})
+		Source:        "http://127.0.0.1:4985/imdb_us",
+		Target:        "http://127.0.0.1:4985/imdb_uk",
+		Async:         true,
+		CreateTarget:  true,
+		ReplicationId: "58a632bb8d7e110445d3d65a98365d61"})
 
 	// Should be skipped; doc_ids option is not currently supported
-	replications = append(replications, &ReplicationConfig{
-		Source: "http://127.0.0.1:4985/imdb_us",
-		Target: "http://127.0.0.1:4985/imdb_uk",
-		Async:  true,
-		DocIds: []string{"doc1", "doc2", "doc3", "doc4"}})
-
-	// Should be skipped; proxy option is not currently supported.
-	replications = append(replications, &ReplicationConfig{
-		Source: "http://127.0.0.1:4985/imdb_us",
-		Target: "http://127.0.0.1:4985/imdb_uk",
-		Async:  true,
-		Proxy:  "http://127.0.0.1:443/proxy"})
-
-	// Start a one-shot replication.
 	replications = append(replications, &ReplicationConfig{
 		Source:        "http://127.0.0.1:4985/imdb_us",
 		Target:        "http://127.0.0.1:4985/imdb_uk",
 		Async:         true,
+		DocIds:        []string{"doc1", "doc2", "doc3", "doc4"},
 		ReplicationId: "58a632bb8d7e110445d3d65a98365d62"})
+
+	// Should be skipped; proxy option is not currently supported.
+	replications = append(replications, &ReplicationConfig{
+		Source:        "http://127.0.0.1:4985/imdb_us",
+		Target:        "http://127.0.0.1:4985/imdb_uk",
+		Async:         true,
+		Proxy:         "http://127.0.0.1:443/proxy",
+		ReplicationId: "58a632bb8d7e110445d3d65a98365d63"})
+
+	// Start a continuous fake replication; it should be added to active tasks
+	replications = append(replications, &ReplicationConfig{
+		Source:        "http://127.0.0.1:4985/imdb_us",
+		Target:        "http://127.0.0.1:4985/imdb_uk",
+		Continuous:    true,
+		ReplicationId: "58a632bb8d7e110445d3d65a98365d64"})
 
 	serverConfig := &ServerConfig{AdminInterface: &DefaultAdminInterface, Replications: replications}
 	serverContext := NewServerContext(serverConfig)
-	out := captureOutput(serverContext.startReplicators)
+	serverContext.startReplicators()
 
-	assert.Contains(t, "/_replicate create_target option is not currently supported", out)
-	assert.Contains(t, "/_replicate doc_ids option is not currently supported", out)
-	assert.Contains(t, "/_replicate proxy option is not currently supported", out)
-	assert.Contains(t, "ReplicationId:58a632bb8d7e110445d3d65a98365d62", out)
+	activeTasks := serverContext.replicator.ActiveTasks()
+	assert.Equal(t, 1, len(activeTasks))
+	activeTask := activeTasks[0]
+	assert.Equal(t, "58a632bb8d7e110445d3d65a98365d64", activeTask.ReplicationID)
+	serverContext.Close()
 }
 
 func TestPostStartup(t *testing.T) {
 	defer base.SetUpTestLogging(base.LevelInfo, base.KeyReplicate)()
 	var replications []*ReplicationConfig
 
-	// Start a one-shot replication.
+	// Start a continuous fake replication.
 	replications = append(replications, &ReplicationConfig{
 		Source:        "http://127.0.0.1:4985/imdb_us",
 		Target:        "http://127.0.0.1:4985/imdb_uk",
-		Async:         true,
+		Continuous:    true,
 		ReplicationId: "58a632bb8d7e110445d3d65a98365d62"})
 
 	serverConfig := &ServerConfig{AdminInterface: &DefaultAdminInterface, Replications: replications}
 	serverContext := NewServerContext(serverConfig)
-	out := captureOutput(serverContext.startReplicators)
-	assert.Contains(t, "ReplicationId:58a632bb8d7e110445d3d65a98365d62", out)
+	serverContext.startReplicators()
+
+	activeTasks := serverContext.replicator.ActiveTasks()
+	assert.Equal(t, 1, len(activeTasks))
+	activeTask := activeTasks[0]
+	assert.Equal(t, "58a632bb8d7e110445d3d65a98365d62", activeTask.ReplicationID)
+	serverContext.Close()
 }
 
 func TestGetOrAddDatabaseFromConfig(t *testing.T) {
-	//server := "walrus:"
-	//bucketName := "imbucket"
-
 	serverConfig := &ServerConfig{CORS: &CORSConfig{}, AdminInterface: &DefaultAdminInterface}
 	serverContext := NewServerContext(serverConfig)
-	//bucketConfig := BucketConfig{Server: &server, Bucket: &bucketName}
-
-	oldRevExpirySeconds := base.DefaultOldRevExpirySeconds
-	localDocExpirySecs := base.DefaultLocalDocExpirySecs
+	oldRevExpirySeconds := uint32(600)
+	localDocExpirySecs := uint32(60 * 60 * 24 * 10) // 10 days in seconds
 
 	// Get or add database name from config without valid database name; throws 400 Illegal database name error
 	dbConfig := &DbConfig{OldRevExpirySeconds: &oldRevExpirySeconds, LocalDocExpirySecs: &localDocExpirySecs}
 	dbContext, err := serverContext._getOrAddDatabaseFromConfig(dbConfig, false)
 	assert.Error(t, err, "It should throw 400 Illegal database name")
-	assert.Contains(t, err.Error(), "400 Illegal database name")
+	assert.Contains(t, err.Error(), strconv.Itoa(http.StatusBadRequest))
 
 	// Get or add database from config with unrecognized value for import_docs.
 	dbConfig = &DbConfig{
@@ -329,15 +286,16 @@ func TestGetOrAddDatabaseFromConfig(t *testing.T) {
 
 	dbContext, err = serverContext._getOrAddDatabaseFromConfig(dbConfig, false)
 	assert.Error(t, err, "It should throw Unrecognized value for import_docs")
-	assert.Contains(t, err.Error(), "Unrecognized value for import_docs")
 
 	// Get or add database from config with duplicate database name and useExisting as false.
 	server := "walrus:"
 	bucketName := "imbucket"
 	databaseName := "imdb"
+
 	bucketConfig := BucketConfig{Server: &server, Bucket: &bucketName}
 	dbConfig = &DbConfig{BucketConfig: bucketConfig, Name: databaseName, AllowEmptyPassword: true}
 	dbContext, err = serverContext.AddDatabaseFromConfig(dbConfig)
+
 	assert.NoError(t, err, "No error while adding database to server context")
 	assert.Equal(t, server, dbContext.BucketSpec.Server)
 	assert.Equal(t, bucketName, dbContext.BucketSpec.BucketName)
@@ -350,7 +308,7 @@ func TestGetOrAddDatabaseFromConfig(t *testing.T) {
 
 	dbContext, err = serverContext._getOrAddDatabaseFromConfig(dbConfig, false)
 	assert.Error(t, err, "It should throw 412 Duplicate database names")
-	assert.Contains(t, err.Error(), "412 Duplicate database name")
+	assert.Contains(t, err.Error(), strconv.Itoa(http.StatusPreconditionFailed))
 
 	// Get or add database from config with duplicate database name and useExisting as true
 	// Existing database context should be returned
