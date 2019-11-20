30b9d5758cd9c6d733b59442ec60c45f5b54ad39
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-11-20 00:51:28 -0800

    CBG-590: Code coverage for rest package (server_context.go) - Add coverage for getOrAddDatabaseFromConfig

--- rest/server_context_test.go
+++ rest/server_context_test.go
@@ -302,3 +302,60 @@ func TestPostStartup(t *testing.T) {
 	out := captureOutput(serverContext.startReplicators)
 	assert.Contains(t, "ReplicationId:58a632bb8d7e110445d3d65a98365d62", out)
 }
+
+func TestGetOrAddDatabaseFromConfig(t *testing.T) {
+	//server := "walrus:"
+	//bucketName := "imbucket"
+
+	serverConfig := &ServerConfig{CORS: &CORSConfig{}, AdminInterface: &DefaultAdminInterface}
+	serverContext := NewServerContext(serverConfig)
+	//bucketConfig := BucketConfig{Server: &server, Bucket: &bucketName}
+
+	oldRevExpirySeconds := base.DefaultOldRevExpirySeconds
+	localDocExpirySecs := base.DefaultLocalDocExpirySecs
+
+	// Get or add database name from config without valid database name; throws 400 Illegal database name error
+	dbConfig := &DbConfig{OldRevExpirySeconds: &oldRevExpirySeconds, LocalDocExpirySecs: &localDocExpirySecs}
+	dbContext, err := serverContext._getOrAddDatabaseFromConfig(dbConfig, false)
+	assert.Error(t, err, "It should throw 400 Illegal database name")
+	assert.Contains(t, err.Error(), "400 Illegal database name")
+
+	// Get or add database from config with unrecognized value for import_docs.
+	dbConfig = &DbConfig{
+		Name:                "imdb",
+		OldRevExpirySeconds: &oldRevExpirySeconds,
+		LocalDocExpirySecs:  &localDocExpirySecs,
+		AutoImport:          "Unknown"}
+
+	dbContext, err = serverContext._getOrAddDatabaseFromConfig(dbConfig, false)
+	assert.Error(t, err, "It should throw Unrecognized value for import_docs")
+	assert.Contains(t, err.Error(), "Unrecognized value for import_docs")
+
+	// Get or add database from config with duplicate database name and useExisting as false.
+	server := "walrus:"
+	bucketName := "imbucket"
+	databaseName := "imdb"
+	bucketConfig := BucketConfig{Server: &server, Bucket: &bucketName}
+	dbConfig = &DbConfig{BucketConfig: bucketConfig, Name: databaseName, AllowEmptyPassword: true}
+	dbContext, err = serverContext.AddDatabaseFromConfig(dbConfig)
+	assert.NoError(t, err, "No error while adding database to server context")
+	assert.Equal(t, server, dbContext.BucketSpec.Server)
+	assert.Equal(t, bucketName, dbContext.BucketSpec.BucketName)
+
+	dbConfig = &DbConfig{
+		Name:                databaseName,
+		OldRevExpirySeconds: &oldRevExpirySeconds,
+		LocalDocExpirySecs:  &localDocExpirySecs,
+		AutoImport:          false}
+
+	dbContext, err = serverContext._getOrAddDatabaseFromConfig(dbConfig, false)
+	assert.Error(t, err, "It should throw 412 Duplicate database names")
+	assert.Contains(t, err.Error(), "412 Duplicate database name")
+
+	// Get or add database from config with duplicate database name and useExisting as true
+	// Existing database context should be returned
+	dbContext, err = serverContext._getOrAddDatabaseFromConfig(dbConfig, true)
+	assert.NoError(t, err, "No error while trying to get the existing database name")
+	assert.Equal(t, server, dbContext.BucketSpec.Server)
+	assert.Equal(t, bucketName, dbContext.BucketSpec.BucketName)
+}
