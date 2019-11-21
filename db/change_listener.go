60952bf74b9ccf10ebf597529ca153de49071439
Adam Fraser <adam.fraser@couchbase.com>
2019-11-21 11:26:35 -0800

    Tuning

--- db/change_listener.go
+++ db/change_listener.go
@@ -62,7 +62,7 @@ func (listener *changeListener) BroadcastTicker(terminator chan bool) {
 		select {
 		case <-terminator:
 			return
-		case <-time.After(10 * time.Microsecond):
+		case <-time.After(100 * time.Microsecond):
 			listener.tapNotifier.L.Lock()
 			if listener.counter > tickerCount {
 				tickerCount = listener.counter
