b695ce13ad965c9315d7d1de953fc119b357ef2a
Adam Fraser <adam.fraser@couchbase.com>
2020-01-08 10:55:48 -0800

    CBG-663 Fix for delta handling when previous revision has attachment

--- rest/utilities_testing.go
+++ rest/utilities_testing.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -1016,6 +1017,8 @@ type SendRevWithAttachmentInput struct {
 	attachmentLength int
 	attachmentBody   string
 	attachmentDigest string
+	history          []string
+	body             []byte
 }
 
 // Warning: this can only be called from a single goroutine, given the fact it registers profile handlers.
@@ -1034,7 +1037,15 @@ func (bt *BlipTester) SendRevWithAttachment(input SendRevWithAttachmentInput) (s
 		Revpos:      1,
 		Stub:        true,
 	}
+
 	doc := NewRestDocument()
+	if len(input.body) > 0 {
+		unmarshalErr := json.Unmarshal(input.body, &doc)
+		if unmarshalErr != nil {
+			panic(fmt.Sprintf("Error unmarshalling body into restDocument.  Error: %v", unmarshalErr))
+		}
+	}
+
 	doc.SetID(input.docId)
 	doc.SetRevID(input.revId)
 	doc.SetAttachments(db.AttachmentMap{
@@ -1059,9 +1070,10 @@ func (bt *BlipTester) SendRevWithAttachment(input SendRevWithAttachmentInput) (s
 
 	// Push a rev with an attachment.
 	getAttachmentWg.Add(1)
-	sent, req, res, _ = bt.SendRev(
+	sent, req, res, _ = bt.SendRevWithHistory(
 		input.docId,
 		input.revId,
+		input.history,
 		docBody,
 		blip.Properties{},
 	)
