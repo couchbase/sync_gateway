71ca488ffc4c1577e91b9374a367fc7a352c4f7b
Ben Brooks <ben.brooks@couchbase.com>
2020-01-27 14:47:00 +0000

    Generate a hash for cbgt's index names based on dbName

--- base/dcp_common_test.go
+++ base/dcp_common_test.go
@@ -3,6 +3,7 @@ package base
 import (
 	"fmt"
 	"math/rand"
+	"regexp"
 	"testing"
 
 	"github.com/couchbase/cbgt"
@@ -10,7 +11,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerateDcpStreamName(t *testing.T) {
+func TestDCPNameLength(t *testing.T) {
+	// https://issues.couchbase.com/browse/MB-34280
 	const maxDCPNameLength = 200
 
 	feedIDs := []string{
@@ -28,16 +30,46 @@ func TestGenerateDcpStreamName(t *testing.T) {
 				"len(dcpStreamName)=%d is exceeds max allowed %d chars: %s",
 				len(dcpStreamName), maxDCPNameLength, dcpStreamName)
 		})
+
 		t.Run("cbgt"+feedID, func(t *testing.T) {
 			dcpStreamName, err := GenerateDcpStreamName(feedID)
 			require.NoError(t, err)
 
+			// Format string copied from cbgt's 'NewDCPFeed'
 			cbgtDCPName := fmt.Sprintf("%s%s-%x", cbgt.DCPFeedPrefix, dcpStreamName, rand.Int31())
 			t.Logf("generated name of length %d: %s", len(cbgtDCPName), cbgtDCPName)
 
 			assert.Truef(t, len(cbgtDCPName) <= maxDCPNameLength,
 				"len(cbgtDCPName)=%d is exceeds max allowed %d chars: %s",
 				len(cbgtDCPName), maxDCPNameLength, cbgtDCPName)
+		})
+	}
+
+	dbNames := []string{
+		"db1",
+		"db1$a", // dollars aren't allowed in cbgt index names, but are valid in SG/CouchDB database names
+		"123",   // indexes and db names can't start with a digit
+		"db1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	}
+
+	for _, dbName := range dbNames {
+		t.Run("cbgt-index-"+dbName, func(t *testing.T) {
+			indexName := dbName // GenerateIndexName(dbName)
+
+			// Verify we pass CBGT's index name validation
+			matched, err := regexp.Match(cbgt.INDEX_NAME_REGEXP, []byte(indexName))
+			require.NoError(t, err)
+			assert.True(t, matched, "index name is not a valid cbgt index name")
+
+			// The format of a cbgt index name is of the following format:
+			// sg:db0xbdcaa3f7_index_167dfc5a122bde31_f47365c5-2e5220ce
+			// where 'sg:db0x..._index' are SG's generated name, and the rest is from CBGT.
+			cbgtIndexDCPName := fmt.Sprintf("%s_167dfc5a122bde31_f47365c5-2e5220ce", indexName)
+			t.Logf("generated name of length %d: %s", len(cbgtIndexDCPName), cbgtIndexDCPName)
+
+			assert.Truef(t, len(cbgtIndexDCPName) <= maxDCPNameLength,
+				"len(cbgtIndexDCPName)=%d is exceeds max allowed %d chars: %s",
+				len(cbgtIndexDCPName), maxDCPNameLength, cbgtIndexDCPName)
 		})
 	}
 }
