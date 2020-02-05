d9c9b5c9b452ac2a1382838429d0f3ddcd51e270
Ben Brooks <ben.brooks@couchbase.com>
2020-02-05 12:41:18 +0000

    Change the logCollationWorker timeout to prevent idle spinning

--- base/logger.go
+++ base/logger.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"log"
+	"math"
 	"strings"
 	"time"
 )
@@ -11,26 +12,31 @@ func FlushLogBuffers() {
 	time.Sleep(loggerCollateFlushDelay)
 }
 
+// logCollationWorker will take log lines over the given channel, and buffer them until either the buffer is full, or the flushTimeout is exceeded.
+// This is to reduce the number of writes to the log files, in order to batch them up as larger collated chunks, whilst maintaining a low-level of latency with the flush timeout.
 func logCollationWorker(collateBuffer chan string, logger *log.Logger, maxBufferSize int, collateFlushTimeout time.Duration) {
-	// This is the temporary buffer we'll store logs in.
+
+	// The initial duration of the timeout timer doesn't matter,
+	// because we start it whenever we buffer a log without flushing it.
+	t := time.NewTimer(math.MaxInt64)
 	logBuffer := make([]string, 0, maxBufferSize)
+
 	for {
 		select {
-		// Add log to buffer and flush to output if it's full.
 		case l := <-collateBuffer:
 			logBuffer = append(logBuffer, l)
 			if len(logBuffer) >= maxBufferSize {
+				// flush if the buffer is full after this log
 				logger.Print(strings.Join(logBuffer, "\n"))
-				// Empty buffer
 				logBuffer = logBuffer[:0]
+			} else {
+				// Start the timeout timer going to flush this partial buffer
+				t.Reset(collateFlushTimeout)
 			}
-		// Flush the buffer to the output after this time, even if we don't fill it.
-		case <-time.After(collateFlushTimeout):
-			if len(logBuffer) > 0 {
-				logger.Print(strings.Join(logBuffer, "\n"))
-				// Empty buffer
-				logBuffer = logBuffer[:0]
-			}
+		case <-t.C:
+			// We've timed out waiting for more logs to be put into the buffer, so flush it now.
+			logger.Print(strings.Join(logBuffer, "\n"))
+			logBuffer = logBuffer[:0]
 		}
 	}
 }
