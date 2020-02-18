fd9c6d6fa6e89ad497307ed98d06cdf05325f165
Ben Brooks <ben.brooks@couchbase.com>
2020-02-18 22:56:11 +0000

    CBG-667 - Backport CBG-659 to 2.7.1 (#4499)
    CBG-659 - Order channels query by ascending sequence (#4443)
    
    * Order channels query by ascending sequence
    
    * Update db/query.go
    
    Co-authored-by: Adam Fraser <adam.fraser@couchbase.com>
    
    Co-authored-by: Adam Fraser <adam.fraser@couchbase.com>

--- db/query.go
+++ db/query.go
@@ -76,7 +76,8 @@ var QueryChannels = SGQuery{
 			"FROM `%s` "+
 			"USE INDEX ($idx) "+
 			"UNNEST OBJECT_PAIRS($sync.channels) AS op "+
-			"WHERE [op.name, LEAST($sync.sequence, op.val.seq),IFMISSING(op.val.rev,null),IFMISSING(op.val.del,null)]  BETWEEN  [$channelName, $startSeq] AND [$channelName, $endSeq]",
+			"WHERE [op.name, LEAST($sync.sequence, op.val.seq),IFMISSING(op.val.rev,null),IFMISSING(op.val.del,null)]  BETWEEN  [$channelName, $startSeq] AND [$channelName, $endSeq] "+
+			"ORDER BY seq",
 		base.BucketQueryToken, base.BucketQueryToken),
 	adhoc: false,
 }
