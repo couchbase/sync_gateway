f032366e5f6b922204f5b1c345084a6c6f0272c9
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-03-05 14:47:51 -0800

    CBG-731: Channels query performance degradation when using limit - Modified TestQueryChannelsActiveOnlyWithLimit - Created documents in the following states, to validate the query's interaction with the bitflag. flags=0. (createDocs) flags = deleted (deleteDocs) flags = branched (two branches, one branch is tombstoned) flags = branched|deleted (two branches, both branches tombstoned) flags = branched|conflict (two branched, neither branch tombstoned)

--- db/query_test.go
+++ db/query_test.go
@@ -579,118 +579,175 @@ func TestQueryChannelsActiveOnlyWithLimit(t *testing.T) {
 	defer testBucket.Close()
 	defer tearDownTestDB(t, db)
 
-	var sequence = 1
-	seqLogEntries := make(map[int]LogEntry)
+	docIdFlagMap := make(map[string]uint8)
+	var startSeq, endSeq uint64
 	body := Body{"channels": []string{"ABC"}}
 
-	createDocs := func(name string, revs int) (startSeq, endSeq uint64) {
-		for i := 1; i <= revs; i++ {
-			body["version"] = "1-a"
-			id := name + strconv.Itoa(i)
-			doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"1-a"}, false)
-			require.NoError(t, err, "Couldn't create document")
-			require.Equal(t, "1-a", revId)
-			seqLogEntries[sequence] = LogEntry{DocID: doc.ID, RevID: revId}
-			sequence++
-			if i == 1 {
-				startSeq = doc.Sequence
-			}
-			endSeq = doc.Sequence
-		}
-		delete(body, "version")
-		return startSeq, endSeq
-	}
-
-	deleteDocs := func(name string, revs int) (startSeq, endSeq uint64) {
-		for i := 1; i <= revs; i++ {
-			id := name + strconv.Itoa(i)
-			body[BodyDeleted] = true
-			doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"2-a", "1-a"}, false)
-			require.NoError(t, err, "Couldn't create document")
-			require.Equal(t, "2-a", revId, "Couldn't create tombstone revision")
-			seqLogEntries[sequence] = LogEntry{DocID: doc.ID, RevID: revId, Flags: uint8(0x1)}
-			sequence++
-			if i == 1 {
-				startSeq = doc.Sequence
-			}
-			endSeq = doc.Sequence
-		}
-		return startSeq, endSeq
-	}
-
-	checkDocs := func(entries LogEntries, sequence int) {
+	checkFlags := func(entries LogEntries) {
 		for _, entry := range entries {
-			assert.Equal(t, seqLogEntries[sequence].DocID, entry.DocID)
-			assert.Equal(t, seqLogEntries[sequence].RevID, entry.RevID)
-			assert.Equal(t, seqLogEntries[sequence].Flags, entry.Flags)
-			sequence++
+			assert.Equal(t, docIdFlagMap[entry.DocID], entry.Flags, "Flags mismatch for doc %v", entry.DocID)
 		}
 	}
 
-	// Create 10 active documents and 2 inactive documents.
-	startSeq, endSeq := createDocs("alice", 10)
-	_, endSeq = deleteDocs("alice", 2)
+	// Create 10 added documents
+	for i := 1; i <= 10; i++ {
+		id := "created" + strconv.Itoa(i)
+		doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"1-a"}, false)
+		require.NoError(t, err, "Couldn't create document")
+		require.Equal(t, "1-a", revId)
+		docIdFlagMap[doc.ID] = uint8(0x0)
+		if i == 1 {
+			startSeq = doc.Sequence
+		}
+		endSeq = doc.Sequence
+	}
+
+	// Create 10 deleted documents
+	for i := 1; i <= 10; i++ {
+		id := "deleted" + strconv.Itoa(i)
+		doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"1-a"}, false)
+		require.NoError(t, err, "Couldn't create document")
+		require.Equal(t, "1-a", revId)
+
+		body[BodyDeleted] = true
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-a", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create document")
+		require.Equal(t, "2-a", revId, "Couldn't create tombstone revision")
+
+		docIdFlagMap[doc.ID] = uint8(0x1) // 1 = Deleted(1)
+		endSeq = doc.Sequence
+	}
+
+	// Create 10 branched documents (two branches, one branch is tombstoned)
+	for i := 1; i <= 10; i++ {
+		body["sound"] = "meow"
+		id := "branched" + strconv.Itoa(i)
+		doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"1-a"}, false)
+		require.NoError(t, err, "Couldn't create document revision 1-a")
+		require.Equal(t, "1-a", revId)
+
+		body["sound"] = "bark"
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-b", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create revision 2-b")
+		require.Equal(t, "2-b", revId)
+
+		body["sound"] = "bleat"
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-a", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create revision 2-a")
+		require.Equal(t, "2-a", revId)
+
+		body[BodyDeleted] = true
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"3-a", "2-a"}, false)
+		require.NoError(t, err, "Couldn't create document")
+		require.Equal(t, "3-a", revId, "Couldn't create tombstone revision")
+
+		docIdFlagMap[doc.ID] = uint8(0x14) // 20 = Branched (16) + Hidden(4)
+		endSeq = doc.Sequence
+	}
+
+	// Create 10 branched|deleted documents (two branches, both branches tombstoned)
+	for i := 1; i <= 10; i++ {
+		body["sound"] = "meow"
+		id := "branched|deleted" + strconv.Itoa(i)
+		doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"1-a"}, false)
+		require.NoError(t, err, "Couldn't create document revision 1-a")
+		require.Equal(t, "1-a", revId)
+
+		body["sound"] = "bark"
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-b", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create revision 2-b")
+		require.Equal(t, "2-b", revId)
+
+		body["sound"] = "bleat"
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-a", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create revision 2-a")
+		require.Equal(t, "2-a", revId)
+
+		body[BodyDeleted] = true
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"3-a", "2-a"}, false)
+		require.NoError(t, err, "Couldn't create document")
+		require.Equal(t, "3-a", revId, "Couldn't create tombstone revision")
+
+		body[BodyDeleted] = true
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"3-b", "2-b"}, false)
+		require.NoError(t, err, "Couldn't create document")
+		require.Equal(t, "3-b", revId, "Couldn't create tombstone revision")
+
+		docIdFlagMap[doc.ID] = uint8(0x11) // 17 = Branched (16) + Deleted(1)
+		endSeq = doc.Sequence
+	}
+
+	// Create 10 branched|conflict (two branched, neither branch tombstoned) documents
+	for i := 1; i <= 10; i++ {
+		body["sound"] = "meow"
+		id := "branched|conflict" + strconv.Itoa(i)
+		doc, revId, err := db.PutExistingRevWithBody(id, body, []string{"1-a"}, false)
+		require.NoError(t, err, "Couldn't create document revision 1-a")
+		require.Equal(t, "1-a", revId)
+
+		body["sound"] = "bark"
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-b", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create revision 2-b")
+		require.Equal(t, "2-b", revId)
+
+		body["sound"] = "bleat"
+		doc, revId, err = db.PutExistingRevWithBody(id, body, []string{"2-a", "1-a"}, false)
+		require.NoError(t, err, "Couldn't create revision 2-a")
+		require.Equal(t, "2-a", revId)
+
+		docIdFlagMap[doc.ID] = uint8(0x1c) // 28 = Branched(16) + Conflict(8) + Hidden(4)
+		endSeq = doc.Sequence
+	}
+
+	// At this point the bucket has 50 documents
+	// 30 active documents (10 created + 10 branched + 10 branched|conflict)
+	// 20 Deleted documents (10 deleted + 10 branched|deleted)
 
 	// Get changes from channel "ABC" with limit and activeOnly true
-	// 8 documents in channel "ABC" are active and 2 documents are inactive at this point.
-	// Retrieved active documents from channel "ABC" must be equal to limit.
-	limit := 4         // Set limit to retrieve only 4 documents
-	activeOnly := true // Set activeOnly to retrieve only active documents.
-	entries, err := db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, limit, activeOnly)
+	entries, err := db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, 25, true)
 	require.NoError(t, err, "Couldn't query active docs from channel ABC with limit")
-	require.Len(t, entries, limit)
-	checkDocs(entries, 3)
+	require.Len(t, entries, 25)
+	checkFlags(entries)
 
 	// Get changes from channel "*" with limit and activeOnly true
-	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, limit, activeOnly)
+	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, 25, true)
 	require.NoError(t, err, "Couldn't query active docs from channel * with limit")
-	require.Len(t, entries, limit)
-	checkDocs(entries, 3)
+	require.Len(t, entries, 25)
+	checkFlags(entries)
 
-	// Get changes from channel "ABC" without limit and activeOnly true.
-	// 8 documents in channel "ABC" are active and 2 documents are inactive at this point.
-	// All the active documents (8) should be retrieved from channel "ABC" when limit is 0.
-	limit = 0 // Set limit to retrieve all documents
-	entries, err = db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, limit, activeOnly)
-	require.NoError(t, err, "Couldn't query active docs from channel ABC without limit")
-	require.Len(t, entries, 8) // 8 active documents
-	checkDocs(entries, 3)
+	// Get changes from channel "ABC" without limit and activeOnly true
+	entries, err = db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, 0, true)
+	require.NoError(t, err, "Couldn't query active docs from channel ABC with limit")
+	require.Len(t, entries, 30)
+	checkFlags(entries)
 
-	// Get changes from channel "*" without limit and activeOnly true.
-	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, limit, activeOnly)
-	require.NoError(t, err, "Couldn't query active docs from channel * without limit")
-	require.Len(t, entries, 8) // 8 active documents
-	checkDocs(entries, 3)
+	// Get changes from channel "*" without limit and activeOnly true
+	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, 0, true)
+	require.NoError(t, err, "Couldn't query active docs from channel * with limit")
+	require.Len(t, entries, 30)
+	checkFlags(entries)
 
 	// Get changes from channel "ABC" with limit and activeOnly false
-	// 8 documents in channel "ABC" are active and 2 documents are inactive at this point.
-	// Retrieved active/inactive documents from channel "ABC" must be equal to limit.
-	limit = 6          // Set limit to retrieve only 6 documents
-	activeOnly = false // Set activeOnly to false to retrieve both active and inactive documents.
-	entries, err = db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, 6, activeOnly)
-	require.NoError(t, err, "Couldn't query both active and inactive docs from channel ABC with limit")
-	require.Len(t, entries, limit) // 6 active/inactive documents
-	checkDocs(entries, 3)
+	entries, err = db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, 45, false)
+	require.NoError(t, err, "Couldn't query active docs from channel ABC with limit")
+	require.Len(t, entries, 45)
+	checkFlags(entries)
 
 	// Get changes from channel "*" with limit and activeOnly false
-	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, 6, activeOnly)
-	require.NoError(t, err, "Couldn't query both active and inactive docs from channel * with limit")
-	require.Len(t, entries, limit) // 6 active/inactive documents
-	checkDocs(entries, 3)
+	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, 45, false)
+	require.NoError(t, err, "Couldn't query active docs from channel * with limit")
+	require.Len(t, entries, 45)
+	checkFlags(entries)
 
-	// Get changes from channel "ABC" with limit and activeOnly false
-	// 8 documents in channel "ABC" are active and 2 documents are inactive at this point.
-	// Retrieved active/inactive documents from channel "ABC" must be equal to limit.
-	limit = 0          // Set limit to retrieve all documents
-	activeOnly = false // Set activeOnly to false to retrieve both active and inactive documents.
-	entries, err = db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, limit, activeOnly)
-	require.NoError(t, err, "Couldn't query both active and inactive docs from channel ABC without limit")
-	require.Len(t, entries, 10) // 8 active + 2 inactive docs
-	checkDocs(entries, 3)
+	// Get changes from channel "ABC" without limit and activeOnly false
+	entries, err = db.getChangesInChannelFromQuery("ABC", startSeq, endSeq, 0, false)
+	require.NoError(t, err, "Couldn't query active docs from channel ABC with limit")
+	require.Len(t, entries, 50)
+	checkFlags(entries)
 
-	// Get changes from channel "*" with limit and activeOnly false
-	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, limit, activeOnly)
-	require.NoError(t, err, "Couldn't query both active and inactive docs from channel * without limit")
-	require.Len(t, entries, 10) // 8 active + 2 inactive docs
-	checkDocs(entries, 3)
+	// Get changes from channel "*" without limit and activeOnly true
+	entries, err = db.getChangesInChannelFromQuery("*", startSeq, endSeq, 0, false)
+	require.NoError(t, err, "Couldn't query active docs from channel * with limit")
+	require.Len(t, entries, 50)
+	checkFlags(entries)
 }
