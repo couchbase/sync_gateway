cf337625de39f990d64e32fcf78daf414c20c480
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-05-07 22:34:19 -0700

    CBG-820: Removed globally scoped variables and included additional coverage.
    - Additional coverage in TestGetOIDCCallbackURL to check whether the provider is getting added to the OIDC callback URL for both oidc and oidc challenge requests. Following scenarios are included:
      - oidc default provider specified but current provider is not default.
      - oidc challenge default provider specified but current provider is not default.
      - oidc default provider specified and current provider is default.
      - oidc default provider specified with no current provider.
      - oidc challenge default provider specified with no current provider.
    
    - Additional coverage in TestOpenIDConnectAuth:
      - Auto registration failure
      - Authorization server sends the callback URL without auth code.
      - OpenID Connect callback received with error code and error description from Auth server.
      - No provider in the callback URL returned from authorization server.

--- rest/oidc_api_test.go
+++ rest/oidc_api_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"sync"
 	"testing"
 	"time"
 
@@ -18,11 +19,27 @@ import (
 	"gopkg.in/square/go-jose.v2/jwt"
 )
 
+// mockOptions keeps options required for mocking
+// auth code flow responses.
+type mockOptions map[string]interface{}
+
 var (
-	issuerGoogle      = ""
-	issuerFacebook    = ""
-	wantTokenResponse OIDCTokenResponse
+	optionsOnce sync.Once
+	options     mockOptions
 )
+
+func NewMockOptions() mockOptions {
+	optionsOnce.Do(func() {
+		options = make(mockOptions)
+	})
+	return options
+}
+
+func (options mockOptions) Clear() {
+	for option := range options {
+		delete(options, option)
+	}
+}
 
 // mockAuthServer mocks a fake OAuth server by registering mock handlers
 func mockAuthServer() (*httptest.Server, error) {
@@ -39,12 +56,14 @@ func mockDiscoveryHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	provider := vars["provider"]
 	if provider == "google" {
+		issuerGoogle := options["issuerGoogle"].(string)
 		metadata = auth.ProviderMetadata{
 			Issuer: issuerGoogle, AuthorizationEndpoint: issuerGoogle + "/auth",
 			TokenEndpoint: issuerGoogle + "/token", JwksUri: issuerGoogle + "/oauth2/v3/certs",
 			IdTokenSigningAlgValuesSupported: []string{"RS256"},
 		}
 	} else if provider == "facebook" {
+		issuerFacebook := options["issuerFacebook"].(string)
 		metadata = auth.ProviderMetadata{
 			Issuer: issuerFacebook, AuthorizationEndpoint: issuerFacebook + "/auth",
 			TokenEndpoint: issuerFacebook + "/token", JwksUri: issuerFacebook + "/oauth2/v3/certs",
@@ -77,15 +96,34 @@ func mockAuthHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	redirectionURL = fmt.Sprintf("%s?code=%s", redirect, base.GenerateRandomSecret())
+	if val, ok := options["wantCallbackError"]; ok && val.(bool) {
+		err := "?error=unsupported_response_type&error_description=response_type%20not%20supported"
+		redirectionURL = fmt.Sprintf("%s?error=%s", redirect, err)
+		http.Redirect(w, r, redirectionURL, http.StatusTemporaryRedirect)
+	}
+	code := base.GenerateRandomSecret()
+	if val, ok := options["wantNoCode"]; ok && val.(bool) {
+		code = ""
+	}
+	redirectionURL = fmt.Sprintf("%s?code=%s", redirect, code)
 	if state != "" {
 		redirectionURL = fmt.Sprintf("%s&state=%s", redirectionURL, state)
+	}
+	if val, ok := options["wantUntoldProvider"]; ok && val.(bool) {
+		uri, err := auth.SetURLQueryParam(redirectionURL, requestParamProvider, "untold")
+		if err != nil {
+			base.Errorf("error setting untold provider in mock callback URL")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		redirectionURL = uri
 	}
 	http.Redirect(w, r, redirectionURL, http.StatusTemporaryRedirect)
 }
 
 // mockTokenHandler mocks the token handler for requesting and exchanging auth token.
 func mockTokenHandler(w http.ResponseWriter, r *http.Request) {
+	issuerGoogle := options["issuerGoogle"].(string)
 	claims := jwt.Claims{ID: "id0123456789", Issuer: issuerGoogle,
 		Audience: jwt.Audience{"aud1", "aud2", "aud3", "foo"},
 		IssuedAt: jwt.NewNumericDate(time.Now()), Subject: "noah",
@@ -112,7 +150,7 @@ func mockTokenHandler(w http.ResponseWriter, r *http.Request) {
 		TokenType:    "Bearer",
 		Expires:      time.Now().Add(5 * time.Minute).UTC().Second(),
 	}
-	wantTokenResponse = response
+	options["wantTokenResponse"] = response
 	renderJSON(w, r, http.StatusOK, response)
 }
 
@@ -121,8 +159,12 @@ func TestGetOIDCCallbackURL(t *testing.T) {
 	require.NoError(t, err, "Error mocking fake authorization server")
 	defer mockAuthServer.Close()
 
-	issuerGoogle = mockAuthServer.URL + "/google"
-	issuerFacebook = mockAuthServer.URL + "/facebook"
+	options := NewMockOptions()
+	defer options.Clear()
+	options["issuerGoogle"] = mockAuthServer.URL + "/google"
+	options["issuerFacebook"] = mockAuthServer.URL + "/facebook"
+	issuerGoogle := options["issuerGoogle"].(string)
+	issuerFacebook := options["issuerFacebook"].(string)
 
 	// Default OpenID Connect Provider
 	providerGoogle := auth.OIDCProvider{
@@ -152,40 +194,63 @@ func TestGetOIDCCallbackURL(t *testing.T) {
 		name             string
 		inputRequestURL  string
 		wantProviderName string
+		oidcChallenge    bool
+		expectedProvider string
 	}
 	tests := []test{
 		{
 			// When multiple providers are defined, default provider is specified and the current provider is
 			// not default, then current provider should be added to the generated OpenID Connect callback URL.
-			name:             "default_specified_but_current_is_not_default",
+			name:             "oidc default provider specified but current provider is not default",
 			inputRequestURL:  "/db/_oidc?provider=facebook&offline=true",
 			wantProviderName: "facebook",
 		}, {
+			name:             "oidc challenge default provider specified but current provider is not default",
+			inputRequestURL:  "/db/_oidc_challenge?provider=facebook&offline=true",
+			wantProviderName: "facebook",
+			expectedProvider: "provider%3Dfacebook",
+			oidcChallenge:    true,
+		}, {
 			// When multiple providers are defined, default provider is specified and the current provider is
 			// default, then current provider should NOT be added to the generated OpenID Connect callback URL.
-			name:            "default_specified_and_current_is_default",
+			name:            "oidc default provider specified and current provider is default",
 			inputRequestURL: "/db/_oidc?provider=google&offline=true",
+		}, {
+			name:            "oidc challenge default provider specified and current provider is default",
+			inputRequestURL: "/db/_oidc_challenge?provider=google&offline=true",
+			oidcChallenge:   true,
 		}, {
 			// When multiple providers are defined, default provider is specified and no current provider is
 			// provided, then provider name should NOT be added to the generated OpenID Connect callback URL.
-			name:            "default_specified_with_no_current",
+			name:            "oidc default provider specified with no current provider",
 			inputRequestURL: "/db/_oidc?offline=true",
+		}, {
+			name:            "oidc challenge default provider specified with no current provider",
+			inputRequestURL: "/db/_oidc_challenge?offline=true",
+			oidcChallenge:   true,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			resp := rt.SendAdminRequest(http.MethodGet, tc.inputRequestURL, "")
-			require.Equal(t, http.StatusFound, resp.Code)
-			location := resp.Header().Get(headerLocation)
-			require.NotEmpty(t, location, "Location should be available in response header")
-			locationURL, err := url.Parse(location)
-			require.NoError(t, err, "Location header should be a valid URL")
-			redirectURI := locationURL.Query().Get(requestParamRedirectURI)
-			require.NotEmpty(t, location, "redirect_uri should be available in auth URL")
-			redirectURL, err := url.Parse(redirectURI)
-			require.NoError(t, err, "redirect_uri should be a valid URL")
-			assert.Equal(t, tc.wantProviderName, redirectURL.Query().Get(auth.OIDCAuthProvider))
+			if tc.oidcChallenge {
+				resp := rt.SendAdminRequest(http.MethodGet, tc.inputRequestURL, "")
+				require.Equal(t, http.StatusUnauthorized, resp.Code)
+				wwwAuthHeader := resp.Header().Get("Www-Authenticate")
+				assert.Contains(t, wwwAuthHeader, tc.expectedProvider)
+			} else {
+				resp := rt.SendAdminRequest(http.MethodGet, tc.inputRequestURL, "")
+				require.Equal(t, http.StatusFound, resp.Code)
+				location := resp.Header().Get(headerLocation)
+				require.NotEmpty(t, location, "Location should be available in response header")
+				locationURL, err := url.Parse(location)
+				require.NoError(t, err, "Location header should be a valid URL")
+				redirectURI := locationURL.Query().Get(requestParamRedirectURI)
+				require.NotEmpty(t, location, "redirect_uri should be available in auth URL")
+				redirectURL, err := url.Parse(redirectURI)
+				require.NoError(t, err, "redirect_uri should be a valid URL")
+				assert.Equal(t, tc.wantProviderName, redirectURL.Query().Get(auth.OIDCAuthProvider))
+			}
 		})
 	}
 }
@@ -194,7 +259,11 @@ func TestOpenIDConnectAuth(t *testing.T) {
 	mockAuthServer, err := mockAuthServer()
 	require.NoError(t, err, "Error mocking fake authorization server")
 	defer mockAuthServer.Close()
-	issuerGoogle = mockAuthServer.URL + "/google"
+
+	options := NewMockOptions()
+	defer options.Clear()
+	options["issuerGoogle"] = mockAuthServer.URL + "/google"
+	issuerGoogle := options["issuerGoogle"].(string)
 
 	type test struct {
 		name                 string
@@ -204,6 +273,9 @@ func TestOpenIDConnectAuth(t *testing.T) {
 		wantUsername         string
 		enableAutoRegNewUser bool
 		includeAccessToken   bool
+		wantNoCode           bool
+		wantCallbackError    bool
+		wantUntoldProvider   bool
 	}
 	tests := []test{
 		{
@@ -225,8 +297,7 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
 			enableAutoRegNewUser: true,
 			includeAccessToken:   true,
-		},
-		{
+		}, {
 			name: "new user auto registration disabled single provider",
 			inputProviders: auth.OIDCProviderMap{
 				"google": &auth.OIDCProvider{
@@ -245,8 +316,7 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
 			enableAutoRegNewUser: false,
 			includeAccessToken:   true,
-		},
-		{
+		}, {
 			name: "new user auto registration enabled no access token single provider",
 			inputProviders: auth.OIDCProviderMap{
 				"google": &auth.OIDCProvider{
@@ -265,8 +335,7 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
 			enableAutoRegNewUser: true,
 			includeAccessToken:   false,
-		},
-		{
+		}, {
 			name: "new user auto registration enabled multiple providers",
 			inputProviders: auth.OIDCProviderMap{
 				"google": &auth.OIDCProvider{
@@ -295,8 +364,7 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
 			enableAutoRegNewUser: true,
 			includeAccessToken:   true,
-		},
-		{
+		}, {
 			name: "new user auto registration disabled multiple providers",
 			inputProviders: auth.OIDCProviderMap{
 				"google": &auth.OIDCProvider{
@@ -325,8 +393,7 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
 			enableAutoRegNewUser: false,
 			includeAccessToken:   true,
-		},
-		{
+		}, {
 			name: "new user auto registration disabled no access token multiple providers",
 			inputProviders: auth.OIDCProviderMap{
 				"google": &auth.OIDCProvider{
@@ -355,18 +422,93 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
 			enableAutoRegNewUser: false,
 			includeAccessToken:   false,
+		}, {
+			name: "auth code missing in callback URL received from auth server",
+			inputProviders: auth.OIDCProviderMap{
+				"google": &auth.OIDCProvider{
+					Name:               "google",
+					Issuer:             issuerGoogle,
+					ClientID:           "foo",
+					UserPrefix:         "google",
+					ValidationKey:      base.StringPointer("bar"),
+					Register:           true,
+					DiscoveryURI:       issuerGoogle + auth.DiscoveryConfigPath,
+					IncludeAccessToken: true,
+				},
+			},
+			inputDefaultProvider: "google",
+			wantUsername:         "google_noah",
+			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
+			enableAutoRegNewUser: true,
+			includeAccessToken:   true,
+			wantNoCode:           true,
+		}, {
+			name: "callback error received from auth server via callback URL",
+			inputProviders: auth.OIDCProviderMap{
+				"google": &auth.OIDCProvider{
+					Name:               "google",
+					Issuer:             issuerGoogle,
+					ClientID:           "foo",
+					UserPrefix:         "google",
+					ValidationKey:      base.StringPointer("bar"),
+					Register:           true,
+					DiscoveryURI:       issuerGoogle + auth.DiscoveryConfigPath,
+					IncludeAccessToken: true,
+				},
+			},
+			inputDefaultProvider: "google",
+			wantUsername:         "google_noah",
+			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
+			enableAutoRegNewUser: true,
+			includeAccessToken:   true,
+			wantCallbackError:    true,
+		}, {
+			name: "untold provider received from auth server via callback URL",
+			inputProviders: auth.OIDCProviderMap{
+				"google": &auth.OIDCProvider{
+					Name:               "google",
+					Issuer:             issuerGoogle,
+					ClientID:           "foo",
+					UserPrefix:         "google",
+					ValidationKey:      base.StringPointer("bar"),
+					Register:           true,
+					DiscoveryURI:       issuerGoogle + auth.DiscoveryConfigPath,
+					IncludeAccessToken: true,
+				},
+			},
+			inputDefaultProvider: "google",
+			wantUsername:         "google_noah",
+			inputRequestURL:      "/db/_oidc?provider=google&offline=true",
+			enableAutoRegNewUser: true,
+			includeAccessToken:   true,
+			wantUntoldProvider:   true,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			options := auth.OIDCOptions{Providers: tc.inputProviders, DefaultProvider: &tc.inputDefaultProvider}
-			restTesterConfig := RestTesterConfig{DatabaseConfig: &DbConfig{OIDCConfig: &options}}
+			opts := auth.OIDCOptions{Providers: tc.inputProviders, DefaultProvider: &tc.inputDefaultProvider}
+			restTesterConfig := RestTesterConfig{DatabaseConfig: &DbConfig{OIDCConfig: &opts}}
 			restTester := NewRestTester(t, &restTesterConfig)
 			defer restTester.Close()
 			mockSyncGateway := httptest.NewServer(restTester.TestPublicHandler())
 			defer mockSyncGateway.Close()
 			mockSyncGatewayURL := mockSyncGateway.URL
+
+			if tc.wantNoCode {
+				options["wantNoCode"] = true
+			}
+			defer delete(options, "wantNoCode")
+
+			if tc.wantCallbackError {
+				options["wantCallbackError"] = true
+			}
+			defer delete(options, "wantCallbackError")
+
+			if tc.wantUntoldProvider {
+				options["wantUntoldProvider"] = true
+			}
+			defer delete(options, "wantUntoldProvider")
 
 			// Initiate OpenID Connect Authorization Code flow.
 			requestURL := mockSyncGatewayURL + tc.inputRequestURL
@@ -383,15 +525,35 @@ func TestOpenIDConnectAuth(t *testing.T) {
 				assert.Equal(t, http.StatusInternalServerError, response.StatusCode)
 				return
 			}
-
-			require.Equal(t, http.StatusOK, response.StatusCode)
-
+			if tc.wantNoCode {
+				bodyBytes, err := ioutil.ReadAll(response.Body)
+				require.NoError(t, err, "error reading response body")
+				assert.Contains(t, string(bodyBytes), "Code must be present on oidc callback")
+				assert.Equal(t, http.StatusBadRequest, response.StatusCode)
+				return
+			}
+			if tc.wantCallbackError {
+				bodyBytes, err := ioutil.ReadAll(response.Body)
+				require.NoError(t, err, "error reading response body")
+				assert.Contains(t, string(bodyBytes), "oidc callback received an error")
+				assert.Equal(t, http.StatusUnauthorized, response.StatusCode)
+				return
+			}
+			if tc.wantUntoldProvider {
+				bodyBytes, err := ioutil.ReadAll(response.Body)
+				require.NoError(t, err, "error reading response body")
+				assert.Contains(t, string(bodyBytes), "Unable to identify provider for callback request")
+				assert.Equal(t, http.StatusBadRequest, response.StatusCode)
+				return
+			}
 			// Validate received token response
+			require.Equal(t, http.StatusOK, response.StatusCode)
 			var receivedToken OIDCTokenResponse
 			require.NoError(t, err, json.NewDecoder(response.Body).Decode(&receivedToken))
 			require.NoError(t, response.Body.Close(), "Error closing response body")
 			assert.NotEmpty(t, receivedToken.SessionID, "session_id doesn't exist")
 			assert.Equal(t, tc.wantUsername, receivedToken.Username, "name mismatch")
+			wantTokenResponse := options["wantTokenResponse"].(OIDCTokenResponse)
 			assert.Equal(t, wantTokenResponse.IDToken, receivedToken.IDToken, "id_token mismatch")
 			assert.Equal(t, wantTokenResponse.RefreshToken, receivedToken.RefreshToken, "refresh_token mismatch")
 			if tc.includeAccessToken {
@@ -424,6 +586,7 @@ func TestOpenIDConnectAuth(t *testing.T) {
 			// Validate received refresh token response
 			require.NoError(t, err, json.NewDecoder(response.Body).Decode(&receivedToken))
 			require.NoError(t, response.Body.Close(), "Error closing response body")
+			wantTokenResponse = options["wantTokenResponse"].(OIDCTokenResponse)
 			assert.NotEmpty(t, receivedToken.SessionID, "session_id doesn't exist")
 			assert.Equal(t, tc.wantUsername, receivedToken.Username, "name mismatch")
 			assert.Equal(t, wantTokenResponse.IDToken, receivedToken.IDToken, "id_token mismatch")
