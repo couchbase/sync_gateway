8c57c2b19c1cbfbc4f74420ca5a6687087751076
Ben Brooks <ben.brooks@couchbase.com>
2020-05-13 16:32:36 +0100

    Replace GetTestKeyNamespace with t.Name (#4572)
    * Make GetTestKeyNamespace public
    
    * Remove getTestKeyNamespace(t) in favour of t.Name()

--- base/util_testing.go
+++ base/util_testing.go
@@ -320,8 +320,3 @@ func DirExists(filename string) bool {
 	}
 	return info.IsDir()
 }
-
-// getTestKeyNamespace returns a unique doc key namespace that can be prepended in tests.
-func getTestKeyNamespace(t *testing.T) string {
-	return t.Name()
-}
