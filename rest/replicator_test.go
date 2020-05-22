5c87432b1d7c114b4243d95a22f8c090c85f41f4
Ben Brooks <ben@bbrks.me>
2020-05-22 17:06:35 +0100

    Use public REST API with user and rename test

--- rest/replicator_test.go
+++ rest/replicator_test.go
@@ -7,33 +7,36 @@ import (
 	"testing"
 
 	"github.com/couchbase/sync_gateway/base"
+	"github.com/couchbase/sync_gateway/db"
 	"github.com/couchbase/sync_gateway/replicator"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSGR2Pull(t *testing.T) {
-	if base.GTestBucketPool.NumUsableBuckets() < 2 {
-		t.Skip("need at least 2 usable buckets for this test")
-	}
-
+// TestActiveReplicatorBlipsync starts a RestTester, and publishes the REST API on a httptest server, which the ActiveReplicator can call blipsync on.
+func TestActiveReplicatorBlipsync(t *testing.T) {
 	defer base.SetUpTestLogging(base.LevelTrace, base.KeyHTTP, base.KeyHTTPResp, base.KeySync, base.KeySyncMsg, base.KeyReplicate)()
 
 	// 2 single-node clusters:
-	rt1 := NewRestTester(t, &RestTesterConfig{DatabaseConfig: &DbConfig{
-		// active replicator config
-	}})
-	defer rt1.Close()
-	rt2 := NewRestTester(t, &RestTesterConfig{DatabaseConfig: &DbConfig{
-		// passive node config
-	}})
-	defer rt2.Close()
+	rt := NewRestTester(t, &RestTesterConfig{
+		DatabaseConfig: &DbConfig{
+			Users: map[string]*db.PrincipalConfig{
+				"alice": {Password: base.StringPtr("pass")},
+			},
+		},
+		noAdminParty: true,
+	})
+	defer rt.Close()
 
-	// Make rt2 listen on an actual HTTP port, so it can receive the blipsync request.
-	srv := httptest.NewServer(rt2.TestAdminHandler())
+	// Make rt listen on an actual HTTP port, so it can receive the blipsync request.
+	srv := httptest.NewServer(rt.TestPublicHandler())
 	defer srv.Close()
 
 	targetDB, err := url.Parse(srv.URL + "/db")
 	require.NoError(t, err)
+
+	// Add basic auth creds to target db URL
+	targetDB.User = url.UserPassword("alice", "pass")
 
 	bar, err := replicator.NewBidirectionalActiveReplicator(context.Background(), &replicator.ActiveReplicatorConfig{
 		ID:        t.Name(),
@@ -41,8 +44,8 @@ func TestSGR2Pull(t *testing.T) {
 		TargetDB:  targetDB,
 	})
 	require.NoError(t, err)
-	defer bar.Close()
 
-	require.NoError(t, bar.Connect())
-	require.NoError(t, bar.Start())
+	assert.NoError(t, bar.Connect())
+	assert.NoError(t, bar.Start())
+	assert.NoError(t, bar.Close())
 }
