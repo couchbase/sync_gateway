08b728c965e649c900ffde3618b08276f16afd50
Ben Brooks <ben@bbrks.me>
2020-06-22 15:47:04 +0100

    Rip out unused checkpointer code

--- db/active_replicator_push.go
+++ db/active_replicator_push.go
@@ -1,10 +1,7 @@
 package db
 
 import (
-	"context"
-	"expvar"
 	"fmt"
-	"sync"
 
 	"github.com/couchbase/go-blip"
 	"github.com/couchbase/sync_gateway/base"
@@ -15,46 +12,6 @@ type ActivePushReplicator struct {
 	config          *ActiveReplicatorConfig
 	blipSyncContext *BlipSyncContext
 	blipSender      *blip.Sender
-	ctx             context.Context // closes checkpointer goroutine on cancel (can be a context passed from a parent DB)
-	Checkpointer    *PushCheckpointer
-	Stats           expvar.Map
-}
-
-type PushCheckpointer struct {
-	// lastCheckpointRevID is the last known checkpoint RevID.
-	lastCheckpointRevID string
-	// lastCheckpointSeq is the last checkpointed sequence
-	lastCheckpointSeq string
-	// runNow can be sent signals to run a checkpoint (useful for testing)
-	runNow chan struct{}
-	// wg is used to block close until a checkpoint has finished
-	wg sync.WaitGroup
-	// lock guards the expectedSeqs slice, and receivedSeqs map
-	lock sync.Mutex
-	// expectedSeqs is an ordered list of sequence IDs we expect to receive revs for
-	expectedSeqs []string
-	// receivedSeqs is a map of sequence IDs we've received revs for
-	receivedSeqs map[string]struct{}
-}
-
-func NewPushCheckpointer() *PushCheckpointer {
-	return &PushCheckpointer{
-		lock:         sync.Mutex{},
-		expectedSeqs: make([]string, 0),
-		receivedSeqs: make(map[string]struct{}),
-		runNow:       make(chan struct{}),
-	}
-}
-
-func NewPushReplicator(ctx context.Context, config *ActiveReplicatorConfig) *ActivePushReplicator {
-	blipContext := blip.NewContextCustomID(config.ID+"-push", blipCBMobileReplication)
-	bsc := NewBlipSyncContext(blipContext, config.ActiveDB, blipContext.ID)
-	return &ActivePushReplicator{
-		config:          config,
-		blipSyncContext: bsc,
-		ctx:             ctx,
-		Checkpointer:    NewPushCheckpointer(),
-	}
 }
 
 // CheckpointID returns a unique ID to be used for the checkpoint client (which is used as part of the checkpoint Doc ID on the recipient)
@@ -136,8 +93,6 @@ func (apr *ActivePushReplicator) Start() error {
 	}
 
 	checkpoint := apr.getCheckpoint()
-	apr.Checkpointer.lastCheckpointRevID = checkpoint.RevID
-	apr.Checkpointer.lastCheckpointSeq = checkpoint.Checkpoint.LastSequence
 
 	if err := apr.startCheckpointer(); err != nil {
 		return err
@@ -149,11 +104,10 @@ func (apr *ActivePushReplicator) Start() error {
 		serialNumber:    apr.blipSyncContext.incrementSerialNumber(),
 	}
 
+	// TODO: set bh.channels and these other options via sendChanges parameters (CBG-915)
 	bh.continuous = apr.config.Continuous
 	bh.batchSize = int(apr.config.ChangesBatchSize)
 	bh.activeOnly = apr.config.ActiveOnly
-
-	// TODO: set bh.channels and other options (CBG-915)
 
 	seq, err := apr.config.ActiveDB.ParseSequenceID(checkpoint.Checkpoint.LastSequence)
 	if err != nil {
