e181625334ea89a75c437f5f7cfb330ed1bcdc52
Adam Fraser <adam.fraser@couchbase.com>
2020-06-23 03:30:48 -0700

    CBG-777 Javascript custom conflict resolver (#4626)
    * CBG-777 Javascript custom conflict resolver
    
    Javascript custom conflict resolver taking a conflict as input, and returning a document body.
    
    * Whitespace fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>

--- db/sg_replicate_conflict_resolver.go
+++ db/sg_replicate_conflict_resolver.go
@@ -1,6 +1,13 @@
 package db
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	sgbucket "github.com/couchbase/sg-bucket"
+	"github.com/couchbase/sync_gateway/base"
+	"github.com/robertkrimen/otto"
+)
 
 type ConflictResolverType string
 
@@ -85,6 +92,59 @@ func NewConflictResolverFunc(resolverType ConflictResolverType, customResolverSo
 // NewCustomConflictResolver returns a ConflictResolverFunc that executes the
 // javascript conflict resolver specified by source
 func NewCustomConflictResolver(source string) (ConflictResolverFunc, error) {
-	// TODO: CBG-777
-	return nil, nil
+	conflictResolverJSServer := NewConflictResolverJSServer(source)
+	return conflictResolverJSServer.EvaluateFunction, nil
+}
+
+// ConflictResolverJSServer manages the compiled javascript function runner instance
+type ConflictResolverJSServer struct {
+	*sgbucket.JSServer
+}
+
+func NewConflictResolverJSServer(fnSource string) *ConflictResolverJSServer {
+	base.Debugf(base.KeyReplicate, "Creating new ConflictResolverFunction")
+	return &ConflictResolverJSServer{
+		JSServer: sgbucket.NewJSServer(fnSource, kTaskCacheSize, newConflictResolverRunner),
+	}
+}
+
+// EvaluateFunction executes the conflict resolver with the provided conflict and returns the result.
+func (i *ConflictResolverJSServer) EvaluateFunction(conflict Conflict) (Body, error) {
+
+	docID, _ := conflict.LocalDocument[BodyId].(string)
+	localRevID, _ := conflict.LocalDocument[BodyRev].(string)
+	remoteRevID, _ := conflict.RemoteDocument[BodyRev].(string)
+	result, err := i.Call(conflict)
+	if err != nil {
+		base.Warnf("Unexpected error invoking conflict resolver for document %s, local/remote revisions %s/%s - processing aborted, document will not be replicated.  Error: %v",
+			base.UD(docID), base.UD(localRevID), base.UD(remoteRevID), err)
+		return nil, err
+	}
+	switch result := result.(type) {
+	case Body:
+		return result, nil
+	case map[string]interface{}:
+		return result, nil
+	default:
+		base.Warnf("Custom conflict resolution function returned non-document result %v Type: %T", result, result)
+		return nil, errors.New("Custom conflict resolution function returned non-document value.")
+	}
+}
+
+// Compiles a JavaScript event function to a conflictResolverRunner object.
+func newConflictResolverRunner(funcSource string) (sgbucket.JSServerTask, error) {
+	conflictResolverRunner := &sgbucket.JSRunner{}
+	err := conflictResolverRunner.InitWithLogging(funcSource,
+		func(s string) { base.Errorf(base.KeyJavascript.String()+": ConflictResolver %s", base.UD(s)) },
+		func(s string) { base.Infof(base.KeyJavascript, "ConflictResolver %s", base.UD(s)) })
+	if err != nil {
+		return nil, err
+	}
+
+	conflictResolverRunner.After = func(result otto.Value, err error) (interface{}, error) {
+		nativeValue, _ := result.Export()
+		return nativeValue, err
+	}
+
+	return conflictResolverRunner, nil
 }
