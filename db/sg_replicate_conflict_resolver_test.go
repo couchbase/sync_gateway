676cee02e6a9cc62725c7a9b4cdd28d268de9493
Adam Fraser <adam.fraser@couchbase.com>
2020-06-22 22:37:06 -0700

    CBG-777 Javascript custom conflict resolver
    Javascript custom conflict resolver taking a conflict as input, and returning a document body.

--- db/sg_replicate_conflict_resolver_test.go
+++ db/sg_replicate_conflict_resolver_test.go
@@ -3,10 +3,11 @@ package db
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/assert"
 )
 
-// Test node operations on SGReplicateManager
 func TestDefaultConflictResolver(t *testing.T) {
 
 	defaultConflictResolverTests := []struct {
@@ -54,6 +55,57 @@ func TestDefaultConflictResolver(t *testing.T) {
 				RemoteDocument: test.remoteDocument,
 			}
 			result, err := DefaultConflictResolver(conflict)
+			assert.NoError(tt, err)
+			assert.Equal(tt, test.expectedWinner, result)
+		})
+	}
+}
+
+func TestCustomConflictResolver(t *testing.T) {
+
+	defaultConflictResolverTests := []struct {
+		name           string
+		resolverSource string
+		localDocument  Body
+		remoteDocument Body
+		expectedWinner Body
+	}{
+		{
+			name:           "localWins",
+			resolverSource: `function(conflict) { return conflict.LocalDocument; }`,
+			localDocument:  Body{"_rev": "2-abc"},
+			remoteDocument: Body{"_rev": "1-abc"},
+			expectedWinner: Body{"_rev": "2-abc"},
+		},
+		{
+			name:           "remoteWins",
+			resolverSource: `function(conflict) { return conflict.RemoteDocument; }`,
+			localDocument:  Body{"_rev": "2-abc"},
+			remoteDocument: Body{"_rev": "1-abc"},
+			expectedWinner: Body{"_rev": "1-abc"},
+		},
+		{
+			name: "merge",
+			resolverSource: `function(conflict) { 
+				var mergedDoc = new Object();
+				mergedDoc.prop = conflict.LocalDocument.prop + conflict.RemoteDocument.prop;
+				return mergedDoc;
+			}`,
+			localDocument:  Body{"_rev": "2-abc", "prop": "foo"},
+			remoteDocument: Body{"_rev": "1-abc", "prop": "bar"},
+			expectedWinner: Body{"prop": "foobar"},
+		},
+	}
+
+	for _, test := range defaultConflictResolverTests {
+		t.Run(test.name, func(tt *testing.T) {
+			conflict := Conflict{
+				LocalDocument:  test.localDocument,
+				RemoteDocument: test.remoteDocument,
+			}
+			customConflictResolverFunc, err := NewCustomConflictResolver(test.resolverSource)
+			require.NoError(tt, err)
+			result, err := customConflictResolverFunc(conflict)
 			assert.NoError(tt, err)
 			assert.Equal(tt, test.expectedWinner, result)
 		})
