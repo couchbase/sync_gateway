83cdf36ee95180ffb0569f6d383d96288b78f454
Ben Brooks <ben.brooks@couchbase.com>
2020-08-14 17:18:18 +0100

    Handle getting underlying GoCB Bucket in CouchbaseURIToHttpURL

--- base/heartbeat_test.go
+++ base/heartbeat_test.go
@@ -288,6 +288,7 @@ func TestCBGTManagerHeartbeater(t *testing.T) {
 
 	// Initialize cfgCB
 	cfgCB, err := initCfgCB(testBucket, testBucket.BucketSpec)
+	require.NoError(t, err)
 
 	// Simulate the three nodes self-registering into the cfg
 	nodeDefs := cbgt.NewNodeDefs("1.0.0")
