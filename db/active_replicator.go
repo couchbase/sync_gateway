7f00ebb411f005d265888b5a551db859d9b9b912
Ben Brooks <ben.brooks@couchbase.com>
2020-08-25 18:32:43 +0100

    Reverse context error check for reconnect

--- db/active_replicator.go
+++ db/active_replicator.go
@@ -190,7 +190,7 @@ func connect(arc *activeReplicatorCommon, idSuffix string) (blipSender *blip.Sen
 	blipContext.WebsocketPingInterval = arc.config.WebsocketPingInterval
 	blipContext.OnExitCallback = func() {
 		// fall into a reconnect loop only if the connection is unexpectedly closed.
-		if arc.ctx.Err() != nil {
+		if arc.ctx.Err() == nil {
 			go arc.reconnectLoop()
 		}
 	}
