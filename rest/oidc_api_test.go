0f2cabb6bdff17280657ce03ebaabc551443c801
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-09-16 21:33:44 -0700

    CBG-1082: Include token_endpoint_auth_methods_supported option in mocked discovery response

--- rest/oidc_api_test.go
+++ rest/oidc_api_test.go
@@ -208,6 +208,13 @@ func (s *mockAuthServer) discoveryHandler(w http.ResponseWriter, r *http.Request
 		JwksUri:                          issuer + "/keys",
 		AuthorizationEndpoint:            issuer + "/auth",
 		IdTokenSigningAlgValuesSupported: []string{"RS256"},
+		TokenEndpointAuthMethodsSupported: []string{
+			"client_secret_basic",
+			"client_secret_post",
+			"client_secret_jwt",
+			"private_key_jwt",
+			"none",
+		},
 	}
 	renderJSON(w, r, http.StatusOK, metadata)
 }
