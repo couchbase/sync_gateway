6aef670a3f3c74010b4bda8cb89bda7d9862bac0
Ben Brooks <ben.brooks@couchbase.com>
2020-10-30 18:04:46 +0000

    Tweak log message

--- db/changes.go
+++ db/changes.go
@@ -205,7 +205,7 @@ func (db *Database) changesFeed(singleChannelCache SingleChannelCache, options C
 			}
 
 			// TODO: pass db.Ctx down to changeCache?
-			base.TracefCtx(db.Ctx, base.KeyChanges, "Querying channel %q with pagination options: %+v", base.UD(singleChannelCache.ChannelName()), paginationOptions)
+			base.TracefCtx(db.Ctx, base.KeyChanges, "Querying channel %q with options: %+v", base.UD(singleChannelCache.ChannelName()), paginationOptions)
 			changes, err := singleChannelCache.GetChanges(paginationOptions)
 			if err != nil {
 				base.WarnfCtx(db.Ctx, "Error retrieving changes for channel %q: %v", base.UD(singleChannelCache.ChannelName()), err)
