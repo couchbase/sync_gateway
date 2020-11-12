0a99ab7fdfd4e83b225bfa7588b0226cc26d2f72
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2020-11-12 13:11:01 -0800

    Fix typo in test_with_coverage.sh

--- test_with_coverage.sh
+++ test_with_coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-set -x 
+set -x
 
 ## Go Tests
 echo "Testing code with 'go test' ..."
@@ -22,12 +22,12 @@ echo "Running Sync Gateway unit tests against Walrus"
 SG_TEST_USE_XATTRS="false" SG_TEST_BACKING_STORE=Walrus gocoverutil -coverprofile=cover_sg.out test -v "$@" -covermode=count github.com/couchbase/sync_gateway/...
 go tool cover -html=cover_sg.out -o cover_sg.html
 
-echo "Running Sync Gateway integraton unit tests (XATTRS=false)"
+echo "Running Sync Gateway integration unit tests (XATTRS=false)"
 echo "Integration mode: tests to run in serial across packages by default using gocoverutil"
 SG_TEST_USE_XATTRS="false" SG_TEST_BACKING_STORE=Couchbase gocoverutil -coverprofile=cover_sg_integration_xattrs_false.out test -v "$@" -covermode=count github.com/couchbase/sync_gateway/...
 go tool cover -html=cover_sg_integration_xattrs_false.out -o cover_sg_integration_xattrs_false.html
 
-echo "Running Sync Gateway integraton unit tests (XATTRS=true)"
+echo "Running Sync Gateway integration unit tests (XATTRS=true)"
 echo "Integration mode: tests to run in serial across packages by default using gocoverutil"
 SG_TEST_USE_XATTRS="true" SG_TEST_BACKING_STORE=Couchbase gocoverutil -coverprofile=cover_sg_integration_xattrs_true.out test -v "$@" -covermode=count github.com/couchbase/sync_gateway/...
 go tool cover -html=cover_sg_integration_xattrs_true.out -o cover_sg_integration_xattrs_true.html
