b7710aa058837be7bd2b70485f1b33d3f94b4a37
Ben Brooks <ben.brooks@couchbase.com>
2020-12-22 15:02:38 +0000

    Add String method to EventType for better logging

---
+++ db/event_test.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventTypeNames(t *testing.T) {
+	// Ensure number of level constants, and names match.
+	assert.Equal(t, int(eventTypeCount), len(eventTypeNames))
+
+	assert.Equal(t, "DocumentChange", DocumentChange.String())
+	assert.Equal(t, "DBStateChange", DBStateChange.String())
+
+	// Test out of bounds event type
+	assert.Equal(t, "EventType(255)", EventType(math.MaxUint8).String())
+}
