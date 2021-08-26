9985b7806351b237a321853c894db371d986bb49
Isaac Lambat <isaac.lambat@couchbase.com>
2021-08-26 17:03:31 +0100

    CBG-1652 - Fixed panic on /_config/include_javascript=false when no webhooks used (#5208)
    * CBG-1652 - Fixed panic on /_config/include_javascript=false when no webhooks used
    
    * Shortend nil check, added DocumentChange iteration
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>

--- rest/admin_api.go
+++ rest/admin_api.go
@@ -189,11 +189,13 @@ func (h *handler) handleGetDbConfig() error {
 	if !includeJavascript {
 		responseConfig.Sync = nil
 		responseConfig.ImportFilter = nil
-		for _, evt := range responseConfig.EventHandlers.DBStateChanged {
-			evt.Filter = ""
-		}
-		for _, evt := range responseConfig.EventHandlers.DBStateChanged {
-			evt.Filter = ""
+		if responseConfig.EventHandlers != nil {
+			for _, evt := range responseConfig.EventHandlers.DocumentChanged {
+				evt.Filter = ""
+			}
+			for _, evt := range responseConfig.EventHandlers.DBStateChanged {
+				evt.Filter = ""
+			}
 		}
 	}
 
