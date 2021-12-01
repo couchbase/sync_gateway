277014f05ca694f938fb2d83da03b2a99b67549e
Jacques Rascagneres <jacquesrascagneres@gmail.com>
2021-12-01 17:39:50 +0000

    Split out Background Manager processes (#5354)

---
+++ db/background_mgr_resync.go
@@ -0,0 +1,86 @@
+//  Copyright 2012-Present Couchbase, Inc.
+//
+//  Use of this software is governed by the Business Source License included
+//  in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+//  in that file, in accordance with the Business Source License, use of this
+//  software will be governed by the Apache License, Version 2.0, included in
+//  the file licenses/APL2.txt.
+
+package db
+
+import (
+	"sync"
+	"sync/atomic"
+
+	"github.com/couchbase/sync_gateway/base"
+)
+
+// ======================================================
+// Resync Implementation of Background Manager Process
+// ======================================================
+
+type ResyncManager struct {
+	DocsProcessed int
+	DocsChanged   int
+	lock          sync.Mutex
+}
+
+var _ BackgroundManagerProcessI = &ResyncManager{}
+
+func NewResyncManager() *BackgroundManager {
+	return &BackgroundManager{
+		Process:    &ResyncManager{},
+		terminator: base.NewSafeTerminator(),
+	}
+}
+
+func (r *ResyncManager) Init(options map[string]interface{}, clusterStatus []byte) error {
+	return nil
+}
+
+func (r *ResyncManager) Run(options map[string]interface{}, persistClusterStatusCallback updateStatusCallbackFunc, terminator *base.SafeTerminator) error {
+	database := options["database"].(*Database)
+	regenerateSequences := options["regenerateSequences"].(bool)
+
+	defer atomic.CompareAndSwapUint32(&database.State, DBResyncing, DBOffline)
+	callback := func(docsProcessed, docsChanged *int) {
+		r.lock.Lock()
+		defer r.lock.Unlock()
+		r.DocsProcessed = *docsProcessed
+		r.DocsChanged = *docsChanged
+	}
+
+	_, err := database.UpdateAllDocChannels(regenerateSequences, callback, terminator)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *ResyncManager) ResetStatus() {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	r.DocsProcessed = 0
+	r.DocsChanged = 0
+}
+
+type ResyncManagerResponse struct {
+	BackgroundManagerStatus
+	DocsChanged   int `json:"docs_changed"`
+	DocsProcessed int `json:"docs_processed"`
+}
+
+func (r *ResyncManager) GetProcessStatus(backgroundManagerStatus BackgroundManagerStatus) ([]byte, error) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	retStatus := ResyncManagerResponse{
+		BackgroundManagerStatus: backgroundManagerStatus,
+		DocsChanged:             r.DocsChanged,
+		DocsProcessed:           r.DocsProcessed,
+	}
+
+	return base.JSONMarshal(retStatus)
+}
---
+++ db/background_mgr_tombstone_compaction.go
@@ -0,0 +1,74 @@
+//  Copyright 2012-Present Couchbase, Inc.
+//
+//  Use of this software is governed by the Business Source License included
+//  in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+//  in that file, in accordance with the Business Source License, use of this
+//  software will be governed by the Apache License, Version 2.0, included in
+//  the file licenses/APL2.txt.
+
+package db
+
+import (
+	"sync/atomic"
+	"time"
+
+	"github.com/couchbase/sync_gateway/base"
+)
+
+// =====================================================================
+// Tombstone Compaction Implementation of Background Manager Process
+// =====================================================================
+
+type TombstoneCompactionManager struct {
+	PurgedDocCount int64
+}
+
+var _ BackgroundManagerProcessI = &TombstoneCompactionManager{}
+
+func NewTombstoneCompactionManager() *BackgroundManager {
+	return &BackgroundManager{
+		Process:    &TombstoneCompactionManager{},
+		terminator: base.NewSafeTerminator(),
+	}
+}
+
+func (t *TombstoneCompactionManager) Init(options map[string]interface{}, clusterStatus []byte) error {
+	database := options["database"].(*Database)
+	database.DbStats.Database().CompactionAttachmentStartTime.Set(time.Now().UTC().Unix())
+
+	return nil
+}
+
+func (t *TombstoneCompactionManager) Run(options map[string]interface{}, persistClusterStatusCallback updateStatusCallbackFunc, terminator *base.SafeTerminator) error {
+	database := options["database"].(*Database)
+
+	defer atomic.CompareAndSwapUint32(&database.CompactState, DBCompactRunning, DBCompactNotRunning)
+	callback := func(docsPurged *int) {
+		atomic.StoreInt64(&t.PurgedDocCount, int64(*docsPurged))
+	}
+
+	_, err := database.Compact(true, callback, terminator)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type TombstoneManagerResponse struct {
+	BackgroundManagerStatus
+	DocsPurged int64 `json:"docs_purged"`
+}
+
+func (t *TombstoneCompactionManager) GetProcessStatus(backgroundManagerStatus BackgroundManagerStatus) ([]byte, error) {
+	retStatus := TombstoneManagerResponse{
+		BackgroundManagerStatus: backgroundManagerStatus,
+		DocsPurged:              atomic.LoadInt64(&t.PurgedDocCount),
+	}
+
+	return base.JSONMarshal(retStatus)
+}
+
+func (t *TombstoneCompactionManager) ResetStatus() {
+	atomic.StoreInt64(&t.PurgedDocCount, 0)
+}
