acb31487870521c4aed64089e1fd45e563d2044a
Isaac Lambat <isaac.lambat@couchbase.com>
2022-02-28 23:27:16 +0000

    CBG-1950: Removed Sync Gateway Accel service scripts and references (#5451)

--- service/sg-windows/build.cmd
+++ service/sg-windows/build.cmd
@@ -11,7 +11,4 @@ SET GOPATH=%cd%;%GO_PATH%
 REM Build the Sync Gateway service wrapper
 go build -o sg-windows.exe sg-service\sg-service.go
 
-REM Build the Sync Gateway Accelerator service wrapper
-go build -o sg-accel-service.exe sg-accel-service\sg-accel-service.go
-
 ENDLOCAL
