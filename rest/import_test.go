f1905600a3812a9016d74c6359c3494df273d569
Adam Fraser <adam.fraser@couchbase.com>
2022-05-10 09:47:10 -0700

    failing import test w/ touch

--- rest/import_test.go
+++ rest/import_test.go
@@ -2363,3 +2363,60 @@ func TestImportInternalPropertiesHandling(t *testing.T) {
 		})
 	}
 }
+
+// Test import of an SDK expiry change
+func TestImportTouch(t *testing.T) {
+
+	SkipImportTestsIfNotEnabled(t)
+
+	rtConfig := RestTesterConfig{
+		SyncFn: `function(doc, oldDoc) {
+			if (oldDoc == null) {
+				channel("oldDocNil")
+			} 
+			if (doc._deleted) {
+				channel("docDeleted")
+			}
+		}`,
+		DatabaseConfig: &DatabaseConfig{DbConfig: DbConfig{
+			AutoImport: false,
+		}},
+	}
+	rt := NewRestTester(t, &rtConfig)
+	defer rt.Close()
+
+	bucket := rt.Bucket()
+
+	// 1. Create a document
+	key := "TestImportTouch"
+	docBody := make(map[string]interface{})
+	docBody["test"] = "TestImportTouch"
+	docBody["channels"] = "ABC"
+
+	_, err := bucket.Add(key, 0, docBody)
+	assert.NoError(t, err, "Unable to insert doc TestImportDelete")
+
+	// Attempt to get the document via Sync Gateway, to trigger import.
+	response := rt.SendAdminRequest("GET", "/db/_raw/"+key+"?redact=false", "")
+	assert.Equal(t, 200, response.Code)
+	var rawInsertResponse RawResponse
+	err = base.JSONUnmarshal(response.Body.Bytes(), &rawInsertResponse)
+	assert.NoError(t, err, "Unable to unmarshal raw response")
+	initialRev := rawInsertResponse.Sync.Rev
+
+	// 2. Test import behaviour after SDK touch
+	updatedBody := make(map[string]interface{})
+	updatedBody["test"] = "TestImportDelete"
+	updatedBody["channels"] = "HBO"
+
+	_, err = bucket.Touch(key, 1000000)
+	assert.NoError(t, err, "Unable to touch doc TestImportTouch")
+
+	// Attempt to get the document via Sync Gateway, to trigger import.
+	response = rt.SendAdminRequest("GET", "/db/_raw/"+key+"?redact=false", "")
+	assert.Equal(t, 200, response.Code)
+	var rawUpdateResponse RawResponse
+	err = base.JSONUnmarshal(response.Body.Bytes(), &rawUpdateResponse)
+	assert.NoError(t, err, "Unable to unmarshal raw response")
+	assert.Equal(t, initialRev, rawUpdateResponse.Sync.Rev)
+}
