733d2f3e5ca60b1af34ee65bf90c6a1a008bad84
Marks Polakovs <marks.polakovs@couchbase.com>
2022-05-10 11:49:35 +0100

    Review comments

--- db/database_test.go
+++ db/database_test.go
@@ -67,9 +67,9 @@ func setupTestDBForBucketWithOptions(t testing.TB, tBucket base.Bucket, dbcOptio
 func setupTestDBWithOptionsAndImport(t testing.TB, dbcOptions DatabaseContextOptions) *Database {
 	AddOptionsFromEnvironmentVariables(&dbcOptions)
 	if dbcOptions.GroupID == "" && base.IsEnterpriseEdition() {
-		// In a real SG this would be set in DefaultStartupConfig.
-		dbcOptions.GroupID = "default"
-		RegisterImportPindexImpl("default")
+		// TODO: Once RegisterImportPindexImpl is moved into NewDatabaseContext this won't be necessary
+		dbcOptions.GroupID = t.Name()
+		RegisterImportPindexImpl(t.Name())
 	}
 	context, err := NewDatabaseContext("db", base.GetTestBucket(t), true, dbcOptions)
 	require.NoError(t, err, "Couldn't create context for database 'db'")
