a8dc0838c9335a6dabe8a12fd8ec64493940c4d9
Marks Polakovs <marks.polakovs@couchbase.com>
2022-07-25 13:54:56 +0100

    Use existing X509 utilities

--- base/dcp_sharded.go
+++ base/dcp_sharded.go
@@ -347,16 +347,14 @@ func initCBGTManager(bucket Bucket, spec BucketSpec, cfgSG cbgt.Cfg, dbUUID stri
 		if err != nil {
 			return nil, fmt.Errorf("failed to fetch UUID of bucket %v: %w", MD(bucket.GetName()).Redact(), err)
 		}
-		if spec.CACertPath != "" {
-			certs, err := loadCertificatesFromFile(spec.CACertPath)
-			if err != nil {
-				return nil, err
-			}
-			setCbgtRootCertsForBucket(bucketUUID, certs)
-		} else if spec.TLSSkipVerify {
+		if spec.TLSSkipVerify {
 			setCbgtRootCertsForBucket(bucketUUID, nil)
 		} else {
-			setCbgtRootCertsForBucket(bucketUUID, getSystemCertPool())
+			certs, err := getRootCAs(spec.CACertPath)
+			if err != nil {
+				return nil, fmt.Errorf("failed to load root CAs: %w", err)
+			}
+			setCbgtRootCertsForBucket(bucketUUID, certs)
 		}
 	}
 
