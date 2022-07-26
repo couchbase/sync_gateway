1734a85d3c38980457222ba0eb0454fec1152afd
Ben Brooks <ben.brooks@couchbase.com>
2022-07-26 16:54:21 +0100

    CBG-2022: Cherry-pick: Comment out the problematic test case for CBS < 7.1 (MB-50619) (#5656)

--- db/query_test.go
+++ db/query_test.go
@@ -456,7 +456,9 @@ func TestAccessQuery(t *testing.T) {
 
 	// Attempt to introduce syntax errors. Each of these should return zero rows and no error.
 	// Validates select clause protection
-	for _, username := range []string{"user1'", "user1`AND", "user1?", "user1 ! user2$"} {
+	usernames := []string{"user1'", "user1?", "user1 ! user2$"}
+	// usernames = append(usernames, "user1`AND") // TODO: MB-50619 - broken until Server 7.1.0
+	for _, username := range usernames {
 		results, queryErr = db.QueryAccess(username)
 		assert.NoError(t, queryErr, "Query error")
 		rowCount = 0
@@ -499,7 +501,9 @@ func TestRoleAccessQuery(t *testing.T) {
 
 	// Attempt to introduce syntax errors. Each of these should return zero rows and no error.
 	// Validates select clause protection
-	for _, username := range []string{"user1'", "user1`AND", "user1?", "user1 ! user2$"} {
+	usernames := []string{"user1'", "user1?", "user1 ! user2$"}
+	// usernames = append(usernames, "user1`AND") // TODO: MB-50619 - broken until Server 7.1.0
+	for _, username := range usernames {
 		results, queryErr = db.QueryRoleAccess(username)
 		assert.NoError(t, queryErr, "Query error")
 		rowCount = 0
