1bb299fe760e5e4abe9d0b5ac73bd39ef86c03b1
Isaac Lambat <isaac.lambat@couchbase.com>
2022-08-18 12:56:55 +0100

    Added retry loop to AssertLogContains

--- base/logging.go
+++ base/logging.go
@@ -374,10 +374,18 @@ func LogTraceEnabled(logKey LogKey) bool {
 func AssertLogContains(t *testing.T, s string, f func()) {
 	b := bytes.Buffer{}
 
-	// temporarily override logger output for the given function call
+	// Temporarily override logger output for the given function call
 	consoleLogger.logger.SetOutput(&b)
 	f()
+	// Allow time for logs to be printed
+	retry := func() (shouldRetry bool, err error, value interface{}) {
+		if strings.Contains(b.String(), s) {
+			return false, nil, nil
+		}
+		return true, nil, nil
+	}
+	err, _ := RetryLoop("wait for logs", retry, CreateSleeperFunc(10, 100))
 	consoleLogger.logger.SetOutput(os.Stderr)
 
-	assert.Contains(t, b.String(), s)
+	assert.NoError(t, err, "Console logs did not contain %q", s)
 }
