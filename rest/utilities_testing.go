80c9cb967d0f9ff1bb57f92452e3af125532feb0
Tor Colvin <tor.colvin@couchbase.com>
2022-08-19 10:08:32 -0400

    restore to original

--- rest/utilities_testing.go
+++ rest/utilities_testing.go
@@ -558,6 +558,7 @@ func (rt *RestTester) SendAdminRequest(method, resource string, body string) *Te
 	require.NoError(rt.tb, err)
 
 	response := &TestResponse{ResponseRecorder: httptest.NewRecorder(), Req: request}
+	response.Code = 200 // doesn't seem to be initialized by default; filed Go bug #4188
 
 	rt.TestAdminHandler().ServeHTTP(response, request)
 	return response
@@ -1178,12 +1179,12 @@ func getChangesHandler(changesFinishedWg, revsFinishedWg *sync.WaitGroup) func(r
 //
 // - Call subChanges (continuous=false) endpoint to get all changes from Sync Gateway
 // - Respond to each "change" request telling the other side to send the revision
-//   - NOTE: this could be made more efficient by only requesting the revision for the docid/revid pair
-//     passed in the parameter.
-//
+//		- NOTE: this could be made more efficient by only requesting the revision for the docid/revid pair
+//              passed in the parameter.
 // - If the rev handler is called back with the desired docid/revid pair, save that into a variable that will be returned
 // - Block until all pending operations are complete
 // - Return the resultDoc or an empty resultDoc
+//
 func (bt *BlipTester) GetDocAtRev(requestedDocID, requestedDocRev string) (resultDoc RestDocument, err error) {
 
 	docs := map[string]RestDocument{}
