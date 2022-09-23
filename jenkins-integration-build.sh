025a188988eb49e8b700b717ecb4755538b453b2
Ben Brooks <ben.brooks@couchbase.com>
2022-09-23 17:32:34 +0100

    Add addlicense template, CI check and run `addlicense` fo existing files (#5740)
    * Add `addlicense` template
    
    * Add CI check
    
    * Bump outdated GH Actions step versions
    
    * Run `addlicense -f licenses/addlicense.tmpl .`

--- jenkins-integration-build.sh
+++ jenkins-integration-build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# Copyright 2022-Present Couchbase, Inc.
+#
+# Use of this software is governed by the Business Source License included
+# in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+# in that file, in accordance with the Business Source License, use of this
+# software will be governed by the Apache License, Version 2.0, included in
+# the file licenses/APL2.txt.
+
 DEFAULT_PACKAGE_TIMEOUT="45m"
 
 set -u
