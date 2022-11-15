117d8872ea07f3fb6bc656a21c68a5422390afc1
adamcfraser <adam.fraser@couchbase.com>
2022-11-14 16:55:19 -0800

    Update licenses

--- auth/role_collection_access.go
+++ auth/role_collection_access.go
@@ -1,8 +1,16 @@
+// Copyright 2022-Present Couchbase, Inc.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+// in that file, in accordance with the Business Source License, use of this
+// software will be governed by the Apache License, Version 2.0, included in
+// the file licenses/APL2.txt.
+
 package auth
 
 import (
 	"fmt"
-	
+
 	"github.com/couchbase/sync_gateway/base"
 	ch "github.com/couchbase/sync_gateway/channels"
 )
