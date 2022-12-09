725e8c3e87ff88282f5757a79b36712e22747e03
Mohammed Madi <madim0044@gmail.com>
2022-12-09 12:55:16 +0000

    Add star channel handling and unit test

--- db/changes.go
+++ db/changes.go
@@ -347,7 +347,7 @@ func (db *DatabaseCollectionWithUser) wasDocInChannelPriorToRevocation(ctx conte
 	// Iterate over the channel history information on the document and find any periods where the doc was in the
 	// channel and the channel was accessible by the user
 	for _, docHistoryEntry := range append(syncData.ChannelSet, syncData.ChannelSetHistory...) {
-		if docHistoryEntry.Name != chanName {
+		if docHistoryEntry.Name != chanName && "*" != chanName {
 			continue
 		}
 
