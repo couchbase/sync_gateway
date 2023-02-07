012d35b58a11f5f4065e658b91e33bac4ddd7f6f
Gregory Newman-Smith <109068393+gregns1@users.noreply.github.com>
2023-02-07 11:25:33 +0000

    CBG-2600: [2.8.4 Backport] ISGR processedSeqs/expectedSeq mismatch based on JSON string (#6036)Co-authored-by: Ben Brooks <ben.brooks@couchbase.com> Co-authored-by: Adam Fraser <adam.fraser@couchbase.com>
    * CBG-2600: [2.8.4 Backport] Make ISGR checkpointer callbacks deal with pre-parsed SequenceID (#5957)
    
    * Add TestActiveReplicatorPullSkippedSequence
    
    * Update test and assert on sequence set lengths
    
    * Use parsed sequence IDs in ISGR checkpointer for efficiency gain during checkpointing, and avoid expected/processed formatting mismatches
    
    * Track pre/post cleanup sequence list lengths
    
    * improve test comments
    
    * better error handling for unparsable sequences
    
    * fix TestCheckpointerSafeSeq
    
    * Make SequenceID.Before() also take LowSeq into account
    
    * Don't add to expectedSeqs for unparsable sequence
    
    * Address PR comments
    
    * standardise error handling for ParseJSONSequenceID by not including affected sequences in checkpointing
    
    * updates based off test failiure
    
    * Backport jenkinsfile updates to 2.8.3 (#5585)
    
    * Backport jenkinsfile fixes to 2.8.3
    
    * Remove 3.0-specific config
    
    * jenkins file problem
    
    * jenkins file fix
    
    * rebase
    
    * update jenkins file to mirror other PR
    
    * update
    
    * update spacing
    
    * updates my goland missed
    
    * remove update to jenkins file
    
    * remove whitespace
    
    ---------
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>
    Co-authored-by: Adam Fraser <adam.fraser@couchbase.com>

--- db/blip_sync_messages.go
+++ db/blip_sync_messages.go
@@ -111,9 +111,9 @@ func NewSubChangesParams(logCtx context.Context, rq *blip.Message, zeroSeq Seque
 
 	// Determine incoming since and docIDs once, since there is some overhead associated with their calculation
 	sinceSequenceId := zeroSeq
+	var err error
 	if sinceStr, found := rq.Properties[SubChangesSince]; found {
-		var err error
-		if sinceSequenceId, err = sequenceIDParser(base.ConvertJSONString(sinceStr)); err != nil {
+		if sinceSequenceId, err = sequenceIDParser(sinceStr); err != nil {
 			base.InfofCtx(logCtx, base.KeySync, "%s: Invalid sequence ID in 'since': %s", rq, sinceStr)
 			return params, err
 		}
--- rest/changes_api.go
+++ rest/changes_api.go
@@ -84,7 +84,7 @@ func (h *handler) updateChangesOptionsFromQuery(feed *string, options *db.Change
 	}
 
 	if _, ok := values["since"]; ok {
-		if options.Since, err = h.db.ParseSequenceID(h.getJSONStringQuery("since")); err != nil {
+		if options.Since, err = db.ParsePlainSequenceID(h.getJSONStringQuery("since")); err != nil {
 			return nil, nil, err
 		}
 	}
@@ -126,8 +126,8 @@ func (h *handler) updateChangesOptionsFromQuery(feed *string, options *db.Change
 					docIdsArray = querydocidKeys
 				}
 			} else {
-				//This is not a JSON array so treat as a simple
-				//comma separated list of doc id's
+				// This is not a JSON array so treat as a simple
+				// comma separated list of doc id's
 				docIdsArray = strings.Split(docidsParam, ",")
 			}
 		}
@@ -172,7 +172,7 @@ func (h *handler) handleChanges() error {
 		// GET request has parameters in URL:
 		feed = h.getQuery("feed")
 		var err error
-		if options.Since, err = h.db.ParseSequenceID(h.getJSONStringQuery("since")); err != nil {
+		if options.Since, err = db.ParsePlainSequenceID(h.getJSONStringQuery("since")); err != nil {
 			return err
 		}
 		options.Limit = int(h.getIntQuery("limit", 0))
@@ -194,8 +194,8 @@ func (h *handler) handleChanges() error {
 					docIdsArray = docidKeys
 				}
 			} else {
-				//This is not a JSON array so treat as a simple
-				//comma separated list of doc id's
+				// This is not a JSON array so treat as a simple
+				// comma separated list of doc id's
 				docIdsArray = strings.Split(docidsParam, ",")
 			}
 		}
@@ -507,9 +507,9 @@ func (h *handler) sendContinuousChangesByWebSocket(inChannels base.Set, options 
 			}
 		}
 
-		//Copy options.Terminator to new WebSocket options
-		//options.Terminator will be closed automatically when
-		//changes feed completes
+		// Copy options.Terminator to new WebSocket options
+		// options.Terminator will be closed automatically when
+		// changes feed completes
 		wsoptions.Terminator = options.Terminator
 
 		// Set up GZip compression
