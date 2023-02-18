0499145259ef7e60e1056fb0e61ca693f96f0a5f
Jens Alfke <jens@couchbase.com>
2023-02-17 17:09:20 -0800

    Added license header to files

--- db/functions/engine/polyfill/url.js
+++ db/functions/engine/polyfill/url.js
@@ -1,3 +1,13 @@
+/*
+Copyright 2022-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 // Apollo's library imports the URL class from Node's `url` package.
 // Unfortunately there isn't a polyfill library that works exactly like this.
 // The `url-parse` comes close, but the class is its default export, not a name.
--- db/functions/engine/src/api.ts
+++ db/functions/engine/src/api.ts
@@ -1,3 +1,13 @@
+/*
+Copyright 2022-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 import { Args, User, Config, Database, Context, Credentials, Document, JSONObject } from './types'
 import { ClearCallDepth, MakeDatabase, Upstream } from "./impl";
 
