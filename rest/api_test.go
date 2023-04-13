e0520bcbe86c5d12a35dba8a77bc73f15e09fa6b
Tor Colvin <tor.colvin@couchbase.com>
2023-04-13 16:49:06 -0400

    CBG-2842 Always return unauthorized for non authorized public API (#6183)
    * CBG-2842 Always return unauthorized for non authicated admin
    
    * Check for unauthorized
    
    * restore 403 in non existent db
    
    * cleanup test cases
    
    * Switch all values to use a constant
    
    * change to invalid login
    
    * Make sure the code compiles
    
    * add assertion
    
    * correct usage for login required vs invalid login
    
    * Throw an error for password auth disabled
    
    * Ensure public auth basic disabled throws identical messages
    
    * Fix spelling

--- rest/api_test.go
+++ rest/api_test.go
@@ -104,6 +104,12 @@ func TestDisablePublicBasicAuth(t *testing.T) {
 
 	response := rt.SendRequest(http.MethodGet, "/{{.db}}/", "")
 	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrLoginRequired.Message)
+	assert.NotContains(t, response.Header(), "WWW-Authenticate", "expected to not receive a WWW-Auth header when password auth is disabled")
+
+	response = rt.SendRequest(http.MethodGet, "/notadb/", "")
+	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrLoginRequired.Message)
 	assert.NotContains(t, response.Header(), "WWW-Authenticate", "expected to not receive a WWW-Auth header when password auth is disabled")
 
 	// Double-check that even if we provide valid credentials we still won't be let in
@@ -114,11 +120,22 @@ func TestDisablePublicBasicAuth(t *testing.T) {
 
 	response = rt.SendUserRequest(http.MethodGet, "/{{.db}}/", "", "user1")
 	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrInvalidLogin.Message)
+	assert.NotContains(t, response.Header(), "WWW-Authenticate", "expected to not receive a WWW-Auth header when password auth is disabled")
+
+	response = rt.SendUserRequest(http.MethodGet, "/notadb/", "", "user1")
+	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrInvalidLogin.Message)
 	assert.NotContains(t, response.Header(), "WWW-Authenticate", "expected to not receive a WWW-Auth header when password auth is disabled")
 
 	// Also check that we can't create a session through POST /db/_session
 	response = rt.SendRequest(http.MethodPost, "/{{.db}}/_session", `{"name":"user1","password":"letmein"}`)
 	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrLoginRequired.Message)
+
+	response = rt.SendRequest(http.MethodPost, "/notadb/_session", `{"name":"user1","password":"letmein"}`)
+	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrLoginRequired.Message)
 
 	// As a sanity check, ensure it does work when the setting is disabled
 	rt.ServerContext().Database(ctx, "db").Options.DisablePasswordAuthentication = false
