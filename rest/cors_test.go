5ac4b9762064fbed6b51581caa72999bc13c9299
Tor Colvin <tor.colvin@couchbase.com>
2023-04-13 12:38:31 -0400

    add new test

---
+++ rest/cors_test.go
@@ -0,0 +1,78 @@
+package rest
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/couchbase/sync_gateway/auth"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCORSDynamicSet(t *testing.T) {
+	rt := NewRestTester(t, &RestTesterConfig{
+		PersistentConfig: true,
+	})
+	defer rt.Close()
+
+	// CORS is set to http://example.com by RestTester ServerContext
+	dbName := "corsdb"
+	dbConfig := rt.NewDbConfig()
+
+	resp := rt.CreateDatabase(dbName, dbConfig)
+	RequireStatus(t, resp, http.StatusCreated)
+
+	const username = "alice"
+	rt.CreateUser(username, nil)
+
+	invalidDatabaseName := "invalid database name"
+	reqHeaders := map[string]string{
+		"Origin": "http://example.com",
+	}
+	response := rt.SendRequestWithHeaders("GET", "/{{.keyspace}}/", "", reqHeaders)
+	require.Equal(t, "http://example.com", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusBadRequest)
+	require.Contains(t, response.Body.String(), invalidDatabaseName)
+
+	// successful request
+	response = rt.SendUserRequestWithHeaders("GET", "/{{.keyspace}}/_all_docs", "", reqHeaders, username, RestTesterDefaultUserPassword)
+	require.Equal(t, "http://example.com", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusOK)
+
+	response = rt.SendRequestWithHeaders("GET", "/{{.db}}/", "", reqHeaders)
+	require.Equal(t, "http://example.com", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrLoginRequired.Message)
+
+	response = rt.SendUserRequestWithHeaders("GET", "/{{.db}}/", "", reqHeaders, username, RestTesterDefaultUserPassword)
+	require.Equal(t, "http://example.com", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusOK)
+
+	dbConfig = rt.NewDbConfig()
+	dbConfig.CORS = &auth.CORSConfig{
+		Origin: []string{"http://example.org"},
+	}
+
+	resp = rt.ReplaceDbConfig(dbName, dbConfig)
+	RequireStatus(t, resp, http.StatusCreated)
+
+	// this falls back to the server config CORS without the user being authenticated
+	response = rt.SendRequestWithHeaders("GET", "/{{.keyspace}}/", "", reqHeaders)
+	require.Equal(t, "http://example.com", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusBadRequest)
+	require.Contains(t, response.Body.String(), invalidDatabaseName)
+
+	// successful request
+	response = rt.SendUserRequestWithHeaders("GET", "/{{.keyspace}}/_all_docs", "", reqHeaders, username, RestTesterDefaultUserPassword)
+	require.Equal(t, "", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusOK)
+
+	response = rt.SendRequestWithHeaders("GET", "/{{.db}}/", "", reqHeaders)
+	require.Equal(t, "", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusUnauthorized)
+	require.Contains(t, response.Body.String(), ErrLoginRequired.Message)
+
+	response = rt.SendUserRequestWithHeaders("GET", "/{{.db}}/", "", reqHeaders, username, RestTesterDefaultUserPassword)
+	require.Equal(t, "", response.Header().Get("Access-Control-Allow-Origin"))
+	RequireStatus(t, response, http.StatusOK)
+
+}
