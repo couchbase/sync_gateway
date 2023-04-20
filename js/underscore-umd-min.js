bb41639c0004b3e93ea7be98ab263ec3474b4760
Tor Colvin <tor.colvin@couchbase.com>
2023-04-20 09:23:23 -0400

    Add licenses

--- js/underscore-umd-min.js
+++ js/underscore-umd-min.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2023-Present Couchbase, Inc.
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ * in that file, in accordance with the Business Source License, use of this
+ * software will be governed by the Apache License, Version 2.0, included in
+ * the file licenses/APL2.txt.
+ */
+
 !function(n,r){"object"==typeof exports&&"undefined"!=typeof module?module.exports=r():"function"==typeof define&&define.amd?define("underscore",r):(n="undefined"!=typeof globalThis?globalThis:n||self,function(){var t=n._,e=n._=r();e.noConflict=function(){return n._=t,e}}())}(this,(function(){
 //     Underscore.js 1.13.6
 //     https://underscorejs.org
--- js/underscore-umd.js
+++ js/underscore-umd.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2023-Present Couchbase, Inc.
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ * in that file, in accordance with the Business Source License, use of this
+ * software will be governed by the Apache License, Version 2.0, included in
+ * the file licenses/APL2.txt.
+ */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define('underscore', factory) :
