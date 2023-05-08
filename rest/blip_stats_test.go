205bab765f3f6b7ad54207b7cdbe0fb1f43fe367
Tor Colvin <tor.colvin@couchbase.com>
2023-05-08 18:09:15 -0400

    Add test to make sure stats are incremented on BlipSyncContext.Close

--- rest/blip_stats_test.go
+++ rest/blip_stats_test.go
@@ -11,17 +11,59 @@ package rest
 import (
 	"testing"
 
+	"github.com/couchbase/sync_gateway/base"
 	"github.com/stretchr/testify/require"
 )
 
-func TestBlipStatsNoData(t *testing.T) {
+func sendGetCheckpointRequest(bt *BlipTester) {
+	t := bt.restTester.TB
+	rq := bt.newRequest()
+	rq.SetProfile("getCheckpoint")
+	require.True(t, bt.sender.Send(rq))
+	errorCode, exists := rq.Response().Properties["Error-Code"]
+	require.True(t, exists)
+	require.Equal(t, "404", errorCode)
+}
+
+// waitForStatGreaterThan will retry for up to 20 seconds until the result of getStatFunc is equal to the expected value.
+func waitForStatGreaterThan(t *testing.T, getStatFunc func() int64, expected int64) {
+	workerFunc := func() (shouldRetry bool, err error, val interface{}) {
+		val = getStatFunc()
+		stat, ok := val.(int64)
+		require.True(t, ok)
+		return stat <= expected, nil, val
+	}
+	// wait for up to 20 seconds for the stat to meet the expected value
+	err, val := base.RetryLoop("waitForStatGreaterThan retry loop", workerFunc, base.CreateSleeperFunc(200, 100))
+	require.NoError(t, err)
+	valInt64, ok := val.(int64)
+	require.True(t, ok)
+	require.Greater(t, valInt64, expected)
+}
+
+func TestBlipStatsBasic(t *testing.T) {
 	bt, err := NewBlipTester(t)
 	require.NoError(t, err)
 	defer bt.Close()
 
+	// make sure requests have not incremented stats.
+	/// Note: there is a blip call in NewBlipTester to initialize collections
 	dbStats := bt.restTester.GetDatabase().DbStats.Database()
 	require.Equal(t, int64(0), dbStats.BlipBytesReceived.Value())
 	require.Equal(t, int64(0), dbStats.BlipBytesSent.Value())
+
+	// send a request, close BlipSyncContext and make sure stats are incremented
+	sendGetCheckpointRequest(bt)
+
+	// requests shouldn't be implemented as part of handler
+	require.Equal(t, int64(0), dbStats.BlipBytesReceived.Value())
+	require.Equal(t, int64(0), dbStats.BlipBytesSent.Value())
+
+	bt.sender.Close()
+
+	waitForStatGreaterThan(t, dbStats.BlipBytesReceived.Value, 1)
+	waitForStatGreaterThan(t, dbStats.BlipBytesSent.Value, 1)
+
 }
 
 func TestBlipStatsFastReport(t *testing.T) {
