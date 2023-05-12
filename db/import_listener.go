daa56c3c10cf078324fe98d4402b9fb49b3140c9
Ben Brooks <ben.brooks@couchbase.com>
2023-05-12 21:20:30 +0100

    CBG-2938: Ignore Cbgt EOF feed errors when intentionally stopped (#6235)
    * Use sgMgrEventHandlers context to add cancellation handling for EOF feed errors
    
    * Move all importListener.Stop cbgtContext code into CbgtContext.Stop()

--- db/import_listener.go
+++ db/import_listener.go
@@ -214,19 +214,7 @@ func (il *importListener) ImportFeedEvent(event sgbucket.FeedEvent) {
 func (il *importListener) Stop() {
 	if il != nil {
 		if il.cbgtContext != nil {
-			il.cbgtContext.StopHeartbeatListener()
-
-			// Close open PIndexes before stopping the manager.
-			_, pindexes := il.cbgtContext.Manager.CurrentMaps()
-			for _, pIndex := range pindexes {
-				err := il.cbgtContext.Manager.ClosePIndex(pIndex)
-				if err != nil {
-					base.DebugfCtx(il.loggingCtx, base.KeyImport, "Error closing pindex: %v", err)
-				}
-			}
-			// ClosePIndex calls are synchronous, so can stop manager once they've completed
-			il.cbgtContext.Manager.Stop()
-			il.cbgtContext.RemoveFeedCredentials(il.dbName)
+			il.cbgtContext.Stop()
 
 			// Remove entry from global listener directory
 			base.RemoveDestFactory(il.importDestKey)
