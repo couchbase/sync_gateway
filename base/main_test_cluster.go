1188ab2074c48e96285ea6e0c050217565c5431e
Ben Brooks <ben@bbrks.me>
2023-06-13 17:12:01 +0100

    Do CE/EE check in supportsMobileRBAC

--- base/main_test_cluster.go
+++ base/main_test_cluster.go
@@ -200,6 +200,15 @@ func (c *tbpClusterV2) supportsCollections() (bool, error) {
 
 // supportsMobileRBAC is true if running couchbase server with all Sync Gateway roles
 func (c *tbpClusterV2) supportsMobileRBAC() (bool, error) {
+	isEE, err := c.isServerEnterprise()
+	if err != nil {
+		return false, err
+	}
+	// mobile RBAC is only supported on EE
+	if !isEE {
+		return false, nil
+	}
+	// mobile RBAC is only supported on 7.1+
 	major, minor, err := getClusterVersion(c.cluster)
 	if err != nil {
 		return false, err
