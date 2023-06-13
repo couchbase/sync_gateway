7f4c63f46c69191b4e41fccc1d8998a857487d6b
Ben Brooks <ben@bbrks.me>
2023-06-13 13:56:14 +0100

    Add keyspace-scoped request to TestOpenIDConnectImplicitFlow

--- rest/oidc_api_test.go
+++ rest/oidc_api_test.go
@@ -1080,6 +1080,11 @@ func TestOpenIDConnectImplicitFlow(t *testing.T) {
 				return
 			}
 			checkGoodAuthResponse(t, restTester, response, "foo_noah")
+
+			c := getCookie(response.Cookies(), auth.DefaultCookieName)
+
+			resp := restTester.SendRequestWithHeaders(http.MethodPut, "/{{.keyspace}}/doc1", `{"foo":"bar"}`, map[string]string{"Cookie": c.String()})
+			RequireStatus(t, resp, http.StatusCreated)
 		})
 	}
 }
