d1898fd1d26b986da1f8534e27698e8df768814a
Mohammed Madi <66013188+mohammed-madi@users.noreply.github.com>
2023-08-23 18:06:36 +0100

    CBG-3273: Fix revocation feed loop (#6378)
    * Add TestRevocationsWithQueryLimit2Channels and fix loop
    
    * Move lastSeq assignment before inner loop

--- db/changes.go
+++ db/changes.go
@@ -252,6 +252,7 @@ func (db *DatabaseCollectionWithUser) buildRevokedFeed(ctx context.Context, ch c
 				// Otherwise: we need to determine whether a previous revision of the document was in the channel prior
 				// to the since value, and only send a revocation if that was the case
 
+				lastSeq = logEntry.Sequence
 				if logEntry.Sequence > sinceVal {
 					// Get doc sync data so we can verify the docs grant history
 					syncData, err := db.GetDocSyncData(ctx, logEntry.DocID)
@@ -290,7 +291,6 @@ func (db *DatabaseCollectionWithUser) buildRevokedFeed(ctx context.Context, ch c
 				}
 
 				change := makeRevocationChangeEntry(logEntry, seqID, singleChannelCache.ChannelID())
-				lastSeq = logEntry.Sequence
 
 				base.DebugfCtx(ctx, base.KeyChanges, "Channel feed processing revocation seq: %v in channel %s ", seqID, base.UD(singleChannelCache.ChannelID().Name))
 
