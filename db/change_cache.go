0be75c111cbc9e5e82b2e5150ea30ad6873d35f1
Mohammed Madi <66013188+mohammed-madi@users.noreply.github.com>
2023-08-24 17:07:58 +0100

    CBG-3171: Fix rev cache multi node inconsistency (#6369)
    Co-authored-by: adamcfraser <adam.fraser@couchbase.com>

--- db/change_cache.go
+++ db/change_cache.go
@@ -549,6 +549,9 @@ func (c *changeCache) DocChanged(event sgbucket.FeedEvent) {
 	}
 
 	// Now add the entry for the new doc revision:
+	if len(rawUserXattr) > 0 {
+		c.context.revisionCache.Invalidate(docID, syncData.CurrentRev)
+	}
 	change := &LogEntry{
 		Sequence:     syncData.Sequence,
 		DocID:        docID,
