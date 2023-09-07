b26200345b55c40eb60d07bfe164f24ad3be7ac8
Ben Brooks <ben.brooks@couchbase.com>
2023-09-07 17:29:41 +0100

    Missed parameter from other cherry-pick

--- rest/server_context.go
+++ rest/server_context.go
@@ -711,7 +711,7 @@ func (sc *ServerContext) _getOrAddDatabaseFromConfig(ctx context.Context, config
 		return nil, err
 	}
 
-	ctx = base.DatabaseLogCtx(ctx, dbName)
+	ctx = base.DatabaseLogCtx(ctx, dbName, contextOptions.LoggingConfig.Console)
 
 	contextOptions.UseViews = useViews
 
