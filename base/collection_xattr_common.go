ea96fbb93d173851b0c4c63853bfaca42b706551
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2023-09-26 17:28:55 +0100

    CBG-3448: chnaged needed in sync gateway to pick up change to WriteUpdateWithXattrFunc in sgbucket, will nedd to pick up sgbucket and rosmar chnages on go mod once they are merged

--- base/collection_xattr_common.go
+++ base/collection_xattr_common.go
@@ -201,7 +201,7 @@ func WriteUpdateWithXattr(ctx context.Context, store *Collection, k string, xatt
 		}
 
 		// Invoke callback to get updated value
-		updatedValue, updatedXattrValue, isDelete, callbackExpiry, err := callback(value, xattrValue, userXattrValue, cas)
+		updatedValue, updatedXattrValue, isDelete, callbackExpiry, macroOpts, err := callback(value, xattrValue, userXattrValue, cas)
 
 		// If it's an ErrCasFailureShouldRetry, then retry by going back through the for loop
 		if err == ErrCasFailureShouldRetry {
@@ -214,6 +214,9 @@ func WriteUpdateWithXattr(ctx context.Context, store *Collection, k string, xatt
 		}
 		if callbackExpiry != nil {
 			exp = *callbackExpiry
+		}
+		if len(macroOpts) != 0 {
+			opts.MacroExpansion = append(opts.MacroExpansion, macroOpts...)
 		}
 
 		// Attempt to write the updated document to the bucket.  Mark body for deletion if previous body was non-empty
