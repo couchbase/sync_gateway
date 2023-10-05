29354a08d3aae5e008457a2e98e9494e7a3eb9f1
Ben Brooks <ben.brooks@couchbase.com>
2023-10-05 18:43:15 +0100

    [3.1.2 Backport] CBG-3509: Add unsupported.allow_dbconfig_env_vars
    * Add unsupported.disallow_dbconfig_env_vars to opt out of db config env var expansion
    
    * Add test for DisallowDbConfigEnvVars coverage
    
    * Flip bool (default to true)

--- rest/utilities_testing.go
+++ rest/utilities_testing.go
@@ -217,6 +217,7 @@ func (rt *RestTester) Bucket() base.Bucket {
 	sc.Bootstrap.UseTLSServer = &rt.RestTesterConfig.useTLSServer
 	sc.Bootstrap.ServerTLSSkipVerify = base.BoolPtr(base.TestTLSSkipVerify())
 	sc.Unsupported.Serverless.Enabled = &rt.serverless
+	sc.Unsupported.AllowDbConfigEnvVars = rt.RestTesterConfig.allowDbConfigEnvVars
 	if rt.serverless {
 		if !rt.PersistentConfig {
 			rt.TB.Fatalf("Persistent config must be used when running in serverless mode")
