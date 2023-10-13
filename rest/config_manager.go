8e094a2c133090090e8b08d5861af7ce35f4539c
adamcfraser <adam.fraser@couchbase.com>
2023-10-13 15:20:57 -0700

    CBG-3462 [3.1.2 backport] Fix handling for duplicate legacy and non-legacy database configs
    Backports CBG-3461 to 3.1.2.

--- rest/config_manager.go
+++ rest/config_manager.go
@@ -366,7 +366,7 @@ func (b *bootstrapContext) GetDatabaseConfigs(ctx context.Context, bucketName, g
 			}
 			dbConfigs = append(dbConfigs, dbConfig)
 			if dbConfig.Name == legacyDbName {
-				legacyFoundInRegistry = false
+				legacyFoundInRegistry = true
 			}
 		}
 
