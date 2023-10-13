70b1825113980327fa4c623cf359b5d81a9344f2
Tor Colvin <tor.colvin@couchbase.com>
2023-10-13 14:39:32 -0400

    CBG-3518 flush the log buffers collateBuffer is enabled (#6523)
    * CBG-3518 flush the log buffers collateBuffer is enabled
    * Remove warning count assertion since warnings can come through from DCP mutations from previous tests
    * Update the assertion to use testify contains

--- rest/changestest/changes_api_test.go
+++ rest/changestest/changes_api_test.go
@@ -4333,7 +4333,6 @@ func TestDocChangedLogging(t *testing.T) {
 	rest.RequireStatus(t, response, http.StatusCreated)
 	require.NoError(t, rt.WaitForPendingChanges())
 
-	warnCountBefore := base.SyncGatewayStats.GlobalStats.ResourceUtilizationStats().WarnCount.Value()
 	base.AssertLogContains(t, "Ignoring non-metadata mutation for doc", func() {
 		err := rt.GetDatabase().MetadataStore.Set("doc1", 0, nil, db.Body{"foo": "bar"})
 		require.NoError(t, err)
@@ -4343,5 +4342,4 @@ func TestDocChangedLogging(t *testing.T) {
 		rest.RequireStatus(t, response, http.StatusCreated)
 		require.NoError(t, rt.WaitForPendingChanges())
 	})
-	assert.Equal(t, warnCountBefore, base.SyncGatewayStats.GlobalStats.ResourceUtilizationStats().WarnCount.Value())
 }
