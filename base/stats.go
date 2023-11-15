2c91620eb5a61feeb4663d150d5df06c592e5bc6
Ben Brooks <ben.brooks@couchbase.com>
2023-11-15 13:49:13 +0000

    4.0: Bump SG API version (#6578)

--- base/stats.go
+++ base/stats.go
@@ -88,6 +88,7 @@ const (
 	StatAddedVersion3dot2dot0     = "3.2.0"
 	StatAddedVersion3dot2dot1     = "3.2.1"
 	StatAddedVersion3dot3dot0     = "3.3.0"
+	StatAddedVersion4dot0dot0     = "4.0.0"
 
 	StatDeprecatedVersionNotDeprecated = ""
 	StatDeprecatedVersion3dot2dot0     = "3.2.0"
