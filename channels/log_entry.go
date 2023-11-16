45b1d3f9e76dbc045cec23064313cba56a79b052
Gregory Newman-Smith <109068393+gregns1@users.noreply.github.com>
2023-11-16 22:19:42 +0000

    CBG-3355: Add current version to channel cache (#6571)
    * CBG-3255: Add current version to log entry for population on the channel cache. Pre-requisite for my work on adding CV to change entries. Only adds CV to log entry from docs seen over DCP at this time pending work on channel cache backfill
    
    * add comments and protect against panic in channel cache population
    
    * add more commnets
    
    * updated to move test and few lines populating log entry

--- channels/log_entry.go
+++ channels/log_entry.go
@@ -54,6 +54,8 @@ type LogEntry struct {
 	PrevSequence uint64       // Sequence of previous active revision
 	IsPrincipal  bool         // Whether the log-entry is a tracking entry for a principal doc
 	CollectionID uint32       // Collection ID
+	SourceID     string       // SourceID allocated to the doc's Current Version on the HLV
+	Version      uint64       // Version allocated to the doc's Current Version on the HLV
 }
 
 func (l LogEntry) String() string {
