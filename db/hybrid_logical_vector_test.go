0d28f5c38d2eeff571f5c3184fea920b797c1ee7
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2023-12-11 13:10:16 +0000

    lint error

--- db/hybrid_logical_vector_test.go
+++ db/hybrid_logical_vector_test.go
@@ -502,7 +502,8 @@ func BenchmarkExtractHLVFromBlipMessage(b *testing.B) {
 	for _, bm := range extractHLVFromBlipMsgBMarkCases {
 		b.Run(bm.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				extractHLVFromBlipMessage(bm.hlvString)
+				_, err := extractHLVFromBlipMessage(bm.hlvString)
+				require.NoError(b, err)
 			}
 		})
 	}
