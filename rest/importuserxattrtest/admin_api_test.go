590de9e10be1732f5d631c979be8b122cfb8da93
Ben Brooks <ben.brooks@couchbase.com>
2024-01-03 12:42:38 +0000

    Add new TestUserXattrSetUnsetDBConfig test for empty string POST/Upsert and nil on PUT

---
+++ rest/importuserxattrtest/admin_api_test.go
@@ -0,0 +1,50 @@
+package importuserxattrtest
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/couchbase/sync_gateway/base"
+	"github.com/couchbase/sync_gateway/rest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserXattrSetUnsetDBConfig(t *testing.T) {
+	if !base.TestUseXattrs() {
+		t.Skip("This test only works with XATTRS enabled")
+	}
+
+	if !base.IsEnterpriseEdition() {
+		t.Skipf("test is EE only - user xattrs")
+	}
+
+	rt := rest.NewRestTesterDefaultCollection(t, &rest.RestTesterConfig{
+		DatabaseConfig: &rest.DatabaseConfig{DbConfig: rest.DbConfig{
+			UserXattrKey: base.StringPtr("myXattr"),
+		}},
+	})
+	defer rt.Close()
+
+	resp := rt.SendAdminRequest(http.MethodGet, "/db/_config", "")
+	rest.AssertStatus(t, resp, http.StatusOK)
+	assert.Contains(t, string(resp.BodyBytes()), `"user_xattr_key":"myXattr"`)
+
+	// upsert an explicit empty string (instead of nil) to ensure we can remove the config option
+	resp = rt.UpsertDbConfig(rt.GetDatabase().Name, rest.DbConfig{UserXattrKey: base.StringPtr("")})
+	rest.AssertStatus(t, resp, http.StatusCreated)
+
+	// empty string for the config option will be treated like nil in use
+	resp = rt.SendAdminRequest(http.MethodGet, "/db/_config", "")
+	rest.AssertStatus(t, resp, http.StatusOK)
+	assert.Contains(t, string(resp.BodyBytes()), `"user_xattr_key":""`)
+
+	// PUT to fully remove the config option (nil)
+	dbConfig := rt.DatabaseConfig
+	dbConfig.UserXattrKey = nil
+	resp = rt.ReplaceDbConfig(rt.GetDatabase().Name, dbConfig.DbConfig)
+	rest.AssertStatus(t, resp, http.StatusCreated)
+
+	resp = rt.SendAdminRequest(http.MethodGet, "/db/_config", "")
+	rest.AssertStatus(t, resp, http.StatusOK)
+	assert.NotContains(t, string(resp.BodyBytes()), `"user_xattr_key"`)
+}
