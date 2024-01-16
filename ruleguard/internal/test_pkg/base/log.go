759955db745637d0abf06c153f94311d2bbb7431
Ben Brooks <ben.brooks@couchbase.com>
2024-01-16 10:59:50 +0000

    nolint test files

--- ruleguard/internal/test_pkg/base/log.go
+++ ruleguard/internal/test_pkg/base/log.go
@@ -6,11 +6,13 @@ import (
 	"log"
 )
 
-func PanicfCtx(ctx context.Context, format string, args ...interface{}) {
+//nolint:goprintffuncname
+func PanicfCtx(_ context.Context, format string, args ...interface{}) {
 	log.Printf("[ERR] "+format, args...)
 }
 
-func InfofCtx(ctx context.Context, logkey, format string, args ...interface{}) {
+//nolint:goprintffuncname
+func InfofCtx(_ context.Context, logkey, format string, args ...interface{}) {
 	format = fmt.Sprintf("[INF] %s: %s", logkey, format)
 	log.Printf(format, args...)
 }
