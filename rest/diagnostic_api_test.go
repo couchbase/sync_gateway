6ffe41027e5e61044299838f7966cf6856777fdc
Mohammed Madi <madim0044@gmail.com>
2024-01-18 17:59:52 +0000

    Fix test 2

--- rest/diagnostic_api_test.go
+++ rest/diagnostic_api_test.go
@@ -26,7 +26,7 @@ func TestGetAllChannelsByUser(t *testing.T) {
 
 	dbConfig := rt.NewDbConfig()
 
-	dbName := "{{.keyspace}}"
+	dbName := "db"
 	rt.CreateDatabase("db", dbConfig)
 
 	// create sessions before users
