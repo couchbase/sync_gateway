352f6b36aaa905aec74a2b9250d47b61145c6ad1
Mohammed Madi <madim0044@gmail.com>
2024-02-22 10:29:08 +0000

    Add handling and test for get all doc channels

---
+++ rest/diagnostic_doc_api.go
@@ -0,0 +1,35 @@
+package rest
+
+import (
+	"github.com/couchbase/sync_gateway/auth"
+	"github.com/couchbase/sync_gateway/db"
+	"log"
+)
+
+// HTTP handler for a GET of a document
+func (h *handler) handleGetDocChannels() error {
+	docid := h.PathVar("docid")
+
+	doc, err := h.collection.GetDocument(h.ctx(), docid, db.DocUnmarshalSync)
+	if err != nil {
+		return err
+	}
+	if doc == nil {
+		return kNotFoundError
+	}
+	resp := make(map[string][]auth.GrantHistorySequencePair, len(doc.Channels))
+
+	for _, chanSetInfo := range doc.SyncData.ChannelSet {
+		resp[chanSetInfo.Name] = append(resp[chanSetInfo.Name], auth.GrantHistorySequencePair{StartSeq: chanSetInfo.Start, EndSeq: chanSetInfo.End})
+		for _, hist := range doc.SyncData.ChannelSetHistory {
+			if hist.Name == chanSetInfo.Name {
+				resp[chanSetInfo.Name] = append(resp[chanSetInfo.Name], auth.GrantHistorySequencePair{StartSeq: hist.Start, EndSeq: hist.End})
+				continue
+			}
+		}
+	}
+	log.Print(resp)
+
+	h.writeJSON(resp)
+	return nil
+}
---
+++ rest/diagnostic_doc_api_test.go
@@ -0,0 +1,32 @@
+package rest
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func TestGetAlldocChannels(t *testing.T) {
+	rt := NewRestTester(t, &RestTesterConfig{SyncFn: `function(doc) {channel(doc.channel);}`})
+	defer rt.Close()
+
+	version := rt.PutDoc("doc", `{"channel":["CHAN1"]}`)
+	updatedVersion := rt.UpdateDoc("doc", version, `{"channel":["CHAN2"]}`)
+	updatedVersion = rt.UpdateDoc("doc", updatedVersion, `{"channel":["CHAN1"]}`)
+	updatedVersion = rt.UpdateDoc("doc", updatedVersion, `{"channel":["CHAN1", "CHAN2"]}`)
+	updatedVersion = rt.UpdateDoc("doc", updatedVersion, `{"channel":["CHAN3"]}`)
+	updatedVersion = rt.UpdateDoc("doc", updatedVersion, `{"channel":["CHAN1"]}`)
+
+	response := rt.SendDiagnosticRequest("GET", "/{{.keyspace}}/doc/_all_channels", "")
+	RequireStatus(t, response, http.StatusOK)
+	log.Printf(response.BodyString())
+	var channelMap map[string][]string
+	err := json.Unmarshal(response.BodyBytes(), &channelMap)
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, channelMap["CHAN1"], []string{"6-0", "1-2", "3-5"})
+	assert.ElementsMatch(t, channelMap["CHAN2"], []string{"4-5", "2-3"})
+	assert.ElementsMatch(t, channelMap["CHAN3"], []string{"5-6"})
+
+}
