4a29c7902779efde60604e225da2ab02a446b5f0
Ben Brooks <ben.brooks@couchbase.com>
2024-06-14 18:37:43 +0100

    addlicense

--- base/devmode_off.go
+++ base/devmode_off.go
@@ -1,6 +1,14 @@
 //go:build !cb_sg_devmode
 // +build !cb_sg_devmode
 
+// Copyright 2024-Present Couchbase, Inc.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+// in that file, in accordance with the Business Source License, use of this
+// software will be governed by the Apache License, Version 2.0, included in
+// the file licenses/APL2.txt.
+
 package base
 
 const cbSGDevModeBuildTagSet = false
