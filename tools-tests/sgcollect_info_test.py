35724dbd567edb7f052a9ba7db92ba581406af85
Tor Colvin <tor.colvin@couchbase.com>
2024-06-21 10:30:32 -0400

    Add missing test

---
+++ tools-tests/sgcollect_info_test.py
@@ -0,0 +1,23 @@
+import sgcollect_info
+import unittest
+import io
+
+
+def test_make_collect_logs_tasks(tmpdir):
+    with unittest.mock.patch(
+        "sgcollect_info.urlopen_with_basic_auth",
+        return_value=io.BytesIO(f'{{"logfilepath": "{tmpdir}"}}'.encode("utf-8")),
+    ):
+        pprof_file = tmpdir.join("pprof_heap_high_01.pb.gz")
+        pprof_file.write("foo")
+        tasks = sgcollect_info.make_collect_logs_tasks(
+            tmpdir,
+            sg_url="fakeurl",
+            sg_config_file_path="",
+            sg_username="",
+            sg_password="",
+            salt="",
+            should_redact=False,
+        )
+        assert len(tasks) == 1
+        assert tasks[0].log_file == pprof_file.basename
