ea4026953a8c474523ed874a5501d2fe1224de4f
Gregory Newman-Smith <109068393+gregns1@users.noreply.github.com>
2024-07-09 10:43:40 +0100

    CBG-4035: [3.1.9 backport] Skipped Sequence Optimisations (#6925)
    * CBG-3852: Implementation of single sequence entry for skipped sequence slice (#6747)
    
    * CBG-3852: implementation of single sequence entry for skipped sequence slice
    
    * minor updates to make setLastSeq no-op for single values
    
    * updates for review round
    
    * remove not needed comment
    
    * updates based off review
    
    * CBG-3853: Implementation of range sequence entry for skipped sequence slice (#6764)
    
    * CBG-3854: Integration of the skipped sequence slice into the cache (#6789)
    
    * CBG-3854: Integeration of the skipped sequence slice into the cache
    
    * remove old chnages not needed anymore + gh actions failures
    
    * updates based of review. Move stat update to updateStats and alter soem naming. Test updates to reflect stat chnages too.
    
    * remove incorrect incrementing of num current sequences inside PushSkippedSequenceEntry
    
    * Fix race condition in updateStats()
    
    * updates off review
    
    * tidy CleanSkippedSequenceQueue
    
    * stats update
    
    * CBG-3855: Sequence range removal support (#6801)
    
    * CBG-3855: support for seqeuence range removal
    
    * add comment to function + remove repeated code
    
    * refactor error handling, simplify stat calculation update
    
    * CBG-3850: Optimise releaseUnusedSequenceRange  (#6831)
    
    * CBG-3850: Optimise releaseUnusedSequenceRange function + support for ranges in pending queue
    
    * updates to add _pushRangeToPending and _removeSubsetOfRangeFromSkipped to process duplicates sequences between unused rnage and pending/skipped lists
    
    * fix race condition in test
    
    * fix for incorrect logic for removing skipped range
    
    * updates based off review
    
    * updates to fix incorrect code + add extra test coverage of changes
    
    * change error name
    
    * CBG-3945: Fix for panic in _clip method on skipped sequence slice (#6842)
    
    * fix test to use old api

--- channels/log_entry.go
+++ channels/log_entry.go
@@ -17,17 +17,6 @@ import (
 	"time"
 )
 
-// LogEntry
-type LogEntryType uint8
-
-const (
-	LogEntryDocument = LogEntryType(iota)
-	LogEntryPrincipal
-	LogEntryCheckpoint
-	LogEntryRollback
-	LogEntryPurge
-)
-
 // Bits in LogEntry.Flags
 const (
 	Deleted  = 1 << iota // This rev is a deletion
@@ -41,30 +30,26 @@ const (
 )
 
 type LogEntry struct {
-	Sequence     uint64       // Sequence number
-	DocID        string       // Document ID
-	RevID        string       // Revision ID
-	Flags        uint8        // Deleted/Removed/Hidden flags
-	VbNo         uint16       // vbucket number
-	TimeSaved    time.Time    // Time doc revision was saved (just used for perf metrics)
-	TimeReceived time.Time    // Time received from tap feed
-	Channels     ChannelMap   // Channels this entry is in or was removed from
-	Skipped      bool         // Late arriving entry
-	Type         LogEntryType // Log entry type
-	Value        []byte       // Snapshot metadata (when Type=LogEntryCheckpoint)
-	PrevSequence uint64       // Sequence of previous active revision
-	IsPrincipal  bool         // Whether the log-entry is a tracking entry for a principal doc
-	CollectionID uint32       // Collection ID
+	Sequence     uint64     // Sequence number
+	EndSequence  uint64     // End sequence on range of sequences that have been released by the sequence allocator (0 if entry is single sequence)
+	DocID        string     // Document ID
+	RevID        string     // Revision ID
+	Flags        uint8      // Deleted/Removed/Hidden flags
+	TimeSaved    time.Time  // Time doc revision was saved (just used for perf metrics)
+	TimeReceived time.Time  // Time received from tap feed
+	Channels     ChannelMap // Channels this entry is in or was removed from
+	Skipped      bool       // Late arriving entry
+	PrevSequence uint64     // Sequence of previous active revision
+	IsPrincipal  bool       // Whether the log-entry is a tracking entry for a principal doc
+	CollectionID uint32     // Collection ID
 }
 
 func (l LogEntry) String() string {
 	return fmt.Sprintf(
-		"seq: %d docid: %s revid: %s vbno: %d type: %v collectionID: %d",
+		"seq: %d docid: %s revid: %s collectionID: %d",
 		l.Sequence,
 		l.DocID,
 		l.RevID,
-		l.VbNo,
-		l.Type,
 		l.CollectionID,
 	)
 }
