9eb5f98f32989eec2e57f24c3cdc9d6439db2724
Tor Colvin <tor.colvin@couchbase.com>
2024-10-17 18:09:30 -0400

    CBG-4254 implement Sync Gateway peer (#7160)

--- topologytest/sync_gateway_peer_test.go
+++ topologytest/sync_gateway_peer_test.go
@@ -10,13 +10,16 @@ package topologytest
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"testing"
+	"time"
 
 	sgbucket "github.com/couchbase/sg-bucket"
 	"github.com/couchbase/sync_gateway/base"
 	"github.com/couchbase/sync_gateway/db"
 	"github.com/couchbase/sync_gateway/rest"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -43,49 +46,96 @@ func (p *SyncGatewayPeer) String() string {
 	return p.name
 }
 
-func (p *SyncGatewayPeer) getCollection(dsName sgbucket.DataStoreName) sgbucket.DataStore {
-	collection, err := p.rt.Bucket().NamedDataStore(dsName)
+// getCollection returns the collection for the given data store name and a related context. The special context is needed to add fields for audit logging, required by build tag cb_sg_devmode.
+func (p *SyncGatewayPeer) getCollection(dsName sgbucket.DataStoreName) (*db.DatabaseCollectionWithUser, context.Context) {
+	dbCtx, err := db.GetDatabase(p.rt.GetDatabase(), nil)
 	require.NoError(p.TB(), err)
-	return collection
+	collection, err := dbCtx.GetDatabaseCollectionWithUser(dsName.ScopeName(), dsName.CollectionName())
+	require.NoError(p.TB(), err)
+	ctx := base.UserLogCtx(collection.AddCollectionContext(p.Context()), "gotest", base.UserDomainBuiltin, nil)
+	return collection, ctx
 }
 
 // GetDocument returns the latest version of a document. The test will fail the document does not exist.
 func (p *SyncGatewayPeer) GetDocument(dsName sgbucket.DataStoreName, docID string) (rest.DocVersion, db.Body) {
-	// this function is not yet collections aware
-	return p.rt.GetDoc(docID)
+	collection, ctx := p.getCollection(dsName)
+	doc, err := collection.GetDocument(ctx, docID, db.DocUnmarshalAll)
+	require.NoError(p.TB(), err)
+	return docVersionFromDocument(doc), doc.Body(ctx)
 }
 
 // CreateDocument creates a document on the peer. The test will fail if the document already exists.
 func (p *SyncGatewayPeer) CreateDocument(dsName sgbucket.DataStoreName, docID string, body []byte) rest.DocVersion {
-	return rest.EmptyDocVersion()
+	return p.WriteDocument(dsName, docID, body)
 }
 
 // WriteDocument writes a document to the peer. The test will fail if the write does not succeed.
 func (p *SyncGatewayPeer) WriteDocument(dsName sgbucket.DataStoreName, docID string, body []byte) rest.DocVersion {
-	// this function is not yet collections aware
-	putVersion := p.rt.PutDoc(docID, string(body))
-	// get the version since PutDoc only has revtree information
-	getVersion, _ := getBodyAndVersion(p, p.getCollection(dsName), docID)
-	// make sure RevTreeID is the same
-	require.Equal(p.TB(), putVersion.RevTreeID, getVersion.RevTreeID)
-	return getVersion
+	collection, ctx := p.getCollection(dsName)
+
+	var doc *db.Document
+	// loop to write document in the case that there is a conflict while writing the document
+	err, _ := base.RetryLoop(ctx, "write document", func() (shouldRetry bool, err error, value any) {
+		var bodyMap db.Body
+		err = base.JSONUnmarshal(body, &bodyMap)
+		require.NoError(p.TB(), err)
+
+		// allow upsert rev
+		existingDoc, err := collection.GetDocument(ctx, docID, db.DocUnmarshalAll)
+		if err == nil {
+			bodyMap[db.BodyRev] = existingDoc.CurrentRev
+		}
+		_, doc, err = collection.Put(ctx, docID, bodyMap)
+		if err != nil {
+			var httpError *base.HTTPError
+			if errors.As(err, &httpError) && httpError.Status == http.StatusConflict {
+				return true, err, nil
+			}
+			require.NoError(p.TB(), err)
+		}
+		return false, nil, nil
+	}, base.CreateSleeperFunc(5, 100))
+	require.NoError(p.TB(), err)
+	return docVersionFromDocument(doc)
 }
 
 // DeleteDocument deletes a document on the peer. The test will fail if the document does not exist.
 func (p *SyncGatewayPeer) DeleteDocument(dsName sgbucket.DataStoreName, docID string) rest.DocVersion {
-	return rest.EmptyDocVersion()
+	collection, ctx := p.getCollection(dsName)
+	doc, err := collection.GetDocument(ctx, docID, db.DocUnmarshalAll)
+	var revID string
+	if err == nil {
+		revID = doc.CurrentRev
+	}
+	_, doc, err = collection.DeleteDoc(ctx, docID, revID)
+	require.NoError(p.TB(), err)
+	return docVersionFromDocument(doc)
 }
 
 // WaitForDocVersion waits for a document to reach a specific version. The test will fail if the document does not reach the expected version in 20s.
 func (p *SyncGatewayPeer) WaitForDocVersion(dsName sgbucket.DataStoreName, docID string, expected rest.DocVersion) db.Body {
-	return waitForDocVersion(p, p.getCollection(dsName), docID, expected)
+	collection, ctx := p.getCollection(dsName)
+	var doc *db.Document
+	require.EventuallyWithT(p.TB(), func(c *assert.CollectT) {
+		var err error
+		doc, err = collection.GetDocument(ctx, docID, db.DocUnmarshalAll)
+		assert.NoError(c, err)
+		docVersion := docVersionFromDocument(doc)
+		// Only assert on CV since RevTreeID might not be present if this was a Couchbase Server write
+		assert.Equal(c, expected.CV, docVersion.CV)
+	}, 5*time.Second, 100*time.Millisecond)
+	return doc.Body(ctx)
 }
 
 // RequireDocNotFound asserts that a document does not exist on the peer.
 func (p *SyncGatewayPeer) RequireDocNotFound(dsName sgbucket.DataStoreName, docID string) {
-	/*_, err := p.rt.GetDoc(docID)
-	base.RequireDocNotFoundError(p.rt.TB(), err)
-	*/
+	collection, ctx := p.getCollection(dsName)
+	doc, err := collection.GetDocument(ctx, docID, db.DocUnmarshalAll)
+	if err == nil {
+		require.True(p.TB(), doc.IsDeleted(), "expected %s to be deleted", doc)
+		return
+	}
+	base.RequireDocNotFoundError(p.TB(), err)
 }
 
 // Close will shut down the peer and close any active replications on the peer.
@@ -94,14 +144,14 @@ func (p *SyncGatewayPeer) Close() {
 }
 
 // CreateReplication creates a replication instance. This is currently not supported for Sync Gateway peers. A future ISGR implementation will support this.
-func (p *SyncGatewayPeer) CreateReplication(peer Peer, config PeerReplicationConfig) PeerReplication {
+func (p *SyncGatewayPeer) CreateReplication(_ Peer, _ PeerReplicationConfig) PeerReplication {
 	require.Fail(p.rt.TB(), "can not create a replication with Sync Gateway as an active peer")
 	return nil
 }
 
 // SourceID returns the source ID for the peer used in <val>@<sourceID>.
-func (r *SyncGatewayPeer) SourceID() string {
-	return r.rt.GetDatabase().EncodedSourceID
+func (p *SyncGatewayPeer) SourceID() string {
+	return p.rt.GetDatabase().EncodedSourceID
 }
 
 // Context returns the context for the peer.
@@ -117,4 +167,16 @@ func (p *SyncGatewayPeer) TB() testing.TB {
 // GetBackingBucket returns the backing bucket for the peer.
 func (p *SyncGatewayPeer) GetBackingBucket() base.Bucket {
 	return p.rt.Bucket()
+}
+
+// docVersionFromDocument sets the DocVersion from the current revision of the document.
+func docVersionFromDocument(doc *db.Document) rest.DocVersion {
+	sourceID, value := doc.HLV.GetCurrentVersion()
+	return rest.DocVersion{
+		RevTreeID: doc.CurrentRev,
+		CV: db.Version{
+			SourceID: sourceID,
+			Value:    value,
+		},
+	}
 }
