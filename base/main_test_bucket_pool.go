3c76396610315eeaf15d35d733676b0bb534736d
Tor Colvin <tor.colvin@couchbase.com>
2024-11-05 11:41:10 -0500

    CBG-4340 fail test harness if dev time assertions hit
    If a panic occurs in a goroutine, like ServeHTTP handler, it might be
    swallowed.

--- base/main_test_bucket_pool.go
+++ base/main_test_bucket_pool.go
@@ -683,6 +683,12 @@ func TestBucketPoolMain(ctx context.Context, m *testing.M, bucketReadierFunc TBP
 	GTestBucketPool = NewTestBucketPoolWithOptions(ctx, bucketReadierFunc, bucketInitFunc, options)
 	teardownFuncs = append(teardownFuncs, func() { GTestBucketPool.Close(ctx) })
 
+	teardownFuncs = append(teardownFuncs, func() {
+		if DevModeAssertionFailures.Load() > 0 {
+			panic("Test harness failed due to failures from -tag cb_sg_devmode. Look at logs for panic statements.")
+			os.Exit(1)
+		}
+	})
 	// must be the last teardown function added to the list to correctly detect leaked goroutines
 	teardownFuncs = append(teardownFuncs, SetUpTestGoroutineDump(m))
 
