b40f315c2bf750f1f8b0f096bbd1b869d2580642
Gregory Newman-Smith <109068393+gregns1@users.noreply.github.com>
2024-11-08 15:42:16 +0000

    CBG-4213: add attachment migration api (#7183)
    * CBG-4213: add attachment migration api
    
    * tidy up
    
    * fix api docs lint error
    
    * Update db/background_mgr_attachment_migration.go
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>
    
    * Update db/background_mgr_attachment_migration.go
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>
    
    ---------
    
    Co-authored-by: Ben Brooks <ben.brooks@couchbase.com>

--- db/background_mgr_attachment_migration.go
+++ db/background_mgr_attachment_migration.go
@@ -66,9 +66,14 @@ func (a *AttachmentMigrationManager) Init(ctx context.Context, options map[strin
 		var statusDoc AttachmentMigrationManagerStatusDoc
 		err := base.JSONUnmarshal(clusterStatus, &statusDoc)
 
+		reset, _ := options["reset"].(bool)
+		if reset {
+			base.InfofCtx(ctx, base.KeyAll, "Attachment Migration: Resetting migration process. Will not resume any partially completed process")
+		}
+
 		// If the previous run completed, or there was an error during unmarshalling the status we will start the
 		// process from scratch with a new migration ID. Otherwise, we should resume with the migration ID, stats specified in the doc.
-		if statusDoc.State == BackgroundProcessStateCompleted || err != nil {
+		if statusDoc.State == BackgroundProcessStateCompleted || err != nil || reset {
 			return newRunInit()
 		}
 		a.MigrationID = statusDoc.MigrationID
