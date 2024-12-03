cd558f48291767cd873bb6aa003ce3724f64d6d2
Tor Colvin <tor.colvin@couchbase.com>
2024-12-03 11:17:06 -0500

    CBG-4366 enable ressurection tests

--- topologytest/hlv_test.go
+++ topologytest/hlv_test.go
@@ -486,7 +486,6 @@ func TestHLVResurrectDocumentSingleActor(t *testing.T) {
 			if strings.HasPrefix(tc.activePeerID, "cbl") {
 				t.Skip("Skipping Couchbase Lite test, does not know how to push a deletion yet CBG-4257")
 			}
-			t.Skip("Skipping resurection tests CBG-4366")
 
 			peers, _ := setupTests(t, tc.topology, tc.activePeerID)
 
