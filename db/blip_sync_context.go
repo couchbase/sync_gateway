f0af11952c1768b030f5ed30ffbce1aa3b44b39d
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2024-12-04 11:17:52 +0000

    CBG-4389: extract cv from known revs according to spec

--- db/blip_sync_context.go
+++ db/blip_sync_context.go
@@ -341,6 +341,8 @@ func (bsc *BlipSyncContext) handleChangesResponse(ctx context.Context, sender *b
 		return err
 	}
 
+	fmt.Println("answers", answer)
+
 	for i, knownRevsArrayInterface := range answer {
 		seq := changeArray[i][0].(SequenceID)
 		docID := changeArray[i][1].(string)
@@ -357,7 +359,16 @@ func (bsc *BlipSyncContext) handleChangesResponse(ctx context.Context, sender *b
 			// The first element of the knownRevsArray returned from CBL is the parent revision to use as deltaSrc
 			if bsc.useDeltas && len(knownRevsArray) > 0 {
 				if revID, ok := knownRevsArray[0].(string); ok {
-					deltaSrcRevID = revID
+					if bsc.useHLV() {
+						msgHLV, err := extractHLVFromBlipMessage(revID)
+						if err != nil {
+							base.ErrorfCtx(ctx, "Invalid known rev format for hlv")
+							return nil
+						}
+						deltaSrcRevID = msgHLV.GetCurrentVersionString()
+					} else {
+						deltaSrcRevID = revID
+					}
 				}
 			}
 
