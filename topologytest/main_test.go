a6c93033bfa7ea3296a88ee5ee338b34181dfa09
Tor Colvin <tor.colvin@couchbase.com>
2024-12-11 11:29:19 -0500

    CBG-4408 disable CBS topologytests by default

--- topologytest/main_test.go
+++ topologytest/main_test.go
@@ -12,6 +12,8 @@ package topologytest
 
 import (
 	"context"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/couchbase/sync_gateway/base"
@@ -20,6 +22,11 @@ import (
 
 func TestMain(m *testing.M) {
 	ctx := context.Background() // start of test process
+	runTests, _ := strconv.ParseBool(os.Getenv(base.TbpEnvTopologyTests))
+	if !base.UnitTestUrlIsWalrus() && !runTests {
+		base.SkipTestMain(m, "Tests are disabled for Couchbase Server by default, to enable set %s=true environment variable", base.TbpEnvTopologyTests)
+		return
+	}
 	tbpOptions := base.TestBucketPoolOptions{MemWatermarkThresholdMB: 2048}
 	// Do not create indexes for this test, so they are built by server_context.go
 	db.TestBucketPoolWithIndexes(ctx, m, tbpOptions)
