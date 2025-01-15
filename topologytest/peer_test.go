616155a262724c8ae7bcd90f192bb65219baafd0
Ben Brooks <ben.brooks@couchbase.com>
2025-01-15 14:12:52 +0000

    Trace logging for ci debugging

--- topologytest/peer_test.go
+++ topologytest/peer_test.go
@@ -326,7 +326,7 @@ func updatePeersT(t *testing.T, peers map[string]Peer) {
 
 // setupTests returns a map of peers and a list of replications. The peers will be closed and the buckets will be destroyed by t.Cleanup.
 func setupTests(t *testing.T, topology Topology) (base.ScopeAndCollectionName, Peers, Replications) {
-	base.SetUpTestLogging(t, base.LevelDebug, base.KeyImport, base.KeyVV, base.KeyCRUD, base.KeySync)
+	base.SetUpTestLogging(t, base.LevelTrace, base.KeyAll)
 	peers := createPeers(t, topology.peers)
 	replications := createPeerReplications(t, peers, topology.replications)
 
