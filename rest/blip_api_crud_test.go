d599ed54365a057b627cee39529fe18a54820b68
Tor Colvin <tor.colvin@couchbase.com>
2025-01-17 16:32:04 -0500

    CBG-4370 create a cancel context inside BlipSyncContext (#7201)
    * CBG-4370 create a cancel context inside BlipSyncContext
    
    This cancel context allows a forceable closure of the underlying blip
    connection.
    
    In the case there is a continuous pull replication and there is an error
    on the changes feed, the only way to stop the pull replication is to
    shut down the connection. CBL clients do not listen to unsolicited error
    messages.
    
    * Avoid refactoring with the change
    
    * pass lint
    
    * test active replicator reconnection
    
    * test fixup
    
    * lint correctly
    
    * PR comments
    
    * Acquire read lock to present data race
    
    * switch query to use all docs index

--- rest/blip_api_crud_test.go
+++ rest/blip_api_crud_test.go
@@ -11,6 +11,7 @@ licenses/APL2.txt.
 package rest
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -1205,7 +1206,7 @@ func TestBlipSendConcurrentRevs(t *testing.T) {
 		concurrentSendRevNum = 50
 	)
 	rt := NewRestTester(t, &RestTesterConfig{
-		leakyBucketConfig: &base.LeakyBucketConfig{
+		LeakyBucketConfig: &base.LeakyBucketConfig{
 			UpdateCallback: func(_ string) {
 				time.Sleep(time.Millisecond * 5) // slow down rosmar - it's too quick to be throttled
 			},
@@ -3195,6 +3196,47 @@ func TestBlipDatabaseClose(t *testing.T) {
 		btcRunner.WaitForVersion(btc.id, markerDoc, markerDocVersion)
 
 		RequireStatus(t, rt.SendAdminRequest(http.MethodDelete, "/{{.db}}/", ""), http.StatusOK)
+
+		require.EventuallyWithT(t, func(c *assert.CollectT) {
+			assert.True(c, blipContextClosed.Load())
+		}, time.Second*10, time.Millisecond*100)
+	})
+}
+
+// Starts a continuous pull replication then updates the db to trigger a close.
+func TestChangesFeedExitDisconnect(t *testing.T) {
+
+	base.SetUpTestLogging(t, base.LevelInfo, base.KeyHTTP, base.KeySync, base.KeySyncMsg, base.KeyChanges, base.KeyCache)
+	btcRunner := NewBlipTesterClientRunner(t)
+	var shouldChannelQueryError atomic.Bool
+	btcRunner.Run(func(t *testing.T, SupportedBLIPProtocols []string) {
+		rt := NewRestTester(t, &RestTesterConfig{
+			LeakyBucketConfig: &base.LeakyBucketConfig{
+				QueryCallback: func(ddoc, viewname string, params map[string]any) error {
+					if viewname == "channels" && shouldChannelQueryError.Load() {
+						return gocb.ErrTimeout
+					}
+					return nil
+				},
+				N1QLQueryCallback: func(_ context.Context, statement string, params map[string]any, consistency base.ConsistencyMode, adhoc bool) error {
+					if strings.Contains(statement, "sg_channels") && shouldChannelQueryError.Load() {
+						return gocb.ErrTimeout
+					}
+					return nil
+				},
+			},
+		})
+		defer rt.Close()
+		const username = "alice"
+		rt.CreateUser(username, []string{"*"})
+		btc := btcRunner.NewBlipTesterClientOptsWithRT(rt, &BlipTesterClientOpts{Username: username})
+		var blipContextClosed atomic.Bool
+		btcRunner.clients[btc.id].pullReplication.bt.blipContext.OnExitCallback = func() {
+			blipContextClosed.Store(true)
+		}
+
+		shouldChannelQueryError.Store(true)
+		btcRunner.StartPull(btc.id)
 
 		require.EventuallyWithT(t, func(c *assert.CollectT) {
 			assert.True(c, blipContextClosed.Load())
