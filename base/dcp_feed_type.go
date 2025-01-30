9738752e2d0c8ca8b66776efdfdcbc28b92fdf38
Tor Colvin <tor.colvin@couchbase.com>
2025-01-30 10:56:48 -0500

    CBG-4505 update cbgt to v1.4.5
    updates CBG-4317 fix for TLS without certs for import feed

--- base/dcp_feed_type.go
+++ base/dcp_feed_type.go
@@ -296,7 +296,7 @@ func getCbgtCredentials(dbName string) (cbgtCreds, bool) {
 	return creds, found
 }
 
-// See the comment of cbgtRootCAsProvider for usage details.
+// setCbgtRootCertsForBucket creates root certificates for a given bucket. If TLS should be used, this function must be called. If tls certificate verification is skipped, then this function should be called with pool as nil. See the comment of cbgtRootCAsProvider for usage details.
 func setCbgtRootCertsForBucket(bucketUUID string, pool *x509.CertPool) {
 	cbgtGlobalsLock.Lock()
 	defer cbgtGlobalsLock.Unlock()
