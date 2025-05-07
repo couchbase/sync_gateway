1d77d185c149ead1ce1f1c4cd3ab0a000deba4b2
Tor Colvin <tor.colvin@couchbase.com>
2025-05-07 16:52:57 -0400

    skip test for rosmar

--- rest/indextest/post_upgrade_test.go
+++ rest/indextest/post_upgrade_test.go
@@ -20,6 +20,9 @@ import (
 )
 
 func TestPostUpgrade(t *testing.T) {
+	if base.UnitTestUrlIsWalrus() {
+		t.Skip("rosmar doesn't support N1QLStore")
+	}
 	base.RequireNumTestDataStores(t, 2)
 
 	ctx := base.TestCtx(t)
