b479e325e8f88722a59397337328a19fbfead49a
Tor Colvin <tor.colvin@couchbase.com>
2025-05-28 10:53:07 -0400

    add typecheck for boolean

--- docs/api/plugins/rules.js
+++ docs/api/plugins/rules.js
@@ -1,10 +1,12 @@
 const RequireTypeWithAdditionalProperties = require("./rules/require-type-with-additionalProperties.js");
+const TypeCheckDefaults = require("./rules/typecheck-defaults.js");
 
 module.exports = {
   rules: {
     oas3: {
       "require-type-with-additionalProperties":
         RequireTypeWithAdditionalProperties,
+      "typecheck-defaults": TypeCheckDefaults,
     },
   },
   id: "custom-rules",
