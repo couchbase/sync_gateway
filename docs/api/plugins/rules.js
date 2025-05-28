dad06a49d58a38a933957f2a4b9de331ef2df271
Tor Colvin <tor.colvin@couchbase.com>
2025-05-28 11:13:18 -0400

    typecheck defaults

--- docs/api/plugins/rules.js
+++ docs/api/plugins/rules.js
@@ -1,11 +1,8 @@
-const RequireTypeWithAdditionalProperties = require("./rules/require-type-with-additionalProperties.js");
 const TypeCheckDefaults = require("./rules/typecheck-defaults.js");
 
 module.exports = {
   rules: {
     oas3: {
-      "require-type-with-additionalProperties":
-        RequireTypeWithAdditionalProperties,
       "typecheck-defaults": TypeCheckDefaults,
     },
   },
--- docs/api/plugins/rules/typecheck-defaults.js
+++ docs/api/plugins/rules/typecheck-defaults.js
@@ -16,6 +16,78 @@ function TypeCheckDefaults() {
                   typeof schema.default,
               });
             }
+            break;
+          case "number":
+            if (
+              schema.default !== undefined &&
+              typeof schema.default !== "number"
+            ) {
+              ctx.report({
+                message:
+                  "Default value for number type must be a number was " +
+                  typeof schema.default,
+              });
+            }
+            break;
+          case "string":
+            if (
+              schema.default !== undefined &&
+              typeof schema.default !== "string"
+            ) {
+              ctx.report({
+                message:
+                  "Default value for string type must be a string was " +
+                  typeof schema.default,
+              });
+            }
+            break;
+          case "integer":
+            if (
+              schema.default !== undefined &&
+              !Number.isInteger(schema.default)
+            ) {
+              ctx.report({
+                message:
+                  "Default value for integer type must be an integer was " +
+                  typeof schema.default,
+              });
+            }
+            break;
+          case "array":
+            if (
+              schema.default !== undefined &&
+              !Array.isArray(schema.default)
+            ) {
+              ctx.report({
+                message:
+                  "Default value for array type must be an array was " +
+                  typeof schema.default,
+              });
+            }
+            break;
+          case "object":
+            if (
+              schema.default !== undefined &&
+              typeof schema.default !== "object"
+            ) {
+              ctx.report({
+                message:
+                  "Default value for object type must be an object was " +
+                  typeof schema.default,
+              });
+            }
+            break;
+          case undefined:
+            if (schema.oneOf === undefined && schema.allOf === undefined) {
+              ctx.report({
+                message: "Schema missing a type definition",
+              });
+            }
+            break;
+          default:
+            ctx.report({
+              message: "Unsupported type in schema " + schema.type,
+            });
         }
       },
     },
