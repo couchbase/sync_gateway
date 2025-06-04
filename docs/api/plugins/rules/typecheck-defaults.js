88c74d9deac631c26e692d87f3e0cb4ece33be3a
Tor Colvin <tor.colvin@couchbase.com>
2025-06-03 21:17:13 -0400

    remove object assertion

--- docs/api/plugins/rules/typecheck-defaults.js
+++ docs/api/plugins/rules/typecheck-defaults.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2025-Present Couchbase, Inc.
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ * in that file, in accordance with the Business Source License, use of this
+ * software will be governed by the Apache License, Version 2.0, included in
+ * the file licenses/APL2.txt.
+ */
+
 module.exports = TypeCheckDefaults;
 
 function checkMinMaxInteger(schema, ctx) {
@@ -108,11 +118,7 @@ function TypeCheckDefaults() {
             }
             break;
           case undefined:
-            if (schema.oneOf === undefined && schema.allOf === undefined) {
-              ctx.report({
-                message: "Schema missing a type definition",
-              });
-            }
+            // assume no type is equivalent to "object" in JSON Schema
             break;
           default:
             ctx.report({
