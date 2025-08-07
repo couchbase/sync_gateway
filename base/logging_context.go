249a376889a8414e0007cd38fa2ac89a54051bf3
Ben Brooks <ben@bbrks.me>
2025-08-07 19:31:00 +0100

    Revert "Test-only tech-debt cleanup: Make `TestCtx` use stdlib `testing.Conte…"
    This reverts commit 6396718d1539745519a1041a2a4b25c3196140a5.

--- base/logging_context.go
+++ base/logging_context.go
@@ -176,7 +176,9 @@ func NewTaskID(contextID string, taskName string) string {
 
 // TestCtx creates a context for the given test which is also cancelled once the test has completed.
 func TestCtx(t testing.TB) context.Context {
-	return LogContextWith(t.Context(), &LogContext{TestName: t.Name()})
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	t.Cleanup(cancelCtx)
+	return LogContextWith(ctx, &LogContext{TestName: t.Name()})
 }
 
 // BucketCtx extends the parent context with a bucket name.
