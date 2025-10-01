7aebd48e222f3ad1a005917ca05eacec3051e710
Eng Zer Jun <engzerjun@gmail.com>
2025-10-02 00:21:39 +0800

    Update github.com/shirou/gopsutil to v4
    v4 is the latest version with bug fixes and enhancements, notably the
    removal of CGO implementations on Darwin in v4.24.9 [1].
    
    Note: Due to the library's versioning policy, v4.24.5 is the first v4
    release [2].
    
    [1]: https://github.com/shirou/gopsutil/releases/tag/v4.24.9
    [2]: https://github.com/shirou/gopsutil/releases/tag/v4.24.5
    
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

--- rest/stats_context.go
+++ rest/stats_context.go
@@ -24,8 +24,8 @@ import (
 
 	"github.com/couchbase/sync_gateway/base"
 	"github.com/elastic/gosigar"
-	gopsutilnet "github.com/shirou/gopsutil/net"
-	"github.com/shirou/gopsutil/v3/cpu"
+	gopsutilnet "github.com/shirou/gopsutil/v4/net"
+	"github.com/shirou/gopsutil/v4/cpu"
 )
 
 // ppofPrefix is the prefix used for the memory profile files that are collected at high memory times.
