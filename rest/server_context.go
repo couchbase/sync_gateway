d3975ba4b6724c1c3788e590db97c6029e4d2e35
Ritesh Sapata <34858159+RIT3shSapata@users.noreply.github.com>
2025-11-11 20:01:52 +0530

    [4.0.3 backport] CBG-4893: Add eccv to cluster_info endpoint
    cherry-pick of bec887002

--- rest/server_context.go
+++ rest/server_context.go
@@ -2334,3 +2334,12 @@ func (sc *ServerContext) getClusterUUID(ctx context.Context) (string, error) {
 	}
 	return base.ParseClusterUUID(output)
 }
+
+func (sc *ServerContext) getBucketCCVSettings() map[string]bool {
+	bucketCCVSettings := make(map[string]bool)
+	for _, _db := range sc._databases {
+		bucketName := _db.BucketSpec.BucketName
+		bucketCCVSettings[bucketName] = _db.CachedCCVEnabled.Load()
+	}
+	return bucketCCVSettings
+}
