52b6745f668cd8b06553cb6c9d6201a0b6456b48
Tor Colvin <tor.colvin@couchbase.com>
2026-01-28 20:39:39 -0500

    CBG-5144 add test for existing DCP checkpoint prefixes

--- db/background_mgr_attachment_migration_test.go
+++ db/background_mgr_attachment_migration_test.go
@@ -84,8 +84,6 @@ func TestAttachmentMigrationManagerResumeStoppedMigration(t *testing.T) {
 	defer db.Close(ctx)
 	collection, ctx := GetSingleDatabaseCollectionWithUser(ctx, t, db)
 
-	// create some docs with attachments defined, a large number is needed to allow us to stop the migration midway
-	// through without it completing first
 	for i := range 4000 {
 		docBody := Body{
 			"value":         1234,
@@ -254,4 +252,79 @@ func TestMigrationManagerDocWithSyncAndGlobalAttachmentMetadata(t *testing.T) {
 		},
 	}, GetRawGlobalSyncAttachments(t, collection.dataStore, key))
 	require.Empty(t, GetRawSyncXattr(t, collection.dataStore, key).AttachmentsPre4dot0)
+}
+
+func TestAttachmentMigrationCheckpointPrefix(t *testing.T) {
+	base.TestRequiresDCPResync(t)
+	ctx := base.TestCtx(t)
+	bucket := base.GetTestBucket(t)
+	defer bucket.Close(ctx)
+
+	migrationID := "1234"
+	testCases := []struct {
+		name          string
+		collectionIDs []uint32
+		groupID       string
+		expected      string
+	}{
+		{
+			name:          "default collection, no metadata id",
+			collectionIDs: []uint32{base.DefaultCollectionID},
+			groupID:       "",
+			expected:      fmt.Sprintf("_sync:dcp_ck::sg-%v:att_migration:1234", base.ProductAPIVersion),
+		},
+		{
+			name:          "default collection, metadata id",
+			collectionIDs: []uint32{base.DefaultCollectionID},
+			groupID:       "foo",
+			expected:      fmt.Sprintf("_sync:dcp_ck:foo::sg-%v:att_migration:1234", base.ProductAPIVersion),
+		},
+		{
+			name:          "default collection + collection 1, no metadata id",
+			collectionIDs: []uint32{base.DefaultCollectionID, 1},
+			groupID:       "",
+			expected:      fmt.Sprintf("_sync:dcp_ck::sg-%v:att_migration:1234", base.ProductAPIVersion),
+		},
+		{
+			name:          "default collection + collection 1, metadata id",
+			collectionIDs: []uint32{base.DefaultCollectionID, 1},
+			groupID:       "foo",
+			expected:      fmt.Sprintf("_sync:dcp_ck:foo::sg-%v:att_migration:1234", base.ProductAPIVersion),
+		},
+	}
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			autoImport := false
+			db, err := NewDatabaseContext(
+				ctx,
+				"db",
+				bucket.NoCloseClone(),
+				autoImport,
+				DatabaseContextOptions{
+					Scopes:  GetScopesOptions(t, bucket, 1),
+					GroupID: test.groupID,
+				},
+			)
+			require.NoError(t, err)
+			defer db.Close(ctx)
+			clientOptions := getMigrationDCPClientOptions(
+				test.collectionIDs,
+				db.Options.GroupID,
+				db.MetadataKeys.DCPCheckpointPrefix(db.Options.GroupID),
+			)
+
+			dcpFeedKey := GenerateAttachmentMigrationDCPStreamName(migrationID)
+			b, err := base.AsGocbV2Bucket(bucket)
+			require.NoError(t, err)
+			dcpClient, err := base.NewDCPClient(
+				ctx,
+				dcpFeedKey,
+				nil,
+				*clientOptions,
+				b,
+			)
+			require.NoError(t, err)
+			require.Equal(t, test.expected, dcpClient.GetMetadataKeyPrefix())
+		})
+	}
 }
