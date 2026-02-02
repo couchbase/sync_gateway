293cd550922ca7147f42adeeed0a648518870816
Tor Colvin <tor.colvin@couchbase.com>
2026-02-02 11:51:25 -0500

    add a test for metadatakeys

--- base/constants_syncdocs_test.go
+++ base/constants_syncdocs_test.go
@@ -117,3 +117,150 @@ func TestDCPMetadataKeyUpgrade(t *testing.T) {
 	versionPrefix = customMetadataKeys.DCPVersionedCheckpointPrefix("", 0)
 	require.Equal(t, nonVersionedPrefix, versionPrefix)
 }
+
+func TestMetadataKeys(t *testing.T) {
+	testCases := []struct {
+		metadataID                       string
+		groupID                          string
+		syncSeqKey                       string
+		unusedSeqKey                     string // with seq 1
+		unusedSeqPrefix                  string
+		replicationStatusKey             string
+		heartbeaterKey                   string
+		sgCfgPrefix                      string
+		persistentConfigKey              string
+		unusedSeqRangeKey                string // from seq 1 to seq 2
+		dcpCheckpointPrefix              string
+		dcpVersionedCheckpointPrefix     string
+		userKey                          string
+		userKeyPrefix                    string
+		roleKey                          string
+		roleKeyPrefix                    string
+		userEmailKey                     string
+		sessionKey                       string
+		backgroundProcessHeartbeatPrefix string // with backgroundID
+		backgroundProcessStatusPrefix    string // with backgroundID
+	}{
+		{
+			metadataID:                       "",
+			groupID:                          "",
+			heartbeaterKey:                   "_sync:",
+			syncSeqKey:                       "_sync:seq",
+			unusedSeqKey:                     "_sync:unusedSeq:1",
+			unusedSeqPrefix:                  "_sync:unusedSeq:",
+			replicationStatusKey:             "_sync:sgrStatus:",
+			sgCfgPrefix:                      "_sync:cfg",
+			persistentConfigKey:              "default",
+			unusedSeqRangeKey:                "_sync:unusedSeqs:1:2",
+			dcpCheckpointPrefix:              "_sync:dcp_ck:",
+			dcpVersionedCheckpointPrefix:     "_sync:dcp_ck:10:",
+			userKey:                          "_sync:user:aUser",
+			userKeyPrefix:                    "_sync:user:",
+			roleKey:                          "_sync:role:aRole",
+			roleKeyPrefix:                    "_sync:role:",
+			userEmailKey:                     "_sync:useremail:alice@couchbase.com",
+			sessionKey:                       "_sync:session:aSessionID",
+			backgroundProcessHeartbeatPrefix: "_sync:background_process:heartbeat:backgroundID",
+			backgroundProcessStatusPrefix:    "_sync:background_process:status:backgroundID",
+		},
+		{
+			metadataID:                       "",
+			groupID:                          "aGroupID",
+			heartbeaterKey:                   "_sync:aGroupID:",
+			syncSeqKey:                       "_sync:seq",
+			unusedSeqKey:                     "_sync:unusedSeq:1",
+			unusedSeqPrefix:                  "_sync:unusedSeq:",
+			replicationStatusKey:             "_sync:sgrStatus:aGroupID",
+			sgCfgPrefix:                      "_sync:cfgaGroupID:",
+			persistentConfigKey:              "aGroupID",
+			unusedSeqRangeKey:                "_sync:unusedSeqs:1:2",
+			dcpCheckpointPrefix:              "_sync:dcp_ck:aGroupID:",
+			dcpVersionedCheckpointPrefix:     "_sync:dcp_ck:aGroupID:10:",
+			userKey:                          "_sync:user:aUser",
+			userKeyPrefix:                    "_sync:user:",
+			roleKey:                          "_sync:role:aRole",
+			roleKeyPrefix:                    "_sync:role:",
+			userEmailKey:                     "_sync:useremail:alice@couchbase.com",
+			sessionKey:                       "_sync:session:aSessionID",
+			backgroundProcessHeartbeatPrefix: "_sync:background_process:heartbeat:backgroundID",
+			backgroundProcessStatusPrefix:    "_sync:background_process:status:backgroundID",
+		},
+		{
+			metadataID:                       "aMetadataID",
+			groupID:                          "",
+			heartbeaterKey:                   "_sync:m_aMetadataID:hb:",
+			syncSeqKey:                       "_sync:m_aMetadataID:seq",
+			unusedSeqKey:                     "_sync:m_aMetadataID:unusedSeq:1",
+			unusedSeqPrefix:                  "_sync:m_aMetadataID:unusedSeq:",
+			replicationStatusKey:             "_sync:m_aMetadataID:sgrStatus:",
+			sgCfgPrefix:                      "_sync:m_aMetadataID:cfg",
+			persistentConfigKey:              "default",
+			unusedSeqRangeKey:                "_sync:m_aMetadataID:unusedSeqs:1:2",
+			dcpCheckpointPrefix:              "_sync:dcp_ck:aMetadataID:",
+			dcpVersionedCheckpointPrefix:     "_sync:dcp_ck:aMetadataID:10:",
+			userKey:                          "_sync:user:aMetadataID:aUser",
+			userKeyPrefix:                    "_sync:user:aMetadataID:",
+			roleKey:                          "_sync:role:aMetadataID:aRole",
+			roleKeyPrefix:                    "_sync:role:aMetadataID:",
+			userEmailKey:                     "_sync:useremail:aMetadataID:alice@couchbase.com",
+			sessionKey:                       "_sync:session:aMetadataID:aSessionID",
+			backgroundProcessHeartbeatPrefix: "_sync:m_aMetadataID:background_process:heartbeat:backgroundID",
+			backgroundProcessStatusPrefix:    "_sync:m_aMetadataID:background_process:status:backgroundID",
+		},
+		{
+			metadataID:                       "aMetadataID",
+			groupID:                          "aGroupID",
+			heartbeaterKey:                   "_sync:m_aMetadataID:hb:aGroupID:",
+			syncSeqKey:                       "_sync:m_aMetadataID:seq",
+			unusedSeqKey:                     "_sync:m_aMetadataID:unusedSeq:1",
+			unusedSeqPrefix:                  "_sync:m_aMetadataID:unusedSeq:",
+			replicationStatusKey:             "_sync:m_aMetadataID:sgrStatus:aGroupID",
+			sgCfgPrefix:                      "_sync:m_aMetadataID:cfgaGroupID:",
+			persistentConfigKey:              "aGroupID",
+			unusedSeqRangeKey:                "_sync:m_aMetadataID:unusedSeqs:1:2",
+			dcpCheckpointPrefix:              "_sync:dcp_ck:aMetadataID:aGroupID:",
+			dcpVersionedCheckpointPrefix:     "_sync:dcp_ck:aMetadataID:aGroupID:10:",
+			userKey:                          "_sync:user:aMetadataID:aUser",
+			userKeyPrefix:                    "_sync:user:aMetadataID:",
+			roleKey:                          "_sync:role:aMetadataID:aRole",
+			roleKeyPrefix:                    "_sync:role:aMetadataID:",
+			userEmailKey:                     "_sync:useremail:aMetadataID:alice@couchbase.com",
+			sessionKey:                       "_sync:session:aMetadataID:aSessionID",
+			backgroundProcessHeartbeatPrefix: "_sync:m_aMetadataID:background_process:heartbeat:backgroundID",
+			backgroundProcessStatusPrefix:    "_sync:m_aMetadataID:background_process:status:backgroundID",
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(fmt.Sprintf("MetadataID=%s,GroupID=%s", test.metadataID, test.groupID), func(t *testing.T) {
+			metadataKeys := NewMetadataKeys(test.metadataID)
+			require.Equal(t, test.syncSeqKey, metadataKeys.SyncSeqKey())
+			require.Equal(t, test.unusedSeqKey, metadataKeys.UnusedSeqKey(1))
+			require.Equal(t, test.unusedSeqPrefix, metadataKeys.UnusedSeqPrefix())
+			require.Equal(t, test.replicationStatusKey, metadataKeys.ReplicationStatusKey(test.groupID))
+			require.Equal(t, test.heartbeaterKey, metadataKeys.HeartbeaterPrefix(test.groupID))
+			require.Equal(t, test.sgCfgPrefix, metadataKeys.SGCfgPrefix(test.groupID))
+			if test.groupID == "" {
+				// need a groupID for this, specificying default
+				persistentConfigKey, err := metadataKeys.PersistentConfigKey("default")
+				require.NoError(t, err)
+				require.Equal(t, test.persistentConfigKey, persistentConfigKey)
+			} else {
+				persistentConfigKey, err := metadataKeys.PersistentConfigKey(test.groupID)
+				require.NoError(t, err)
+				require.Equal(t, test.persistentConfigKey, persistentConfigKey)
+			}
+			require.Equal(t, test.unusedSeqRangeKey, metadataKeys.UnusedSeqRangeKey(1, 2))
+			require.Equal(t, test.dcpCheckpointPrefix, metadataKeys.DCPCheckpointPrefix(test.groupID))
+			require.Equal(t, test.dcpVersionedCheckpointPrefix, metadataKeys.DCPVersionedCheckpointPrefix(test.groupID, 10))
+			require.Equal(t, test.userKey, metadataKeys.UserKey("aUser"))
+			require.Equal(t, test.userKeyPrefix, metadataKeys.UserKeyPrefix())
+			require.Equal(t, test.roleKey, metadataKeys.RoleKey("aRole"))
+			require.Equal(t, test.roleKeyPrefix, metadataKeys.RoleKeyPrefix())
+			require.Equal(t, test.userEmailKey, metadataKeys.UserEmailKey("alice@couchbase.com"))
+			require.Equal(t, test.sessionKey, metadataKeys.SessionKey("aSessionID"))
+			require.Equal(t, test.backgroundProcessHeartbeatPrefix, metadataKeys.BackgroundProcessHeartbeatPrefix("backgroundID"))
+			require.Equal(t, test.backgroundProcessStatusPrefix, metadataKeys.BackgroundProcessStatusPrefix("backgroundID"))
+		})
+	}
+}
