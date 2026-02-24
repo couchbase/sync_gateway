be47f653394a9beb7a9af138dbaf3527403197f4
Tor Colvin <tor.colvin@couchbase.com>
2026-02-24 11:08:04 -0500

    Allow collections to work

--- base/dcp_sharded.go
+++ base/dcp_sharded.go
@@ -71,7 +71,7 @@ func StartShardedDCPFeed(ctx context.Context, dbName string, configGroup string,
 	}
 	if minVersion.Less(firstVersionToSupportCollections) {
 		// DefaultScope is allowed by older versions of CBGT as long as no collections are specified.
-		if len(collections) > 0 {
+		if len(collections) > 0 && len(collections) != 1 || collections[0] != DefaultCollection {
 			return nil, fmt.Errorf("cannot start DCP feed on non-default collection with legacy nodes present in the cluster")
 		}
 	}
