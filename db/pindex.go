a9b66729d0eae35ffb2c6ad9bcd719aa1b08231e
Tor Colvin <tor.colvin@couchbase.com>
2026-03-09 14:48:58 -0400

    rename pindex functions to not include import (#8098)
    Generalize these functions in preparation for reuse for distributed resync.

--- db/pindex.go
+++ db/pindex.go
@@ -20,14 +20,14 @@ import (
 	"github.com/couchbase/sync_gateway/base"
 )
 
-// registerImportPindexImplMutex locks access to cbgt.RegisterImportPindexImpl.
-var registerImportPindexImplMutex = sync.Mutex{}
+// registerPindexImplMutex serializes access to cbgt.RegisterPIndexImplType, which uses global state without its own synchronization.
+var registerPindexImplMutex = sync.Mutex{}
 
 // RegisterImportPindexImpl registers the PIndex type definition.  This is invoked by cbgt when a Pindex (collection of
 // vbuckets) is assigned to this node.
 func RegisterImportPindexImpl(ctx context.Context, configGroup string) {
-	registerImportPindexImplMutex.Lock()
-	defer registerImportPindexImplMutex.Unlock()
+	registerPindexImplMutex.Lock()
+	defer registerPindexImplMutex.Unlock()
 
 	// Since RegisterPIndexImplType is a global var without synchronization, index type needs to be
 	// config group scoped.  The associated importListener within the context is retrieved based on the
@@ -44,8 +44,8 @@ func RegisterImportPindexImpl(ctx context.Context, configGroup string) {
 		})
 }
 
-// getListenerForIndex looks up the importListener for the dbName specified in the index params
-func getListenerImportDest(ctx context.Context, indexParams string, restart func()) (cbgt.Dest, error) {
+// getCbgtDest looks up a cbgt.Dest based on a name specified in indexParams.
+func getCbgtDest(ctx context.Context, indexParams string, restart func()) (cbgt.Dest, error) {
 
 	var outerParams struct {
 		Params string `json:"params"`
@@ -58,10 +58,10 @@ func getListenerImportDest(ctx context.Context, indexParams string, restart func
 	var sgIndexParams base.SGFeedIndexParams
 	err = base.JSONUnmarshal([]byte(outerParams.Params), &sgIndexParams)
 	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling dbname from cbgt index params: %w", err)
+		return nil, fmt.Errorf("error unmarshalling SGFeedIndexParams from cbgt indexParams.params %s: %w", base.MD(outerParams.Params), err)
 	}
 
-	base.DebugfCtx(ctx, base.KeyDCP, "Fetching listener import dest for %v", base.MD(sgIndexParams.DestKey))
+	base.DebugfCtx(ctx, base.KeyDCP, "Fetching dest for %v", base.MD(sgIndexParams.DestKey))
 	destFactory, fetchErr := base.FetchDestFactory(sgIndexParams.DestKey)
 	if fetchErr != nil {
 		return nil, fmt.Errorf("error retrieving listener for indexParams %v: %v", indexParams, fetchErr)
@@ -69,20 +69,19 @@ func getListenerImportDest(ctx context.Context, indexParams string, restart func
 	return destFactory(restart)
 }
 
+// getNewPIndexImplType finds the correct cbgt.Dest based on the indexParams provided. Looks up the dest based on a key in params set by Sync Gateway.
 func getNewPIndexImplType(ctx context.Context) func(indexType, indexParams, path string, restart func()) (cbgt.PIndexImpl, cbgt.Dest, error) {
-	// NewImportPIndexImpl is called when the node is first added to the cbgt cfg.  On a node restart,
-	// OpenImportPindexImplUsing is called.
-	newImportPIndexImpl := func(indexType, indexParams, path string, restart func()) (cbgt.PIndexImpl, cbgt.Dest, error) {
+	newPIndexImpl := func(indexType, indexParams, path string, restart func()) (cbgt.PIndexImpl, cbgt.Dest, error) {
 		defer base.FatalPanicHandler()
 
-		importDest, err := getListenerImportDest(ctx, indexParams, restart)
+		dest, err := getCbgtDest(ctx, indexParams, restart)
 		if err != nil {
 			// This error can occur when a stale index definition hasn't yet been removed from the plan (e.g. on update to db config)
-			base.InfofCtx(ctx, base.KeyDCP, "No importDest found for indexParams - usually an obsolete index pending removal. %v", err)
+			base.DebugfCtx(ctx, base.KeyDCP, "No dest found for indexParams - usually an obsolete index pending removal. %v", err)
 		}
-		return nil, importDest, err
+		return nil, dest, err
 	}
-	return newImportPIndexImpl
+	return newPIndexImpl
 }
 
 // openPIndexImpl is required for cbgt, but this is used by Sync Gateway.
@@ -104,7 +103,7 @@ func getOpenExPIndexImpl(ctx context.Context) func(indexType, path string, resta
 		if !ok {
 			return nil, nil, errors.New("indexParams in options is not a string for OpenExPIndexImpl")
 		}
-		importDest, err := getListenerImportDest(ctx, indexParams, restart)
-		return nil, importDest, err
+		dest, err := getCbgtDest(ctx, indexParams, restart)
+		return nil, dest, err
 	}
 }
