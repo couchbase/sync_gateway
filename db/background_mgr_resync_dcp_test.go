4d8ea7b0d42b03b63fdc14b7ebc82b7fd74602a6
Tor Colvin <tor.colvin@couchbase.com>
2026-03-30 11:42:11 -0400

    Skip distributed resync tests for CE (#8140)

--- db/background_mgr_resync_dcp_test.go
+++ db/background_mgr_resync_dcp_test.go
@@ -241,6 +241,10 @@ func TestResyncManagerDCPStart(t *testing.T) {
 	})
 
 	for _, distributed := range []bool{false, true} {
+		if distributed && !base.IsEnterpriseEdition() {
+			// distributed resync is only supported in EE
+			continue
+		}
 		t.Run(fmt.Sprintf("Resync with updated sync function/distributed=%t", distributed), func(t *testing.T) {
 			docsToCreate := 100
 			db, ctx := setupTestDBForResyncWithDocs(t, docsToCreate, true)
