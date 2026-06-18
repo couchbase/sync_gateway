e03afdbbc9a876f13296f1ea790584fcde34e94d
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2026-06-18 15:00:53 +0100

    fix build issues

--- base/hlc_windows.go
+++ base/hlc_windows.go
@@ -1,35 +1,48 @@
 //go:build windows
 
+//  Copyright 2026-Present Couchbase, Inc.
+//
+//  Use of this software is governed by the Business Source License included
+//  in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+//  in that file, in accordance with the Business Source License, use of this
+//  software will be governed by the Apache License, Version 2.0, included in
+//  the file licenses/APL2.txt.
+
 package base
 
 import (
 	"sync/atomic"
 	"time"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
 var (
-	gpcFreq          int64
-	gpcAnchorTickets int64
-	gpcAnchorNanos   int64
+	procQPF = windows.NewLazySystemDLL("kernel32.dll").NewProc("QueryPerformanceFrequency")
+	procQPC = windows.NewLazySystemDLL("kernel32.dll").NewProc("QueryPerformanceCounter")
+
+	qpcFreq        int64
+	qpcAnchorTicks int64
+	qpcAnchorNanos int64
 )
 
 func init() {
-	windows.QueryPerformanceFrequency(&gpcFreq)
+	var freq int64
+	procQPF.Call(uintptr(unsafe.Pointer(&freq)))
+	atomic.StoreInt64(&qpcFreq, freq)
 
 	var ticks int64
-	windows.QueryPerformanceCounter(&ticks)
-	atomic.StoreInt64(&gpcAnchorTickets, ticks)
-	atomic.StoreInt64(&gpcAnchorNanos, uint64(time.Now().UnixNano()))
+	procQPC.Call(uintptr(unsafe.Pointer(&ticks)))
+	atomic.StoreInt64(&qpcAnchorTicks, ticks)
+	atomic.StoreInt64(&qpcAnchorNanos, time.Now().UnixNano())
 }
 
-func hlvWallClock() uint64 {
+func hlcWallClock() uint64 {
 	var ticks int64
-	windows.QueryPerformanceCounter(&ticks)
+	procQPC.Call(uintptr(unsafe.Pointer(&ticks)))
 
-	elapsed := ticks - atomic.LoadInt64(&gpcAnchorTickets)
-	// convert elapsed ticks to nanoseconds
-	elapsedNanos := uint64(elapsed) * 1e9 / uint64(gpcFreq)
-	return atomic.LoadUint64(&gpcAnchorNanos) + elapsedNanos
+	elapsed := ticks - atomic.LoadInt64(&qpcAnchorTicks)
+	elapsedNanos := uint64(elapsed) * 1_000_000_000 / uint64(atomic.LoadInt64(&qpcFreq))
+	return uint64(atomic.LoadInt64(&qpcAnchorNanos)) + elapsedNanos
 }
