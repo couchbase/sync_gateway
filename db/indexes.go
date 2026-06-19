dbf264c24c7fa50b02af24d0b1898635ad486d85
Ben Brooks <ben.brooks@couchbase.com>
2026-06-19 14:00:26 +0100

    Support primary and fallback data stores when invalidating principals during resync on a dual metadatastore database

--- db/indexes.go
+++ db/indexes.go
@@ -413,7 +413,15 @@ func InitializeIndexes(ctx context.Context, n1QLStore base.N1QLStore, options In
 		return nil
 	}
 
-	indexesMeta, err := base.GetIndexesMeta(ctx, n1QLStore, requiredIndexes.FullIndexNames())
+	var (
+		indexesMeta map[string]base.IndexMeta
+		err         error
+	)
+	if base.IsMobileSystemCollection(n1QLStore) {
+		indexesMeta, err = base.GetSystemCollectionIndexesMeta(ctx, n1QLStore, n1QLStore.ScopeName(), n1QLStore.CollectionName(), requiredIndexes.FullIndexNames())
+	} else {
+		indexesMeta, err = base.GetIndexesMeta(ctx, n1QLStore, requiredIndexes.FullIndexNames())
+	}
 	if err != nil {
 		base.WarnfCtx(ctx, "Error getting indexes meta: %v - continuing to create", err)
 	}
@@ -432,7 +440,7 @@ func InitializeIndexes(ctx context.Context, n1QLStore base.N1QLStore, options In
 
 	if len(requiredIndexes) == 0 {
 		// all indexes are already online so we can return early without any per-index queries
-		base.InfofCtx(ctx, base.KeyAll, "Indexes ready - already online when checked")
+		base.InfofCtx(ctx, base.KeyAll, "Indexes ready for %q - already online when checked", n1QLStore.GetName())
 		return nil
 	}
 
