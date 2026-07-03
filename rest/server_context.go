079ce5005ea8b91553e1900cc4c59d8093bf3106
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2026-07-03 17:13:15 +0100

    add database error code for this case

--- rest/server_context.go
+++ rest/server_context.go
@@ -860,6 +860,7 @@ func (sc *ServerContext) _getOrAddDatabaseFromConfig(ctx context.Context, config
 		if !defaultCollectionPresent {
 			// If the _default._default collection has been dropped by a customer, we can't use it for metadata.
 			// This is a misconfiguration, fail the database load.
+			sc._handleInvalidDatabaseConfig(ctx, spec.BucketName, config, db.NewDatabaseError(db.DatabaseNoMetadataStore))
 			return nil, fmt.Errorf("use_system_metadata_collection disabled but _default._default does not exist on bucket %s — cannot use legacy collection for metadata for db %s", base.MD(spec.BucketName), base.MD(dbName))
 		}
 		contextOptions.MetadataStore = bucket.DefaultDataStore(ctx)
