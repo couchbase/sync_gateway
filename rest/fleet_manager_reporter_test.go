1d7e4cf03ce24e7a9ea2740b492c8d07cbb71345
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2026-07-16 13:35:25 +0100

    CBG-5596: add sync test for testing report interval

--- rest/fleet_manager_reporter_test.go
+++ rest/fleet_manager_reporter_test.go
@@ -12,9 +12,13 @@ package rest
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"net/http"
+	"sync"
 	"testing"
+	"testing/synctest"
+	"time"
 
 	"github.com/couchbase/sync_gateway/base"
 	"github.com/couchbase/sync_gateway/testing/assert"
@@ -203,4 +207,66 @@ func TestAllFleetManagerMetricsPopulated(t *testing.T) {
 	assert.NotEmpty(t, metrics.ProductInfo.Edition)
 	assert.NotEmpty(t, metrics.ProductInfo.Version)
 	assert.NotEmpty(t, metrics.ProductInfo.Name)
+}
+
+func TestFleetManagerReporterLoop(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		ctx, cancel := context.WithCancelCause(base.TestCtx(t))
+		t.Cleanup(func() {
+			cancel(nil)
+		})
+
+		var mutex sync.Mutex
+		var reports []base.FleetManagerCollectorSettings
+		current := base.FleetManagerCollectorSettings{Enabled: true, ReportingInterval: 1}
+		getCollectorFunc := func() base.FleetManagerCollectorSettings {
+			mutex.Lock()
+			defer mutex.Unlock()
+			return current
+		}
+		reportFunc := func(settings base.FleetManagerCollectorSettings) {
+			mutex.Lock()
+			defer mutex.Unlock()
+			if settings.Enabled { // mirror our report closure in production code
+				reports = append(reports, settings)
+			}
+		}
+		reportCount := func() int {
+			mutex.Lock()
+			defer mutex.Unlock()
+			return len(reports)
+		}
+
+		go runFleetManagerReportLoop(ctx, getCollectorFunc, reportFunc)
+
+		synctest.Wait() // wait until first report runs and is blocking on the ticker interval
+		require.Equal(t, 1, reportCount())
+
+		time.Sleep(1 * time.Hour) // mock 1 hour passing, should trigger new report on ticker interval
+		synctest.Wait()           // wait for report goroutine to block again
+		require.Equal(t, 2, reportCount())
+
+		mutex.Lock()
+		current.Enabled = false
+		mutex.Unlock()
+		time.Sleep(1 * time.Hour)          // mock another hour passing
+		synctest.Wait()                    // wait for report goroutine to block again after another report interval
+		require.Equal(t, 2, reportCount()) // tick has fired above but enabled is false so no report should fire
+
+		// update interval to two hours
+		mutex.Lock()
+		current.ReportingInterval = 2
+		current.Enabled = true
+		mutex.Unlock()
+		time.Sleep(2 * time.Hour)
+		synctest.Wait()                    // wait for report goroutine to block again after another report interval
+		require.Equal(t, 3, reportCount()) // should only report once (on the next 1 hour tick, then tick resets to 2 hours so no 4th report on second hour)
+
+		// assert reports only contain entries of enabled=true
+		mutex.Lock()
+		for _, report := range reports {
+			assert.True(t, report.Enabled)
+		}
+		mutex.Unlock()
+	})
 }
