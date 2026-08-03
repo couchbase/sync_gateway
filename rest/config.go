c6d710d56e897b2da3523e9989ae31ee7e0d7548
Ritesh Kumar <ritesh.kumar@couchbase.com>
2026-08-03 22:36:45 +0530

    fixes based on pr comments

--- rest/config.go
+++ rest/config.go
@@ -965,7 +965,7 @@ func (dbConfig *DbConfig) validateVersion(ctx context.Context, isEnterpriseEditi
 				continue
 			}
 			seenIssuers[oidc.Issuer]++
-			if validateOIDCConfig && oidc.ShouldValidate {
+			if validateOIDCConfig && !oidc.SkipValidation {
 				_, _, err := oidc.DiscoverConfig(ctx)
 				if err != nil {
 					multiError = multiError.Append(fmt.Errorf("failed to validate OIDC configuration for %s: %w", name, err))
