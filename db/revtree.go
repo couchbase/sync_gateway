03c48e2900c00ec7c2f7c6f39ccb8c46d6e501ba
Gregory Newman-Smith <gregory.newmansmith@couchbase.com>
2026-08-18 14:34:00 +0100

    remove assertion and use redact error

--- db/revtree.go
+++ db/revtree.go
@@ -413,8 +413,7 @@ func (tree RevTree) addRevision(ctx context.Context, docid string, info RevInfo)
 		// makes the document unreplicatable to pre-4.0 clients. See CBG-5713.
 		if generation, _, err := parseRevID(revid); err == nil {
 			if parentGeneration, _, parentErr := parseRevID(p); parentErr == nil && generation <= parentGeneration {
-				base.AssertfCtx(ctx, "doc: %s, RevTree addRevision, revision %q is not a higher generation than its parent %q", base.UD(docid), revid, p)
-				return fmt.Errorf("doc: %s, RevTree addRevision, revision %q is not a higher generation than its parent %q", base.UD(docid), revid, p)
+				return base.RedactErrorf("doc: %s, RevTree addRevision, revision %q is not a higher generation than its parent %q", base.UD(docid), revid, p)
 			}
 		}
 		// we're adding a new child, so strip the channels from the parent - they're now superseded
